446acbe92dac9cedcc61a5a41c3db8aedea18755
cerami <cerami@code.google.com>
2003-11-07 16:08:01 +0000

    Made all tables upper case

--- cpath/dbData/bootstrap.sql
+++ cpath/dbData/bootstrap.sql
@@ -13,51 +13,51 @@
 #
 
 use cpath;
-truncate table import;
-truncate table external_db;
-truncate table external_db_cv;
-truncate table external_link;
-truncate table cpath;
-truncate table internal_link;
+truncate table IMPORT;
+truncate table EXTERNAL_DB;
+truncate table EXTERNAL_DB_CV;
+truncate table EXTERNAL_LINK;
+truncate table CPATH;
+truncate table INTERNAL_LINK;
 
 #
 # Insert Data into table 'cpath'
 #
-INSERT INTO `cpath` VALUES (1, 'JUNIT_ENTITY', 'JUnit Entity Description', 'PHYSICAL_ENTITY', '', 101, '<?xml version="1.0" encoding="UTF-8"?><note>testing</note>', 20031020125251, 20031020125251);
+INSERT INTO `CPATH` VALUES (1, 'JUNIT_ENTITY', 'JUnit Entity Description', 'PHYSICAL_ENTITY', '', 101, '<?xml version="1.0" encoding="UTF-8"?><note>testing</note>', 20031020125251, 20031020125251);
 
 #
-#  Insert data into table 'external_db'
+#  Insert data into table 'EXTERNAL_DB'
 #
 
-INSERT INTO `external_db` VALUES (1, 'Swiss-Prot', 'http://us.expasy.org/cgi-bin/niceprot.pl?%ID%', 'Swiss-Prot is a curated protein sequence database which strives to provide a high level of annotation, a minimal level of redundancy and high level of integration with other databases.', NULL, NULL, 20031002162924, 20031002102704);
-INSERT INTO `external_db` VALUES (2, 'PIR', 'http://pir.georgetown.edu/cgi-bin/nbrfget?uid=%ID%', 'The Protein Information Resource (PIR), located at Georgetown University Medical Center (GUMC), is an integrated public bioinformatics resource that supports genomic and proteomic research and scientific studies.', NULL, NULL, 20031002103329, 20031002103329);
-INSERT INTO `external_db` VALUES (3, 'RefSeq', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Search&db=Protein&doptcmdl=GenPept&term=%ID%', 'The Reference Sequence (RefSeq) collection aims to provide a comprehensive, integrated, non-redundant set of sequences, including genomic DNA, transcript (RNA), and protein products, for major research organisms.', NULL, NULL, 20031002163800, 20031002103655);
-INSERT INTO `external_db` VALUES (4, 'InterPro', 'http://www.ebi.ac.uk/interpro/IEntry?ac=%ID%', ' InterPro is a database of protein families, domains and functional sites in which identifiable features found in known proteins can be applied to unknown protein sequences.', NULL, NULL, 20031002162303, 20031002162303);
-INSERT INTO `external_db` VALUES (5, 'PROSITE', 'http://us.expasy.org/cgi-bin/nicedoc.pl?%ID%', 'Database of protein families and domains', NULL, NULL, 20031002162542, 20031002162542);
-INSERT INTO `external_db` VALUES (6, 'DIP', 'http://dip.doe-mbi.ucla.edu/dip/Search.cgi?SM=3&AC=DIP:%ID%&Search2=Query+DIP&GE=&DS=&PIR=&GB=&TX=&SF=&FN=&LO=&KW=', 'Database of Interacting Proteins', NULL, NULL, 20031002162656, 20031002162656);
-INSERT INTO `external_db` VALUES (7, 'Pfam', 'http://www.sanger.ac.uk/cgi-bin/Pfam/getacc?%ID%', 'Protein families database of alignments and HMMs.', NULL, NULL, 20031002162804, 20031002162804);
-INSERT INTO `external_db` VALUES (8, 'Entrez GI', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=protein&list_uids=%ID%&dopt=GenPept&term=%ID%&qty=1', 'NCBI Entrez System', NULL, NULL, 20031002163709, 20031002163709);
-INSERT INTO `external_db` VALUES (9, 'Locus Link', 'http://www.ncbi.nlm.nih.gov/LocusLink/LocRpt.cgi?l=%ID%', 'NCBI Locus Link', NULL, NULL, 20031002163709, 20031002163709);
-INSERT INTO `external_db` VALUES (10, 'GO', 'http://godatabase.org/cgi-bin/go.cgi?view=query&query=%ID%', 'Gene Ontology', NULL, NULL, 20031002163709, 20031002163709);
+INSERT INTO `EXTERNAL_DB` VALUES (1, 'Swiss-Prot', 'http://us.expasy.org/cgi-bin/niceprot.pl?%ID%', 'Swiss-Prot is a curated protein sequence database which strives to provide a high level of annotation, a minimal level of redundancy and high level of integration with other databases.', NULL, NULL, 20031002162924, 20031002102704);
+INSERT INTO `EXTERNAL_DB` VALUES (2, 'PIR', 'http://pir.georgetown.edu/cgi-bin/nbrfget?uid=%ID%', 'The Protein Information Resource (PIR), located at Georgetown University Medical Center (GUMC), is an integrated public bioinformatics resource that supports genomic and proteomic research and scientific studies.', NULL, NULL, 20031002103329, 20031002103329);
+INSERT INTO `EXTERNAL_DB` VALUES (3, 'RefSeq', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Search&db=Protein&doptcmdl=GenPept&term=%ID%', 'The Reference Sequence (RefSeq) collection aims to provide a comprehensive, integrated, non-redundant set of sequences, including genomic DNA, transcript (RNA), and protein products, for major research organisms.', NULL, NULL, 20031002163800, 20031002103655);
+INSERT INTO `EXTERNAL_DB` VALUES (4, 'InterPro', 'http://www.ebi.ac.uk/interpro/IEntry?ac=%ID%', ' InterPro is a database of protein families, domains and functional sites in which identifiable features found in known proteins can be applied to unknown protein sequences.', NULL, NULL, 20031002162303, 20031002162303);
+INSERT INTO `EXTERNAL_DB` VALUES (5, 'PROSITE', 'http://us.expasy.org/cgi-bin/nicedoc.pl?%ID%', 'Database of protein families and domains', NULL, NULL, 20031002162542, 20031002162542);
+INSERT INTO `EXTERNAL_DB` VALUES (6, 'DIP', 'http://dip.doe-mbi.ucla.edu/dip/Search.cgi?SM=3&AC=DIP:%ID%&Search2=Query+DIP&GE=&DS=&PIR=&GB=&TX=&SF=&FN=&LO=&KW=', 'Database of Interacting Proteins', NULL, NULL, 20031002162656, 20031002162656);
+INSERT INTO `EXTERNAL_DB` VALUES (7, 'Pfam', 'http://www.sanger.ac.uk/cgi-bin/Pfam/getacc?%ID%', 'Protein families database of alignments and HMMs.', NULL, NULL, 20031002162804, 20031002162804);
+INSERT INTO `EXTERNAL_DB` VALUES (8, 'Entrez GI', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=protein&list_uids=%ID%&dopt=GenPept&term=%ID%&qty=1', 'NCBI Entrez System', NULL, NULL, 20031002163709, 20031002163709);
+INSERT INTO `EXTERNAL_DB` VALUES (9, 'Locus Link', 'http://www.ncbi.nlm.nih.gov/LocusLink/LocRpt.cgi?l=%ID%', 'NCBI Locus Link', NULL, NULL, 20031002163709, 20031002163709);
+INSERT INTO `EXTERNAL_DB` VALUES (10, 'GO', 'http://godatabase.org/cgi-bin/go.cgi?view=query&query=%ID%', 'Gene Ontology', NULL, NULL, 20031002163709, 20031002163709);
 
 #
-#  Insert data into table 'external_db_cv'
+#  Insert data into table 'EXTERNAL_DB_CV'
 #
 
-INSERT INTO `external_db_cv` VALUES (1, 1, 'SWISS-PROT');
-INSERT INTO `external_db_cv` VALUES (2, 1, 'Swiss-Prot');
-INSERT INTO `external_db_cv` VALUES (3, 1, 'SwissProt');
-INSERT INTO `external_db_cv` VALUES (4, 1, 'SWP');
-INSERT INTO `external_db_cv` VALUES (5, 2, 'PIR');
-INSERT INTO `external_db_cv` VALUES (6, 3, 'RefSeq');
-INSERT INTO `external_db_cv` VALUES (7, 4, 'InterPro');
-INSERT INTO `external_db_cv` VALUES (8, 5, 'PROSITE');
-INSERT INTO `external_db_cv` VALUES (9, 6, 'DIP');
-INSERT INTO `external_db_cv` VALUES (10, 7, 'Pfram');
-INSERT INTO `external_db_cv` VALUES (11, 8, 'Entrez GI');
-INSERT INTO `external_db_cv` VALUES (12, 8, 'GI');
-INSERT INTO `external_db_cv` VALUES (13, 9, 'LocusLink');
-INSERT INTO `external_db_cv` VALUES (14, 10, 'GO');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (1, 1, 'SWISS-PROT');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (2, 1, 'Swiss-Prot');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (3, 1, 'SwissProt');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (4, 1, 'SWP');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (5, 2, 'PIR');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (6, 3, 'RefSeq');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (7, 4, 'InterPro');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (8, 5, 'PROSITE');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (9, 6, 'DIP');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (10, 7, 'Pfram');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (11, 8, 'Entrez GI');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (12, 8, 'GI');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (13, 9, 'LocusLink');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (14, 10, 'GO');
 
 use grid;
 truncate table orf_info;
@@ -89,6 +89,3 @@ INSERT INTO `interactions` VALUES (7, '1662', '5999', 'Affinity Precipitation', 
 INSERT INTO `interactions` VALUES (8, '1662', '1110', 'Affinity Precipitation', 'Bassler et al', 'Not Reported', 'Not Reported', 'AB', ';11583615;', 'F', NULL);
 INSERT INTO `interactions` VALUES (9, '1662', '5945', 'Affinity Precipitation', 'Bassler et al', 'Not Reported', 'Not Reported', 'AB', ';11583615;', 'F', NULL);
 INSERT INTO `interactions` VALUES (10, '1662', '2370', 'Affinity Precipitation', 'Bassler et al', 'Not Reported', 'Not Reported', 'AB', ';11583615;', 'F', NULL);
-
-SELECT COUNT(*) FROM interactions;
-SELECT COUNT(*) FROM orf_info;
