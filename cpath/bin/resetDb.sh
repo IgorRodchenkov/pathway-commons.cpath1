d295289295169f3373a8fa69acc3d268548e1f2b
cerami <cerami@code.google.com>
2003-11-07 18:27:42 +0000

    Moved to resetDb.pl

--- cpath/bin/resetDb.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-print "Reseting Database\n";
-chdir ("../dbData") || die "Cannot cd to dbData directory";
-system "mysql -u tomcat --password=kitty < reset.sql";
-print "Done.  Go!  Go!  Go!\n";
