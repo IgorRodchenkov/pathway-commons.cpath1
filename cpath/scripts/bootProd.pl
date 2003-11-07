31ab0377365262ceebc86268704a81480c98f944
cerami <cerami@code.google.com>
2003-11-07 18:18:20 +0000

    Removed

--- cpath/scripts/bootProd.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-print "Reseting Production Database\n";
-chdir ("dbData") || die "Cannot cd to dbData directory";
-system "mysql -u tomcat --password=kitty < bootProd.sql";
-print "Bootstrap Loading Complete\n";
