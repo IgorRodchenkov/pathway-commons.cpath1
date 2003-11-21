6d70316d93d2e4510896bc1bc7f78f4ef6d846a9
cerami <cerami@code.google.com>
2003-11-21 21:33:22 +0000

    Added setting up sticky bit

--- cpath/bin/deployText.sh
+++ cpath/bin/deployText.sh
@@ -4,4 +4,6 @@
 
 echo Copying Full Text Index to Tomcat
 cp -R /home/cerami/dev/sander/cpath/textIndex/* /var/lib/tomcat4/webapps/textIndex
+# Set Sticky Bit so that Tomcat can write to this directory
+chmod 1777 /home/cerami/dev/sander/cpath/textIndex
 echo Done.
