52079f8e4b0e27eb0c76ae451abac7a68b1c720a
cerami <cerami@code.google.com>
2004-01-02 16:41:10 +0000

    Updated paths

--- cpath/bin/deployText.sh
+++ cpath/bin/deployText.sh
@@ -5,5 +5,5 @@
 echo Copying Full Text Index to Tomcat
 cp -R /home/cerami/dev/sander/cpath/textIndex/* /var/lib/tomcat4/webapps/textIndex
 # Set Sticky Bit so that Tomcat can write to this directory
-chmod 1777 /home/cerami/dev/sander/cpath/textIndex
+chmod 1777 /var/lib/tomcat4/webapps/textIndex 
 echo Done.
