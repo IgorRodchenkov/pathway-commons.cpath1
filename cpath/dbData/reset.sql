e7d20821e215c11d4eedde1b43299d21d21d387e
cerami <cerami@code.google.com>
2004-01-15 21:54:32 +0000

    Updated values for SwissProt, now truncates xml_cache

--- cpath/dbData/reset.sql
+++ cpath/dbData/reset.sql
@@ -10,27 +10,28 @@
 #
 
 use cpath;
-truncate table IMPORT;
-truncate table EXTERNAL_DB;
-truncate table EXTERNAL_DB_CV;
-truncate table EXTERNAL_LINK;
-truncate table CPATH;
-truncate table INTERNAL_LINK;
+truncate table import;
+truncate table external_db;
+truncate table external_db_cv;
+truncate table external_link;
+truncate table cpath;
+truncate table internal_link;
+truncate table xml_cache;
 
 #
 #  Insert data into table 'EXTERNAL_DB'
 #
 
-INSERT INTO `EXTERNAL_DB` VALUES (1, 'Swiss-Prot', 'http://us.expasy.org/cgi-bin/niceprot.pl?%ID%', 'Swiss-Prot is a curated protein sequence database which strives to provide a high level of annotation, a minimal level of redundancy and high level of integration with other databases.', 1, NULL, NULL, 20031002162924, 20031002102704);
-INSERT INTO `EXTERNAL_DB` VALUES (2, 'PIR', 'http://pir.georgetown.edu/cgi-bin/nbrfget?uid=%ID%', 'The Protein Information Resource (PIR), located at Georgetown University Medical Center (GUMC), is an integrated public bioinformatics resource that supports genomic and proteomic research and scientific studies.', 5, NULL, NULL, 20031002103329, 20031002103329);
-INSERT INTO `EXTERNAL_DB` VALUES (3, 'RefSeq', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Search&db=Protein&doptcmdl=GenPept&term=%ID%', 'The Reference Sequence (RefSeq) collection aims to provide a comprehensive, integrated, non-redundant set of sequences, including genomic DNA, transcript (RNA), and protein products, for major research organisms.', 6, NULL, NULL, 20031002163800, 20031002103655);
-INSERT INTO `EXTERNAL_DB` VALUES (4, 'InterPro', 'http://www.ebi.ac.uk/interpro/IEntry?ac=%ID%', ' InterPro is a database of protein families, domains and functional sites in which identifiable features found in known proteins can be applied to unknown protein sequences.', 7, NULL, NULL, 20031002162303, 20031002162303);
-INSERT INTO `EXTERNAL_DB` VALUES (5, 'PROSITE', 'http://us.expasy.org/cgi-bin/nicedoc.pl?%ID%', 'Database of protein families and domains', 8, NULL, NULL, 20031002162542, 20031002162542);
-INSERT INTO `EXTERNAL_DB` VALUES (6, 'DIP', 'http://dip.doe-mbi.ucla.edu/dip/Search.cgi?SM=3&AC=DIP:%ID%&Search2=Query+DIP&GE=&DS=&PIR=&GB=&TX=&SF=&FN=&LO=&KW=', 'Database of Interacting Proteins', 9, NULL, NULL, 20031002162656, 20031002162656);
-INSERT INTO `EXTERNAL_DB` VALUES (7, 'Pfam', 'http://www.sanger.ac.uk/cgi-bin/Pfam/getacc?%ID%', 'Protein families database of alignments and HMMs.', 10, NULL, NULL, 20031002162804, 20031002162804);
-INSERT INTO `EXTERNAL_DB` VALUES (8, 'Entrez GI', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=protein&list_uids=%ID%&dopt=GenPept&term=%ID%&qty=1', 'NCBI Entrez System', 11, NULL, NULL, 20031002163709, 20031002163709);
-INSERT INTO `EXTERNAL_DB` VALUES (9, 'Locus Link', 'http://www.ncbi.nlm.nih.gov/LocusLink/LocRpt.cgi?l=%ID%', 'NCBI Locus Link', 13, NULL, NULL, 20031002163709, 20031002163709);
-INSERT INTO `EXTERNAL_DB` VALUES (10, 'GO', 'http://godatabase.org/cgi-bin/go.cgi?view=query&query=%ID%', 'Gene Ontology', 14, NULL, NULL, 20031002163709, 20031002163709);
+INSERT INTO `EXTERNAL_DB` VALUES (1, 'Swiss-Prot', 'http://us.expasy.org/cgi-bin/niceprot.pl?%ID%', 'Swiss-Prot is a curated protein sequence database which strives to provide a high level of annotation, a minimal level of redundancy and high level of integration with other databases.', 3, NULL, NULL, 20031002162924, 20031002102704);
+INSERT INTO `EXTERNAL_DB` VALUES (2, 'PIR', 'http://pir.georgetown.edu/cgi-bin/nbrfget?uid=%ID%', 'The Protein Information Resource (PIR), located at Georgetown University Medical Center (GUMC), is an integrated public bioinformatics resource that supports genomic and proteomic research and scientific studies.', 6, NULL, NULL, 20031002103329, 20031002103329);
+INSERT INTO `EXTERNAL_DB` VALUES (3, 'RefSeq', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Search&db=Protein&doptcmdl=GenPept&term=%ID%', 'The Reference Sequence (RefSeq) collection aims to provide a comprehensive, integrated, non-redundant set of sequences, including genomic DNA, transcript (RNA), and protein products, for major research organisms.', 7, NULL, NULL, 20031002163800, 20031002103655);
+INSERT INTO `EXTERNAL_DB` VALUES (4, 'InterPro', 'http://www.ebi.ac.uk/interpro/IEntry?ac=%ID%', ' InterPro is a database of protein families, domains and functional sites in which identifiable features found in known proteins can be applied to unknown protein sequences.', 8, NULL, NULL, 20031002162303, 20031002162303);
+INSERT INTO `EXTERNAL_DB` VALUES (5, 'PROSITE', 'http://us.expasy.org/cgi-bin/nicedoc.pl?%ID%', 'Database of protein families and domains', 9, NULL, NULL, 20031002162542, 20031002162542);
+INSERT INTO `EXTERNAL_DB` VALUES (6, 'DIP', 'http://dip.doe-mbi.ucla.edu/dip/Search.cgi?SM=3&AC=DIP:%ID%&Search2=Query+DIP&GE=&DS=&PIR=&GB=&TX=&SF=&FN=&LO=&KW=', 'Database of Interacting Proteins', 10, NULL, NULL, 20031002162656, 20031002162656);
+INSERT INTO `EXTERNAL_DB` VALUES (7, 'Pfam', 'http://www.sanger.ac.uk/cgi-bin/Pfam/getacc?%ID%', 'Protein families database of alignments and HMMs.', 11, NULL, NULL, 20031002162804, 20031002162804);
+INSERT INTO `EXTERNAL_DB` VALUES (8, 'Entrez GI', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=protein&list_uids=%ID%&dopt=GenPept&term=%ID%&qty=1', 'NCBI Entrez System', 12, NULL, NULL, 20031002163709, 20031002163709);
+INSERT INTO `EXTERNAL_DB` VALUES (9, 'Locus Link', 'http://www.ncbi.nlm.nih.gov/LocusLink/LocRpt.cgi?l=%ID%', 'NCBI Locus Link', 14, NULL, NULL, 20031002163709, 20031002163709);
+INSERT INTO `EXTERNAL_DB` VALUES (10, 'GO', 'http://godatabase.org/cgi-bin/go.cgi?view=query&query=%ID%', 'Gene Ontology', 15, NULL, NULL, 20031002163709, 20031002163709);
 INSERT INTO `EXTERNAL_DB` VALUES (11, 'PubMed', 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=PubMed&list_uids=%ID%&dopt=Abstract', 'NCBI PubMed', 15, NULL, NULL, 20031002163709, 20031002163709);
 #
 #  Insert data into table 'EXTERNAL_DB_CV'
@@ -38,19 +39,20 @@ INSERT INTO `EXTERNAL_DB` VALUES (11, 'PubMed', 'http://www.ncbi.nlm.nih.gov/ent
 
 INSERT INTO `EXTERNAL_DB_CV` VALUES (1, 1, 'SWISS-PROT');
 INSERT INTO `EXTERNAL_DB_CV` VALUES (2, 1, 'Swiss-Prot');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (3, 1, 'SwissTrembl');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (4, 1, 'SWP');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (5, 2, 'PIR');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (6, 3, 'RefSeq');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (7, 4, 'InterPro');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (8, 5, 'PROSITE');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (9, 6, 'DIP');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (10, 7, 'Pfam');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (11, 8, 'Entrez GI');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (12, 8, 'GI');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (13, 9, 'LocusLink');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (14, 10, 'GO');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (15, 11, 'PubMed');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (16, 11, 'pubmed');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (17, 11, 'PMID');
-INSERT INTO `EXTERNAL_DB_CV` VALUES (18, 11, 'pmid');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (3, 1, 'SwissProt');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (4, 1, 'SwissTrembl');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (5, 1, 'SWP');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (6, 2, 'PIR');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (7, 3, 'RefSeq');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (8, 4, 'InterPro');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (9, 5, 'PROSITE');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (10, 6, 'DIP');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (11, 7, 'Pfam');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (12, 8, 'Entrez GI');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (13, 8, 'GI');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (14, 9, 'LocusLink');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (15, 10, 'GO');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (16, 11, 'PubMed');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (17, 11, 'pubmed');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (18, 11, 'PMID');
+INSERT INTO `EXTERNAL_DB_CV` VALUES (19, 11, 'pmid');
