22a96a58244a5a0c8cd707c8ff74ff45a5d11519
cerami <cerami@code.google.com>
2004-02-27 21:57:00 +0000

    Created global script for single command deployment to cbio

---
+++ cpath/bin/production.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+# Script Used to Deploy cPath to cBio and restart Tomcat
+# Author:  Ethan Cerami (cerami@cbio.mskcc.org).
+
+use strict;
+
+my $tom = "/var/lib/tomcat4/webapps/cpath";
+
+# CD to the cpath tomcat dir
+chdir "$tom" or die;
+
+# Delete everything within cpath
+system "rm -rf *";
+
+# Update All Code
+chdir "/home/cerami/dev/sander/cpath";
+system "cvs update -d";
+
+# Create War File
+system "ant war";
+
+# Copy War File over
+print "Copying war file to: $tom\n";
+system "cp dist/cpath.war $tom";
+
+# Expand War File, and set permissions on textIndex dir
+chdir "$tom" or die;
+print "Unpacking WAR File\n";
+system "jar -xvf cpath.war";
+system "chmod o+w textIndex";
+system "rm cpath.war";
+
+print "Restarting Tomcat\n";
+system "sudo /etc/init.d/tomcat4 restart";
+
+print "Deployment Complete\n";
+
