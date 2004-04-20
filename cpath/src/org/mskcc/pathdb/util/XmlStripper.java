78cd8144a874cba3801fdd0f1624b8cd449ef916
cerami <cerami@code.google.com>
2004-04-20 22:09:11 +0000

    Now calls getTextTrim() to remove unnecessary whitespace characters

--- cpath/src/org/mskcc/pathdb/util/XmlStripper.java
+++ cpath/src/org/mskcc/pathdb/util/XmlStripper.java
@@ -6,6 +6,7 @@ import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 import java.io.StringReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -20,9 +21,9 @@ public class XmlStripper {
      * Strips all XML Markup from Document.
      * @param xml XML Document.
      * @return String of Tokens.
-     * @throws JDOMException Error in JDOM.
+     * @throws IOException Error in JDOM.
      */
-    public String stripTags(String xml) throws JDOMException {
+    public static String stripTags(String xml) throws IOException {
         StringBuffer textBuffer = new StringBuffer();
         try {
             StringReader reader = new StringReader(xml);
@@ -32,7 +33,6 @@ public class XmlStripper {
             processElement(element, textBuffer);
         } catch (JDOMException e) {
             e.printStackTrace();
-            System.out.println(xml);
         }
         return textBuffer.toString();
     }
@@ -42,17 +42,20 @@ public class XmlStripper {
      * @param element Element.
      * @param textBuffer StringBuffer.
      */
-    private void processElement(Element element, StringBuffer textBuffer) {
+    private static void processElement(Element element,
+            StringBuffer textBuffer) {
         //  Extract Text
-        String text = element.getTextNormalize();
-        textBuffer.append(text + " ");
+        String text = element.getTextTrim();
+        if (text.length() > 0) {
+            appendText(textBuffer, text);
+        }
 
         //  Extract Attribute Values
         List attributes = element.getAttributes();
         for (int i = 0; i < attributes.size(); i++) {
             Attribute attribute = (Attribute) attributes.get(i);
             String value = attribute.getValue();
-            textBuffer.append(value + " ");
+            appendText(textBuffer, value);
         }
 
         //  Recursively process all children.
@@ -61,5 +64,12 @@ public class XmlStripper {
             Element child = (Element) children.get(i);
             processElement(child, textBuffer);
         }
+    }
+
+    private static void appendText(StringBuffer textBuffer, String text) {
+        if (textBuffer.length() > 0) {
+            textBuffer.append(" ");
+        }
+        textBuffer.append(text);
     }
 }
