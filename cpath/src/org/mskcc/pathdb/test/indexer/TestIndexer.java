22299858af151f9012cf9422a8a207d4afc3470c
cerami <cerami@code.google.com>
2004-04-29 19:02:13 +0000

    Changed generic cpath_id to interaction_id

--- cpath/src/org/mskcc/pathdb/test/indexer/TestIndexer.java
+++ cpath/src/org/mskcc/pathdb/test/indexer/TestIndexer.java
@@ -58,7 +58,7 @@ public class TestIndexer extends TestCase {
         Hits hits = lucene.executeQuery(terms);
         assertEquals(1, hits.length());
         Document doc = hits.doc(0);
-        Field id = doc.getField(LuceneIndexer.FIELD_CPATH_ID);
+        Field id = doc.getField(LuceneIndexer.FIELD_INTERACTION_ID);
         assertEquals("4", id.stringValue());
     }
 
