31d524c55b65d6746a1d41d1dc2b8edbeb473a75
cerami <cerami@code.google.com>
2004-05-06 16:53:54 +0000

    Redone completely for left nav column

--- cpath/src/org/mskcc/pathdb/taglib/InteractorTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/InteractorTable.java
@@ -1,33 +1,26 @@
 package org.mskcc.pathdb.taglib;
 
-import org.mskcc.pathdb.sql.assembly.XmlAssembly;
-import org.mskcc.pathdb.sql.dao.DaoException;
-import org.mskcc.pathdb.sql.query.QueryException;
-import org.mskcc.pathdb.controller.ProtocolRequest;
+import org.mskcc.dataservices.schemas.psi.NamesType;
+import org.mskcc.dataservices.schemas.psi.ProteinInteractorType;
 import org.mskcc.pathdb.controller.ProtocolConstants;
+import org.mskcc.pathdb.controller.ProtocolRequest;
 import org.mskcc.pathdb.lucene.LuceneIndexer;
-import org.mskcc.dataservices.schemas.psi.*;
-import org.mskcc.dataservices.mapper.MapperException;
-import org.apache.lucene.search.Hits;
+import org.mskcc.pathdb.model.ProteinWithWeight;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
 
 public class InteractorTable extends HtmlTable {
     private ProtocolRequest protocolRequest;
-    private EntrySet entrySet;
-    private HashMap interactors;
+    private List interactorList;
 
     /**
-     * Sets Interaction Parameter.
+     * Sets Interactor Set
      *
-     * @param xmlAssembly XmlAssembly Object
+     * @param interactorList Interactor List
      */
-    public void setXmlAssembly(XmlAssembly xmlAssembly) {
-        if (!xmlAssembly.isEmpty()) {
-            entrySet = (EntrySet) xmlAssembly.getXmlObject();
-        } else {
-            entrySet = null;
-        }
+    public void setInteractorList(List interactorList) {
+        this.interactorList = interactorList;
     }
 
     /**
@@ -45,13 +38,9 @@ public class InteractorTable extends HtmlTable {
      * @throws org.mskcc.pathdb.sql.dao.DaoException
      *          Database Access Error.
      */
-    protected void subDoStartTag() throws DaoException, MapperException,
-            QueryException {
-        interactors = new HashMap();
+    protected void subDoStartTag() throws Exception {
         protocolRequest.setFormat(ProtocolConstants.FORMAT_PSI);
-        extractEntySet();
-
-        if (interactors.size() > 0) {
+        if (interactorList != null && interactorList.size() > 1) {
             append("<div id=\"proteinview\" class=\"toolgroup\">\n" +
                     "<div class=\"label\">\n" +
                     "<strong>Protein View</strong>\n" +
@@ -63,86 +52,26 @@ public class InteractorTable extends HtmlTable {
         }
     }
 
+    /**
+     * Outputs All Interactors.
+     */
     private void outputInteractors() {
-        ArrayList list = new ArrayList(interactors.values());
-        Collections.sort(list, new ProteinWithHits());
-        Collections.reverse(list);
-        for (int i = 0; i < list.size(); i++) {
-            ProteinWithHits proteinWithHits =
-                    (ProteinWithHits) list.get(i);
-            ProteinInteractorType protein = proteinWithHits.getProtein();
+        Iterator iterator = interactorList.iterator();
+        while (iterator.hasNext()) {
+            ProteinWithWeight proteinWithWeight = (ProteinWithWeight)
+                iterator.next();
+            ProteinInteractorType protein = proteinWithWeight.getProtein();
             NamesType names = protein.getNames();
+            String proteinId = protein.getId();
             append("<div>");
-            String id = protein.getId();
             String link = getInteractionLink(LuceneIndexer.FIELD_INTERACTOR_ID
-                    + ":" + id, ProtocolConstants.FORMAT_HTML);
+                    + ":" + proteinId, ProtocolConstants.FORMAT_HTML);
             String name = names.getFullName().trim();
-            if (name.length() > 50) {
-                name = name.substring(0, 50) + "...";
+            if (name.indexOf(" ") > 25) {
+                name = name.substring(0, 25) + "...";
             }
-            append("<A HREF='" + link + "'>" + name + "</A> [" +
-                    proteinWithHits.getNumHits() + "]");
+            append("<A HREF='" + link + "'>" + name + "</A>");
             append("</div>");
         }
-    }
-
-    private void extractEntySet() throws QueryException {
-        if (entrySet != null) {
-            for (int i = 0; i < entrySet.getEntryCount(); i++) {
-                Entry entry = entrySet.getEntry(i);
-                InteractorList interactorList = entry.getInteractorList();
-                extractInteractors(interactorList);
-            }
-        }
-    }
-
-    /**
-     * Extracts All Interactors and Places in Global HashMap.
-     *
-     * @param interactorList List of Interactors
-     */
-    private void extractInteractors(InteractorList interactorList)
-            throws QueryException {
-        LuceneIndexer indexer = new LuceneIndexer();
-        for (int i = 0; i < interactorList.getProteinInteractorCount(); i++) {
-            ProteinInteractorType protein =
-                    interactorList.getProteinInteractor(i);
-            String id = protein.getId();
-            Hits hits = indexer.executeQuery(LuceneIndexer.FIELD_INTERACTOR_ID
-                    + ":" + id);
-            ProteinWithHits proteinWithHits = new ProteinWithHits(protein,
-                    hits.length());
-            interactors.put(id, proteinWithHits);
-        }
-    }
-}
-
-class ProteinWithHits implements Comparator {
-    private int numHits;
-    private ProteinInteractorType protein;
-
-    public ProteinWithHits() {
-
-    }
-
-    public ProteinWithHits(ProteinInteractorType protein, int numHits) {
-        this.protein = protein;
-        this.numHits = numHits;
-    }
-
-    public int getNumHits() {
-        return numHits;
-    }
-
-    public ProteinInteractorType getProtein() {
-        return protein;
-    }
-
-    public int compare(Object object1, Object object2) {
-        ProteinWithHits protein1 = (ProteinWithHits) object1;
-        ProteinWithHits protein2 = (ProteinWithHits) object2;
-        Integer numHits1 = new Integer(protein1.getNumHits());
-        Integer numHits2 = new Integer(protein2.getNumHits());
-        return numHits1.compareTo(numHits2);
     }
 }
