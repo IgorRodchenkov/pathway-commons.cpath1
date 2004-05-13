80813f5d40058152df1746917078070c743b98bd
cerami <cerami@code.google.com>
2004-05-13 20:53:49 +0000

    Updated to use the DaoXmlCache Object

--- cpath/src/org/mskcc/pathdb/taglib/CacheTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/CacheTable.java
@@ -1,12 +1,17 @@
 package org.mskcc.pathdb.taglib;
 
-import org.mskcc.dataservices.cache.CacheManager;
-import org.mskcc.dataservices.cache.Cacheable;
+import org.mskcc.pathdb.model.XmlCacheRecord;
+import org.mskcc.pathdb.protocol.ProtocolRequest;
+import org.mskcc.pathdb.sql.dao.DaoException;
+import org.mskcc.pathdb.sql.dao.DaoXmlCache;
+import org.mskcc.pathdb.util.XssFilter;
+import org.mskcc.pathdb.xdebug.XDebug;
 
-import java.util.Date;
+import javax.servlet.http.HttpUtils;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.Set;
 
 /**
  * Custom JSP Tag for Displaying the Cache Contents.
@@ -21,8 +26,8 @@ public class CacheTable extends HtmlTable {
      * @throws Exception Exception in writing to JspWriter.
      */
     public void subDoStartTag() throws Exception {
-        String headers[] = {"Key", "Expiration Time"};
-        createHeader("Cache Contents");
+        String headers[] = {"#", "URL", "Query", "Key", "Last Used"};
+        createHeader("XML Cache Contents");
         startTable();
         createTableHeaders(headers);
         outputRecords();
@@ -32,23 +37,39 @@ public class CacheTable extends HtmlTable {
     /**
      * Output Cached Records.
      */
-    private void outputRecords() {
-        CacheManager cacheManager = CacheManager.getInstance();
-        HashMap map = cacheManager.getEntireCache();
-        if (map != null) {
-            Set set = map.keySet();
-            if (set != null) {
-                Iterator keys = set.iterator();
-                while (keys.hasNext()) {
-                    append("<TR>");
-                    String key = (String) keys.next();
-                    outputDataField(key);
-                    Cacheable object = (Cacheable) map.get(key);
-                    Date expirationTime = object.getExpirationTime();
-                    outputDataField(expirationTime.toString());
-                    append("</TR>");
-                }
+    private void outputRecords() throws DaoException {
+        DaoXmlCache dao = new DaoXmlCache(new XDebug());
+        ArrayList records = dao.getAllRecords();
+        if (records.size() == 0) {
+            startRow();
+            append("<TD COLSPAN=5>No Records in Cache</TD>");
+            endRow();
+        } else {
+            for (int i = 0; i < records.size(); i++) {
+                XmlCacheRecord record = (XmlCacheRecord) records.get(i);
+                startRow(i);
+                outputDataField(Integer.toString(i));
+
+                outputProtocolRequest(record);
+                outputDataField(record.getMd5());
+                outputDataField(record.getLastUsed());
+                endRow();
             }
         }
+    }
+
+    private void outputProtocolRequest(XmlCacheRecord record) {
+        Hashtable params1 = HttpUtils.parseQueryString(record.getUrl());
+        HashMap params2 = XssFilter.filterAllParameters(params1);
+        ProtocolRequest request = new ProtocolRequest(params2);
+        outputDataField("<A HREF='" + request.getUri() + "'>URL Link</A>");
+        append("<TD><UL>");
+        Iterator keys = params2.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            Object value = params2.get(key);
+            append("<LI>" + key + ":  " + value);
+        }
+        append("</UL></TD>");
     }
 }
