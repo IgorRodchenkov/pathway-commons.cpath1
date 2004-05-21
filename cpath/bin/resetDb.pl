6db7d63f2e2792d2644f3d48c8609f19e33904e2
cerami <cerami@code.google.com>
2004-05-21 14:40:16 +0000

    Fixed Typo

--- cpath/bin/resetDb.pl
+++ cpath/bin/resetDb.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-print "Reseting Database\n";
+print "Resetting Database\n";
 chdir ("../dbData") || die "Cannot cd to dbData directory";
 system "mysql -u tomcat --password=kitty < reset.sql";
 print "Done.  Go!  Go!  Go!\n";
