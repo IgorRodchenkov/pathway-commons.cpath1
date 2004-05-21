57a3111b8a6fef9876ec109cc5587870b6f5d95f
cerami <cerami@code.google.com>
2004-05-21 14:51:58 +0000

    Removed old Grid constants;  added cPath Version Number constant

--- cpath/src/org/mskcc/pathdb/util/CPathConstants.java
+++ cpath/src/org/mskcc/pathdb/util/CPathConstants.java
@@ -8,27 +8,7 @@ package org.mskcc.pathdb.util;
 public class CPathConstants {
 
     /**
-     * Service ID: READ_INTERACTIONS_FROM_GRID
+     * Current cPath Version Number.
      */
-    public static final String READ_INTERACTIONS_FROM_GRID
-            = "READ_INTERACTIONS_FROM_GRID";
-
-    /**
-     * Service ID: READ_INTERACTORS_FROM_GRID
-     */
-    public static final String READ_INTERACTORS_FROM_GRID
-            = "READ_INTERACTORS_FROM_GRID";
-
-    /**
-     * Service ID: WRITE_INTERACTIONS_TO_GRID
-     */
-    public static final String WRITE_INTERACTIONS_TO_GRID
-            = "WRITE_INTERACTIONS_TO_GRID";
-
-    /**
-     * Service ID: WRITE_INTERACTORS_TO_GRID
-     */
-    public static final String WRITE_INTERACTORS_TO_GRID
-            = "WRITE_INTERACTORS_TO_GRID";
-
+    public static final String VERSION = "0.3 (Beta)";
 }
