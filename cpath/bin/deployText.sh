a572816a7a1c36d4d42eb218b555f1b702240d78
cerami <cerami@code.google.com>
2004-05-27 16:45:04 +0000

    Updated to reflect new location of textIndex

--- cpath/bin/deployText.sh
+++ cpath/bin/deployText.sh
@@ -3,7 +3,7 @@
 #-------------------------------------------------------------------------------
 
 echo Copying Full Text Index to Tomcat
-cp -R /home/cerami/dev/sander/cpath/textIndex/* /var/lib/tomcat4/webapps/textIndex
+cp -R /home/cerami/dev/sander/cpath/build/WEB-INF/textIndex/* /var/lib/tomcat4/webapps/cpath/WEB-INF/textIndex
 # Set Sticky Bit so that Tomcat can write to this directory
-chmod 1777 /var/lib/tomcat4/webapps/textIndex 
+chmod 1777 /var/lib/tomcat4/webapps/cpath/WEB-INF/textIndex 
 echo Done.
