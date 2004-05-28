72bf92e54ebd6d01a564b0443e1f1ad696517b57
cerami <cerami@code.google.com>
2004-05-28 14:41:22 +0000

    Moved to perl script

--- cpath/bin/deployText.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#-------------------------------------------------------------------------------
-
-echo Copying Full Text Index to Tomcat
-cp -R /home/cerami/dev/sander/cpath/build/WEB-INF/textIndex/* /var/lib/tomcat4/webapps/cpath/WEB-INF/textIndex
-# Set Sticky Bit so that Tomcat can write to this directory
-chmod 1777 /var/lib/tomcat4/webapps/cpath/WEB-INF/textIndex 
-echo Done.
