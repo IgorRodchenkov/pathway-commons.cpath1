31e43f001fff76bced7058865f2206a989ea9bb8
cerami <cerami@code.google.com>
2004-05-28 21:59:21 +0000

    Reverted back

--- cpath/src/org/mskcc/pathdb/taglib/InteractionTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/InteractionTable.java
@@ -91,11 +91,7 @@ public class InteractionTable extends HtmlTable {
      * @param interactorList Interactor List
      */
     public void setInteractorList(List interactorList) {
-        if (interactorList.size() == 1
-                && protocolRequest != null
-                && protocolRequest.getQuery() != null
-                && protocolRequest.getQuery().indexOf
-                (LuceneConfig.FIELD_INTERACTOR_ID)>=-1) {
+        if (interactorList.size() == 1) {
             ProteinWithWeight proteinWithWeight = (ProteinWithWeight)
                     interactorList.get(0);
             targetProtein = proteinWithWeight.getProtein();
