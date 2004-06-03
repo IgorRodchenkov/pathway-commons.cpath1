7c92c24095ac08f7349915d2d7d9cd9596c2e6c4
cerami <cerami@code.google.com>
2004-06-03 22:01:48 +0000

    Constructor now takes a File object

--- cpath/src/org/mskcc/pathdb/tool/LoadExternalReferences.java
+++ cpath/src/org/mskcc/pathdb/tool/LoadExternalReferences.java
@@ -5,6 +5,7 @@ import org.mskcc.pathdb.task.ImportReferencesTask;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
 
 /**
  * Loads External References from text file.
@@ -16,12 +17,12 @@ public class LoadExternalReferences {
     /**
      * Loads External References from Specified File.
      *
-     * @param fileName File Name.
+     * @param file File.
      * @throws IOException  File Input Error.
      * @throws DaoException Data Access Error.
      */
-    public void load(String fileName) throws DaoException, IOException {
-        FileReader reader = new FileReader(fileName);
+    public void load(File file) throws DaoException, IOException {
+        FileReader reader = new FileReader(file);
         ImportReferencesTask task =
                 new ImportReferencesTask(true, reader);
         task.importReferences();
