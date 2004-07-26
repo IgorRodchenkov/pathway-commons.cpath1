d4a829166e33b25edcef75c148e2480e296c9c6d
cerami <cerami@code.google.com>
2004-07-26 16:50:22 +0000

    Reformatted license

--- cpath/src/org/mskcc/pathdb/taglib/DiagnosticsTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/DiagnosticsTable.java
@@ -7,7 +7,7 @@
  ** under the terms of the GNU Lesser General Public License as published
  ** by the Free Software Foundation; either version 2.1 of the License, or
  ** any later version.
- ** 
+ **
  ** This library is distributed in the hope that it will be useful, but
  ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
  ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
@@ -22,7 +22,7 @@
  ** Memorial Sloan-Kettering Cancer Center 
  ** has been advised of the possibility of such damage.  See
  ** the GNU Lesser General Public License for more details.
- ** 
+ **
  ** You should have received a copy of the GNU Lesser General Public License
  ** along with this library; if not, write to the Free Software Foundation,
  ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
