20ce03f9beb09c05a6f196f1df9e9b3723388144
cerami <cerami@code.google.com>
2004-07-30 19:42:55 +0000

    Added support for ant eater

--- cpath/bin/dev_site.pl
+++ cpath/bin/dev_site.pl
@@ -17,6 +17,9 @@ chdir "sander/cpath" or die;
 # Run Prepare_dev_site target
 system "ant prepare_dev_site";
 
+# Run Ant Eater Functional Tests
+system "anteater -f ant_eater/tests.xml";
+
 # Run Maven
 system "maven site:generate";
 system "maven pdf";
@@ -26,3 +29,9 @@ system "maven site:fsdeploy";
 system "tar -cvf cpath.tar *";
 system "gzip cpath.tar";
 system "cp cpath.tar.gz /var/www/dev_site/cpath";
+
+# Move Ant Eater Reports
+chdir "/var/www/dev_site/cpath/reports" or die;
+system "rm -rf *";
+chdir "/home/cerami/temp/sander/cpath";
+system "mv ant_eater/reports/* /var/www/dev_site/cpath/reports/";
