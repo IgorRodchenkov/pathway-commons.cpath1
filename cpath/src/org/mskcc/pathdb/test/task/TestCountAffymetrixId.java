5837b018baf03b76a74c543cab071d8ee375c5d5
cerami <cerami@code.google.com>
2004-09-03 14:53:57 +0000

    Added TestCountAffymetrix ID

--- cpath/src/org/mskcc/pathdb/test/task/TestCountAffymetrixId.java
+++ cpath/src/org/mskcc/pathdb/test/task/TestCountAffymetrixId.java
@@ -12,14 +12,14 @@
  ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
  ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
  ** documentation provided hereunder is on an "as is" basis, and
- ** Memorial Sloan-Kettering Cancer Center 
+ ** Memorial Sloan-Kettering Cancer Center
  ** has no obligations to provide maintenance, support,
  ** updates, enhancements or modifications.  In no event shall
  ** Memorial Sloan-Kettering Cancer Center
  ** be liable to any party for direct, indirect, special,
  ** incidental or consequential damages, including lost profits, arising
  ** out of the use of this software and its documentation, even if
- ** Memorial Sloan-Kettering Cancer Center 
+ ** Memorial Sloan-Kettering Cancer Center
  ** has been advised of the possibility of such damage.  See
  ** the GNU Lesser General Public License for more details.
  **
@@ -29,27 +29,36 @@
  **/
 package org.mskcc.pathdb.test.task;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.mskcc.dataservices.bio.ExternalReference;
+import org.mskcc.pathdb.sql.transfer.UpdatePsiInteractor;
+import org.mskcc.pathdb.task.CountAffymetrixIdsTask;
 
-/**
- * Suite of all Utility Unit Tests.
- *
- * @author Ethan Cerami
- */
-public class TaskSuite extends TestCase {
+public class TestCountAffymetrixId extends TestCase {
 
-    /**
-     * The suite method runs all the tests.
-     *
-     * @return Suite of JUnit tests.
-     */
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTestSuite(TestAffymetrixParser.class);
-        suite.addTestSuite(TestCountAffymetrixId.class);
-        suite.setName("Task Tests");
-        return suite;
+    public void test() throws Exception {
+        CountAffymetrixIdsTask task = new CountAffymetrixIdsTask(562, false);
+        int numRecords = task.getTotalNumRecords();
+        int withAffyIds = task.getNumRecordsWithAffymetrixIds();
+
+        //  Should be 1 Record, with 0 Affy Ids.
+        assertEquals(1, numRecords);
+        assertEquals(0, withAffyIds);
+
+        //  Now add an Fake AffyId to existing interactor
+        ExternalReference existingRef = new ExternalReference("SwissProt",
+                "P06139");
+        ExternalReference newRef = new ExternalReference
+                ("Affymetrix", "a100_at");
+        UpdatePsiInteractor updater = new UpdatePsiInteractor
+                (existingRef, newRef, false);
+        updater.doUpdate();
+
+        //  Shoulw now be 1 Record, with 1 Affy Id.
+        task = new CountAffymetrixIdsTask(562, false);
+        numRecords = task.getTotalNumRecords();
+        withAffyIds = task.getNumRecordsWithAffymetrixIds();
+        assertEquals(1, numRecords);
+        assertEquals(1, withAffyIds);
     }
 }
