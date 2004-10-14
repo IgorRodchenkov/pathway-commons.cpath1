4f04784643d17207b431f99a6fe592f6e99cb5c7
cerami <cerami@code.google.com>
2004-10-14 14:22:55 +0000

    Moved ant eater tests until end;  fixes Windows error reported by iain.d.keddie@gsk.com

--- cpath/bin/dev_site.pl
+++ cpath/bin/dev_site.pl
@@ -17,19 +17,21 @@ chdir "sander/cpath" or die;
 # Run Prepare_dev_site target
 system "ant prepare_dev_site";
 
-# Run Ant Eater Functional Tests
-system "anteater -f ant_eater/tests.xml";
+# Create source distribition
+system "ant war";
+system "tar -cvf cpath.tar *";
+system "gzip cpath.tar";
+system "cp cpath.tar.gz /var/www/dev_site/cpath";
 
 # Run Maven
 system "maven site:generate";
 system "maven pdf";
 system "maven site:fsdeploy";
 
-# Create source distribition
-system "ant war";
-system "tar -cvf cpath.tar *";
-system "gzip cpath.tar";
-system "cp cpath.tar.gz /var/www/dev_site/cpath";
+# Run Ant Eater Functional Tests
+# Run Ant Eater after source distribution is complete
+# There is no need to bundle the AntEater results
+system "anteater -f ant_eater/tests.xml";
 
 # Move Ant Eater Reports
 chdir "/var/www/dev_site/cpath/reports" or die;
