23e3c65457c906a057caa915053890a08a4a91c4
cerami <cerami@code.google.com>
2004-11-01 17:59:31 +0000

    Updating to prompt for passwords

--- cpath/bin/production.pl
+++ cpath/bin/production.pl
@@ -6,22 +6,36 @@ use strict;
 
 my $tom = "/var/lib/tomcat4/webapps/cpath";
 
+# Update Passwords with User Input
+print "Enter password for MySQL tomcat user:  ";
+chomp (my $mysql_password = <STDIN>);
+
+print "Enter password for cPath Administrator:  ";
+chomp (my $admin_password = <STDIN>);
+
 # CD to the cpath tomcat dir
-chdir "$tom" or die;
+#chdir "$tom" or die;
 
 # Delete everything within cpath
-system "rm -rf *";
+#system "rm -rf *";
 
 # Update All Code
-chdir "/home/cerami/dev/sander/cpath";
-system "cvs update -d";
+chdir "/home/cerami/dev/sander/cpath" or die;
+#system "cvs update -d";
 
 # Create War File
-system "ant war";
+#system "ant war";
 
 # Copy War File over
 print "Copying war file to: $tom\n";
-system "cp dist/cpath.war $tom";
+#system "cp dist/cpath.war $tom";
+
+# Update web.xml file with correct passwords
+chdir "web/WEB-INF" or die;
+system "sed 's/kitty/$mysql_password/' web.xml";
+#system "mv web_new.xml web.xml";
+
+die;
 
 # Expand War File, and set permissions on textIndex dir
 chdir "$tom" or die;
