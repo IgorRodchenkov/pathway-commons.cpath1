033c6b085de866f6fd34f3e3c2b5f4f0167ea2da
bader <bader@code.google.com>
2005-02-01 21:42:20 +0000

    Adding Iain Keddie's contributed env.pl file that adds Windows compatibility for the admin scripts.

--- cpath/bin/env.pl
+++ cpath/bin/env.pl
@@ -1,6 +1,15 @@
 #  Set up Environment for Running cPath Java Tools
 
 $cpathHome = $ENV{CPATH_HOME};
+my $osCheck = $ENV{OS};
+my $pathDelim;
+
+if( $osCheck =~ /win/i){
+    $pathDelim=";";
+}else{
+    $pathDelim=":";
+}
+
 if ($cpathHome eq "") {
 	die "CPATH_HOME Environment Variable is not set.  Please set, and try again.\n";
 }
@@ -9,7 +18,7 @@ if ($cpathHome eq "") {
 $cp="$cpathHome/build/WEB-INF/classes";
 @jar_files = glob ("$cpathHome/lib/*.jar");
 foreach my $jar (@jar_files) {
-	$cp="$cp:$jar"
+	$cp="$cp$pathDelim$jar"
 }
 
 return 1;
