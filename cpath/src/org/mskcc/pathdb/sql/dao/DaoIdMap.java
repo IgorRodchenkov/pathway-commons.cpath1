87fd731cb14ba3ffebe6401de9a2289ff252eb23
cerami <cerami@code.google.com>
2005-02-11 19:56:27 +0000

    Optimized lookups by using a hash code

--- cpath/src/org/mskcc/pathdb/sql/dao/DaoIdMap.java
+++ cpath/src/org/mskcc/pathdb/sql/dao/DaoIdMap.java
@@ -60,39 +60,62 @@ public class DaoIdMap {
      * that the record already exists and was not saved, or an error occurred.
      *
      * @param record IdMapRecord Object.
+     * @param validateRecord Validates DB1 and DB2 to ensure that these
+     *        actually exist in the database.  When set to true, full validation
+     *        check is run.  When set to false, no validation check is run.
+     *        If code has already validaed DB1 and DB2 prior to this call,
+     *        setting this parameter to false results in much faster execution.
      * @return true if saved successfully.
      * @throws DaoException Error Saving Data.
      */
-    public boolean addRecord(IdMapRecord record) throws DaoException {
+    public boolean addRecord(IdMapRecord record, boolean validateRecord)
+            throws DaoException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
         //  Validate the Incoming Database Ids
-        DaoExternalDb dao = new DaoExternalDb();
-        ExternalDatabaseRecord dbRecord1 = dao.getRecordById(record.getDb1());
-        if (dbRecord1 == null) {
-            throw new IllegalArgumentException("External Database, DB1:  "
-                    + record.getDb1() + " does not exist in database.");
+        if (validateRecord) {
+            DaoExternalDb dao = new DaoExternalDb();
+            ExternalDatabaseRecord dbRecord1 = dao.getRecordById
+                    (record.getDb1());
+            if (dbRecord1 == null) {
+                throw new IllegalArgumentException
+                        ("External Database, DB1:  "
+                        + record.getDb1() + " does not exist in database.");
+            }
+            ExternalDatabaseRecord dbRecord2 = dao.getRecordById
+                    (record.getDb1());
+            if (dbRecord2 == null) {
+                throw new IllegalArgumentException("External Database, DB2:  "
+                        + record.getDb2() + " does not exist in database.");
+            }
         }
-        ExternalDatabaseRecord dbRecord2 = dao.getRecordById(record.getDb1());
-        if (dbRecord2 == null) {
-            throw new IllegalArgumentException("External Database, DB2:  "
-                    + record.getDb2() + " does not exist in database.");
+
+        //  Validate the Incoming Ids
+        String id1 = record.getId1();
+        String id2 = record.getId2();
+        if (id1 == null || id1.trim().length() ==  0) {
+            throw new IllegalArgumentException ("ID1 is null or empty");
+        }
+        if (id2 == null || id2.trim().length() == 0) {
+            throw new IllegalArgumentException ("ID2 is null or empty");
         }
 
         //  Validate that the record does not already exist in the database
         IdMapRecord dbRecord = getRecord(record);
         if (dbRecord == null) {
             try {
+                //  Note that we store a hash code to enable very fast lookups.
                 con = JdbcUtil.getCPathConnection();
                 pstmt = con.prepareStatement
                         ("INSERT INTO id_map (`DB_1`, `ID_1`, "
-                        + "`DB_2`, `ID_2`) VALUES (?,?,?,?)");
+                        + "`DB_2`, `ID_2`, `HASH_CODE`) VALUES (?,?,?,?,?)");
                 pstmt.setInt(1, record.getDb1());
                 pstmt.setString(2, record.getId1().trim());
                 pstmt.setInt(3, record.getDb2());
                 pstmt.setString(4, record.getId2().trim());
+                pstmt.setInt(5, record.hashCode());
                 int rows = pstmt.executeUpdate();
                 return (rows > 0) ? true : false;
             } catch (ClassNotFoundException e) {
@@ -184,17 +207,7 @@ public class DaoIdMap {
      * @throws DaoException Error Retrieving Data.
      */
     public IdMapRecord getRecord(IdMapRecord idRecord) throws DaoException {
-        IdMapRecord option1 = getRecord(idRecord.getDb1(), idRecord.getId1(),
-                idRecord.getDb2(), idRecord.getId2());
-        IdMapRecord option2 = getRecord(idRecord.getDb2(),
-                idRecord.getId2(), idRecord.getDb1(), idRecord.getId1());
-        if (option1 != null) {
-            return option1;
-        } else if (option2 != null) {
-            return option2;
-        } else {
-            return null;
-        }
+        return getRecord(idRecord.hashCode());
     }
 
     /**
@@ -299,12 +312,16 @@ public class DaoIdMap {
             rs = pstmt.executeQuery();
             while (rs.next()) {
                 IdMapRecord idRecord = createBean(rs);
+//                System.out.println("Running query:  "+ xref.toString()
+//                    + " Gets:  " + idRecord.toString());
                 CPathXRef neighbor = null;
                 if (idRecord.getDb1() == xref.getDbId()
                         && idRecord.getId1().equals(xref.getLinkedToId())) {
-                    neighbor = new CPathXRef (idRecord.getDb2(), idRecord.getId2());
+                    neighbor = new CPathXRef (idRecord.getDb2(),
+                            idRecord.getId2());
                 } else {
-                    neighbor = new CPathXRef(idRecord.getDb1(), idRecord.getId1());
+                    neighbor = new CPathXRef(idRecord.getDb1(),
+                            idRecord.getId1());
                 }
                 neighborList.add(neighbor);
             }
@@ -319,29 +336,22 @@ public class DaoIdMap {
     }
 
     /**
-     * Gets Record which matches the specified criteria exactly.
+     * Gets Record which the specified HashCode.  Using the hash code
+     * enables very quick lookups.
      *
-     * @param db1 Foreign Key to External Database.
-     * @param id1 ID String
-     * @param db2 Foreign Key to External Database.
-     * @param id2 ID String
+     * @param hashCode Integer HashCode.
      * @return IdMapRecord, if the record exists;  otherwise, null.
      * @throws DaoException Error Retrieving Data.
      */
-    private IdMapRecord getRecord(int db1, String id1, int db2,
-            String id2) throws DaoException {
+    private IdMapRecord getRecord(int hashCode) throws DaoException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         try {
             con = JdbcUtil.getCPathConnection();
             pstmt = con.prepareStatement
-                    ("SELECT * FROM id_map WHERE DB_1 = ? AND ID_1 = ? "
-                    + "AND DB_2 = ? AND ID_2 = ?");
-            pstmt.setInt(1, db1);
-            pstmt.setString(2, id1);
-            pstmt.setInt(3, db2);
-            pstmt.setString(4, id2);
+                    ("SELECT * FROM id_map WHERE HASH_CODE = ?");
+            pstmt.setInt(1, hashCode);
             rs = pstmt.executeQuery();
             if (rs.next()) {
                 return createBean(rs);
