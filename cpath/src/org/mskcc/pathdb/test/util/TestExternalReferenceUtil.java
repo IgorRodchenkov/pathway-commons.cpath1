eb61e5b770f3a24850c8cec3d49d48e921f67738
cerami <cerami@code.google.com>
2005-03-15 21:01:37 +0000

    Added tests for new utility methods

--- cpath/src/org/mskcc/pathdb/test/util/TestExternalReferenceUtil.java
+++ cpath/src/org/mskcc/pathdb/test/util/TestExternalReferenceUtil.java
@@ -32,6 +32,7 @@ package org.mskcc.pathdb.test.util;
 import junit.framework.TestCase;
 import org.mskcc.dataservices.bio.ExternalReference;
 import org.mskcc.pathdb.util.ExternalReferenceUtil;
+import org.mskcc.pathdb.model.ReferenceType;
 
 /**
  * Tests the ExternalReferenceUtil Class.
@@ -41,25 +42,58 @@ import org.mskcc.pathdb.util.ExternalReferenceUtil;
 public class TestExternalReferenceUtil extends TestCase {
 
     /**
-     * Tests the Filter OutNonIdReferences() Method.
+     * Tests the Filter Method.
      *
      * @throws Exception All Exceptions.
      */
-    public void testFilterOutNonIdReferences() throws Exception {
+    public void testFilterReferences() throws Exception {
         ExternalReference refs[] = new ExternalReference[7];
         refs[0] = new ExternalReference("SwissProt", "P25300");
         refs[1] = new ExternalReference("GO", "ABCD");
-        refs[2] = new ExternalReference("InterPro", "ABCD");
-        refs[3] = new ExternalReference("PubMed", "ABCD");
+        refs[2] = new ExternalReference("InterPro", "InterPro");
+        refs[3] = new ExternalReference("InterPro", "ABCD");
         refs[4] = new ExternalReference("PDB", "ABCD");
         refs[5] = new ExternalReference("Unigene", "ABCD");
         refs[6] = new ExternalReference("REF_SEQ PROTEIN", "ABCD");
         ExternalReference filteredRefs[] =
-                ExternalReferenceUtil.extractProteinUnificationRefs(refs);
+                ExternalReferenceUtil.filterByReferenceType(refs,
+                        ReferenceType.PROTEIN_UNIFICATION);
         //  Before filtering, we have 7 references.
         assertEquals(7, refs.length);
 
         //  After filtering, we have 2 references.
         assertEquals(2, filteredRefs.length);
+    }
+
+    /**
+     * Tests the createUnifiedList method.
+     */
+    public void testUnionMethod() {
+        ExternalReference refs1[] = new ExternalReference[2];
+        refs1[0] = new ExternalReference("SwissProt", "P25300");
+        refs1[1] = new ExternalReference("GO", "ABCD");
+
+        ExternalReference refs2[] = new ExternalReference[2];
+        refs2[0] = new ExternalReference("InterPro", "ABCD");
+        refs2[1] = new ExternalReference("UniGene", "ABCD");
+
+        ExternalReference union[] =
+                ExternalReferenceUtil.createUnifiedList(refs1, refs2);
+        assertEquals (4, union.length);
+        ExternalReference ref4 = union[3];
+        assertEquals ("UniGene", ref4.getDatabase());
+    }
+
+    /**
+     * Tests the RemoveDuplicates Method.
+     */
+    public void testRemoveDuplicates() {
+        ExternalReference refs[] = new ExternalReference[4];
+        refs[0] = new ExternalReference("SwissProt", "P25300");
+        refs[1] = new ExternalReference("GO", "ABCD");
+        refs[2] = new ExternalReference("SwissProt", "P25300");
+        refs[3] = new ExternalReference("SwissProt", "ABCD");
+        refs = ExternalReferenceUtil.removeDuplicates(refs);
+        assertEquals (3, refs.length);
     }
 }
