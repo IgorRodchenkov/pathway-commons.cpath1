a444f06d337b2ce88df1c9febb4028d0627d0969
cerami <cerami@code.google.com>
2005-04-22 13:54:13 +0000

    Changed text from 'Number of Interactions' to 'Number of Pathways/Interactions'

--- cpath/src/org/mskcc/pathdb/taglib/OrganismTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/OrganismTable.java
@@ -103,8 +103,8 @@ public class OrganismTable extends HtmlTable {
         startTable();
         startRow();
         createColumnHeader("Species Name", SORT_BY_NAME, sortBy, sortOrder);
-        createColumnHeader("Number of Interactions", SORT_BY_NUM_INTERACTIONS,
-                sortBy, sortOrder);
+        createColumnHeader("Number of Pathways/Interactions",
+                SORT_BY_NUM_INTERACTIONS, sortBy, sortOrder);
         outputRecords(sortBy, sortOrder);
         endTable();
     }
