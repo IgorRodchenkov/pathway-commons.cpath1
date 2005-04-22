117e047a1787b8f436d68a606023f6d3485b163f
cerami <cerami@code.google.com>
2005-04-22 19:49:20 +0000

    Updated version #

--- cpath/src/org/mskcc/pathdb/util/CPathConstants.java
+++ cpath/src/org/mskcc/pathdb/util/CPathConstants.java
@@ -39,7 +39,7 @@ public class CPathConstants {
     /**
      * Current cPath Version Number.
      */
-    public static final String VERSION = "0.3.2 (Beta)";
+    public static final String VERSION = "0.4 (Beta)";
 
     /**
      * Property Name for PSI_SCHEMA_LOCATION
