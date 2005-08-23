6213db7680ec879aece8636688b495a585206c2e
cerami <cerami@code.google.com>
2005-08-23 19:41:30 +0000

    Added support for creating new unification xrefs

--- cpath/src/org/mskcc/pathdb/schemas/biopax/BioPaxGenerator.java
+++ cpath/src/org/mskcc/pathdb/schemas/biopax/BioPaxGenerator.java
@@ -52,36 +52,22 @@ public class BioPaxGenerator {
      */
     public static Element generateRelationshipXref(ExternalReference ref,
             Namespace bioPaxNamespace) throws DaoException {
+        return generateXref (ref, bioPaxNamespace,
+                BioPaxConstants.XREF_RELATIONSHIP);
+    }
 
-        //  Create Element
-        Element xrefElement = new Element("XREF", bioPaxNamespace);
-        Element relationshipElement = new Element("relationshipXref",
-                bioPaxNamespace);
-        Element commentElement = new Element("COMMENT", bioPaxNamespace);
-        Element dbElement = new Element("DB", bioPaxNamespace);
-        Element idElement = new Element("ID", bioPaxNamespace);
-
-        //  Set RDF Data Types
-        commentElement.setAttribute(RdfConstants.getStringDataTypeAttribute());
-        dbElement.setAttribute(RdfConstants.getStringDataTypeAttribute());
-        idElement.setAttribute(RdfConstants.getStringDataTypeAttribute());
-
-        //  Set Text
-        commentElement.setText("Relationship XRef added by cPath");
-        dbElement.setText(ref.getDatabase());
-        idElement.setText(ref.getId());
-
-        //  Build Hierarchy
-        relationshipElement.addContent(commentElement);
-        relationshipElement.addContent(dbElement);
-        relationshipElement.addContent(idElement);
-        xrefElement.addContent(relationshipElement);
-
-        //  Set an RDF ID, based on locally generated ID.
-        DaoIdGenerator idGenerator = new DaoIdGenerator();
-        relationshipElement.setAttribute(RdfConstants.ID_ATTRIBUTE,
-                idGenerator.getNextId());
-        return xrefElement;
+    /**
+     * Generates a BioPAX Unification XREF Element.
+     *
+     * @param ref             ExternalReference Object.
+     * @param bioPaxNamespace BioPAX Namespace Object.
+     * @return JDOM Element of a BioPAX XREF
+     * @throws DaoException Error Accessing Database
+     */
+    public static Element generateUnificationXref(ExternalReference ref,
+            Namespace bioPaxNamespace) throws DaoException {
+        return generateXref (ref, bioPaxNamespace,
+                BioPaxConstants.XREF_UNIFICATION);
     }
 
     /**
@@ -95,5 +81,56 @@ public class BioPaxGenerator {
             throws DaoException {
         Element xrefElement = generateRelationshipXref(ref, e.getNamespace());
         e.addContent(xrefElement);
+    }
+
+    /**
+     * Appends a BioPAX Unification XREF to the specified Element.
+     *
+     * @param ref ExternalReference Object.
+     * @param e   JDOM Element.
+     * @throws DaoException Error Accessing Database.
+     */
+    public static void appendUnificationXref(ExternalReference ref, Element e)
+            throws DaoException {
+        Element xrefElement = generateUnificationXref(ref, e.getNamespace());
+        e.addContent(xrefElement);
+    }
+
+    /**
+     * Generates a BioPAX XREF Element.
+     *
+     * @param ref             ExternalReference Object.
+     * @param bioPaxNamespace BioPAX Namespace Object.
+     * @return JDOM Element of a BioPAX XREF
+     * @throws DaoException Error Accessing Database
+     */
+    private static Element generateXref(ExternalReference ref,
+            Namespace bioPaxNamespace, String xrefType) throws DaoException {
+
+        //  Create Element
+        Element xrefElement = new Element("XREF", bioPaxNamespace);
+        Element relationshipElement = new Element(xrefType,
+                bioPaxNamespace);
+        Element dbElement = new Element("DB", bioPaxNamespace);
+        Element idElement = new Element("ID", bioPaxNamespace);
+
+        //  Set RDF Data Types
+        dbElement.setAttribute(RdfConstants.getStringDataTypeAttribute());
+        idElement.setAttribute(RdfConstants.getStringDataTypeAttribute());
+
+        //  Set Text
+        dbElement.setText(ref.getDatabase());
+        idElement.setText(ref.getId());
+
+        //  Build Hierarchy
+        relationshipElement.addContent(dbElement);
+        relationshipElement.addContent(idElement);
+        xrefElement.addContent(relationshipElement);
+
+        //  Set an RDF ID, based on locally generated ID.
+        DaoIdGenerator idGenerator = new DaoIdGenerator();
+        relationshipElement.setAttribute(RdfConstants.ID_ATTRIBUTE,
+                idGenerator.getNextId());
+        return xrefElement;
     }
 }
