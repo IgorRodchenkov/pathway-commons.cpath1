d1a6cc1f7bcd9203b42a34b07579c43786181ec0
Benjamin.Gross <Benjamin.Gross@localhost>
2005-11-14 14:54:27 +0000

    maps biopax entity type to plain english

---
+++ cpath/src/org/mskcc/pathdb/model/BioPaxEntityTypeMap.java
@@ -0,0 +1,67 @@
+//
+// $Id: BioPaxEntityTypeMap.java,v 1.1 2005-11-14 14:54:27 grossb Exp $
+//------------------------------------------------------------------------------
+/** Copyright (c) 2005 Memorial Sloan-Kettering Cancer Center.
+ **
+ ** Code written by: Ethan Cerami, Benjamin Gross
+ ** Authors: Ethan Cerami, Benjamin Gross, Gary Bader, Chris Sander
+ **
+ ** This library is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation; either version 2.1 of the License, or
+ ** any later version.
+ **
+ ** This library is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ ** documentation provided hereunder is on an "as is" basis, and
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has no obligations to provide maintenance, support,
+ ** updates, enhancements or modifications.  In no event shall
+ ** Memorial Sloan-Kettering Cancer Center
+ ** be liable to any party for direct, indirect, special,
+ ** incidental or consequential damages, including lost profits, arising
+ ** out of the use of this software and its documentation, even if
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has been advised of the possibility of such damage.  See
+ ** the GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this library; if not, write to the Free Software Foundation,
+ ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ **/
+package org.mskcc.pathdb.model;
+
+import java.util.HashMap;
+
+/**
+ * Hashmap which maps BioPax Entity Type to Plain English.
+ *
+ * @author Benjamin Gross
+ */
+public class BioPaxEntityTypeMap extends HashMap {
+
+    /**
+     * Constructor.
+     */
+    public BioPaxEntityTypeMap() {
+		put("complex", "Complex");
+		put("pathway", "Pathway");
+		put("protein", "Protein");
+		put("rna", "RNA");
+		put("dna", "DNA");
+		put("smallMolecule", "Small Molecule");
+		put("physicalEntity", "Physical Entity");
+		put("transportWithBiochemicalReaction", "Transport with Biochemical Reaction");
+		put("transport", "Transport");
+		put("complexAssembly", "Complex Assembly");
+		put("biochemicalReaction", "Biochemical Reaction");
+		put("conversion", "Conversion");
+		put("modulation", "Modulation");
+		put("catalysis", "Catalysis");
+		put("control", "Control");
+		put("physicalInteraction", "Physical Interaction");
+		put("interaction", "Interaction");
+    }
+
+}
