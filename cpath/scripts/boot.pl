c24dfbd9de2f8ff3501250913b969fbd65c29193
cerami <cerami@code.google.com>
2005-11-14 22:23:11 +0000

    Changed name of bootstrap.sql to unit_test.sql

--- cpath/scripts/boot.pl
+++ cpath/scripts/boot.pl
@@ -2,5 +2,5 @@
 print "Loading Bootstrap Data\n";
 chdir ("dbData") || die "Cannot cd to dbData directory";
 system "mysql -u tomcat --password=kitty < reset.sql";
-system "mysql -u tomcat --password=kitty < bootstrap.sql";
+system "mysql -u tomcat --password=kitty < unit_test.sql";
 print "Bootstrap Loading Complete\n";
