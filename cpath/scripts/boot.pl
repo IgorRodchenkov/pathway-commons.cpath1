fae8155189c0ee74372f1a497b57e7949cd6f447
cerami <cerami@code.google.com>
2005-11-16 13:56:28 +0000

    Removed

--- cpath/scripts/boot.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-print "Loading Bootstrap Data\n";
-chdir ("dbData") || die "Cannot cd to dbData directory";
-system "mysql -u tomcat --password=kitty < reset.sql";
-system "mysql -u tomcat --password=kitty < unit_test.sql";
-print "Bootstrap Loading Complete\n";
