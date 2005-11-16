75de9bad85c3b5487868643a0835f6abea14fc8d
cerami <cerami@code.google.com>
2005-11-16 13:51:36 +0000

    initDb.pl now reads in default values from build.properties

--- cpath/bin/initDb.pl
+++ cpath/bin/initDb.pl
@@ -1,24 +1,49 @@
 #!/usr/bin/perl
-print "Initializing cPath Database\n";
-print "! Running this command will delete all existing data !\n";
-print "Are you sure you wish to proceed (Y/N):  ";
-chomp (my $answer = <STDIN>);
+require "env.pl";
 
-if ($answer eq 'Y' || $answer eq 'y') {
+print "Script to Initialize cPath Database\n";
+print "===================================\n\n";
 
-    # Prompt for user input
-    print "Enter database name:  ";
-    chomp (my $db_name = <STDIN>);
+$arg0 = $ARGV[0];
 
-    print "Enter user name:  ";
-    chomp (my $name = <STDIN>);
+#  Read in build.properties File
+open (PROPS, "< $cpathHome/build.properties");
+while (<PROPS>) {
+    $line = $_;
+    if ($line =~ /=/) {
+        @fields = split (/=/,$_);
+        chomp ($fields[0]);
+        chomp ($fields[1]);
+        $props {$fields[0]} = $fields[1];
+    }
+}
+close (PROPS);
 
-    print "Enter user password:  ";
-    chomp (my $password = <STDIN>);
+$db_name = $props{"db.name"};
+$db_host = $props{"db.host"};
+$db_user = $props{"db.user"};
+$db_password = $props{"db.password"};
+
+# Echo out current properties
+print ("Using Database Name:      $db_name\n");
+print ("Using Database Host:      $db_host\n");
+print ("Using Database User:      $db_user\n");
+print ("Using Database Password:  $db_password\n");
+
+$unitTestFlag = 0;
+if ($arg0 eq "load_test_data") {
+    $unitTestFlag = 1;
+} else {
+    print "\n! Running this command will delete all existing data !\n";
+    print "Are you sure you wish to proceed (Type: YES):  ";
+    chomp (my $answer = <STDIN>);
+}
+
+if ($answer eq 'YES' || $unitTestFlag == 1) {
 
     #  Prepare cpath.sql
-    open (OLD, "< ../dbData/cpath.sql");
-    open (NEW, "> ../dbData/temp1.sql") or die;
+    open (OLD, "< $cpathHome/dbData/cpath.sql");
+    open (NEW, "> $cpathHome/dbData/temp1.sql") or die;
     while (<OLD>) {
         $line = $_;
         $line =~ s/db_name/$db_name/;
@@ -28,8 +53,8 @@ if ($answer eq 'Y' || $answer eq 'y') {
     close (NEW);
 
     #  Prepare seed.sql
-    open (OLD, "< ../dbData/seed.sql");
-    open (NEW, "> ../dbData/temp2.sql") or die;
+    open (OLD, "< $cpathHome/dbData/seed.sql");
+    open (NEW, "> $cpathHome/dbData/temp2.sql") or die;
     while (<OLD>) {
         $line = $_;
         $line =~ s/db_name/$db_name/;
@@ -39,14 +64,30 @@ if ($answer eq 'Y' || $answer eq 'y') {
     close (NEW);
 
     #  Load up Table Structure
-    system "mysql -u $name --password=$password < ../dbData/temp1.sql";
+    system "mysql --host=$db_host --user=$db_user --password=$db_password < ../dbData/temp1.sql";
 
     #  Load up Seed Data
-    system "mysql -u $name --password=$password < ../dbData/temp2.sql";
+    system "mysql --host=$db_host --user=$db_user --password=$db_password < ../dbData/temp2.sql";
 
     #  Delete Temp Files
-    unlink ("../dbData/temp1.sql");
-    unlink ("../dbData/temp2.sql");
+    unlink ("$cpathHome/dbData/temp1.sql");
+    unlink ("$cpathHome/dbData/temp2.sql");
+
+    if ($unitTestFlag ==1) {
+        #  Prepare cpath.sql
+        open (OLD, "< $cpathHome/dbData/unit_test.sql");
+        open (NEW, "> $cpathHome/dbData/temp3.sql") or die;
+        while (<OLD>) {
+            $line = $_;
+            $line =~ s/db_name/$db_name/;
+            print NEW $line;
+        }
+        close (OLD);
+        close (NEW);
+
+        #  Load up Test Data
+        system "mysql --host=$db_host --user=$db_user --password=$db_password < ../dbData/temp3.sql";
+    }
 
     #  All is done
     print "Done.  cPath is now ready to go...\n";
