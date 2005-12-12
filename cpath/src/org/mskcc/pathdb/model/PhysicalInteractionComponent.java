66d0bfedd797152570976185934daef86fb12f91
Benjamin.Gross <Benjamin.Gross@localhost>
2005-12-12 20:56:40 +0000

    bean for physical interaction components

---
+++ cpath/src/org/mskcc/pathdb/model/PhysicalInteractionComponent.java
@@ -0,0 +1,82 @@
+/** Copyright (c) 2005 Memorial Sloan-Kettering Cancer Center.
+ **
+ ** Code written by: Benjamin Gross
+ ** Authors: Ethan Cerami, Benjamin Gross, Gary Bader, Chris Sander
+ **
+ ** This library is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation; either version 2.1 of the License, or
+ ** any later version.
+ **
+ ** This library is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ ** documentation provided hereunder is on an "as is" basis, and
+ ** Memorial Sloan-Kettering Cancer Center 
+ ** has no obligations to provide maintenance, support,
+ ** updates, enhancements or modifications.  In no event shall
+ ** Memorial Sloan-Kettering Cancer Center
+ ** be liable to any party for direct, indirect, special,
+ ** incidental or consequential damages, including lost profits, arising
+ ** out of the use of this software and its documentation, even if
+ ** Memorial Sloan-Kettering Cancer Center 
+ ** has been advised of the possibility of such damage.  See
+ ** the GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this library; if not, write to the Free Software Foundation,
+ ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ **/
+
+// package
+package org.mskcc.pathdb.model;
+
+// imports
+
+/**
+ * This class encapsulates
+ * the members of a physical interaction component.
+ *
+ * @author Benjamin Gross.
+ */
+public class PhysicalInteractionComponent {
+
+	/**
+	 * The component name.
+	 */
+	private String name;
+
+	/**
+	 * The component id.
+	 */
+	private long recordID;
+
+	/**
+     * Constructor.
+	 *
+	 * @param name String.
+	 * @param recordID long.
+     */
+    public PhysicalInteractionComponent(String name, long recordID){
+		this.name = name;
+		this.recordID = recordID;
+	}
+
+	/**
+	 * Returns the component name.
+	 *
+	 * @return String.
+	 */
+	public String getName(){
+		return name;
+	}
+
+	/**
+	 * Returns the record id.
+	 *
+	 * @return long.
+	 */
+	public long getRecordID(){
+		return recordID;
+	}
+}
