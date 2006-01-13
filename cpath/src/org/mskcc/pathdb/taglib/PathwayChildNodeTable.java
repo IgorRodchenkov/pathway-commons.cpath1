0f2fa51184599eb4488f7e277eef0ac7810cc1c3
Benjamin.Gross <Benjamin.Gross@localhost>
2006-01-13 17:02:35 +0000

    move code shared by PathwayChildNodeTable into new model.PhysicalInteractionUtils class()

--- cpath/src/org/mskcc/pathdb/taglib/PathwayChildNodeTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/PathwayChildNodeTable.java
@@ -40,12 +40,8 @@ import org.jdom.JDOMException;
 import org.jdom.xpath.XPath;
 import org.jdom.input.SAXBuilder;
 import org.mskcc.pathdb.sql.dao.DaoCPath;
-
 import org.mskcc.pathdb.model.CPathRecord;
-import org.mskcc.pathdb.model.PhysicalInteraction;
-import org.mskcc.pathdb.model.PhysicalInteractionComponent;
-import org.mskcc.pathdb.schemas.biopax.BioPaxConstants;
-import org.mskcc.pathdb.schemas.biopax.InteractionParser;
+import org.mskcc.pathdb.model.PhysicalInteractionUtils;
 
 /**
  * Custom jsp tag for displaying pathway child node (1 level deep)
@@ -126,7 +122,7 @@ public class PathwayChildNodeTable extends HtmlTable {
 			}
 
 			// interaction summary
-			String interactionString = getInteractionSummary(record.getId());
+			String interactionString = PhysicalInteractionUtils.getInteractionSummary(record.getId());
 			append("<td>" + interactionString + "</td>");
 
 			// details hyperlink
@@ -137,173 +133,6 @@ public class PathwayChildNodeTable extends HtmlTable {
 			jspError(e);
 		}
     }
-
-    /**
-     * Gets Interaction Summary string.
-	 *
-	 * @param recordID long.
-	 * @return String.
-	 * @throws Exception.
-     */
-	public String getInteractionSummary(long recordID) throws Exception {
-		// get interaction parser
-		InteractionParser interactionParser = new InteractionParser(recordID);
-
-		// get physical interacittion
-		PhysicalInteraction physicalInteraction = interactionParser.getConversionInformation();
-		if (physicalInteraction == null){
-			physicalInteraction = interactionParser.getControllerInformation();
-			if (physicalInteraction == null){
-				physicalInteraction = interactionParser.getPhysicalInteractionInformation();
-			}
-		}
-		if (physicalInteraction == null){
-			return "";
-		}
-
-		return createInteractionSummaryString(physicalInteraction);
-	}
-
-    /**
-     * Creates the interaction summary string.
-	 *
-	 * @param physicalInteraction PhysicalInteractiong.
-	 * @return String.
-     */
-	private String createInteractionSummaryString(PhysicalInteraction physicalInteraction){
-
-		int lc, cnt;
-		Vector components;
-		String summaryString = new String();
-		boolean physicalInteractionType =
-			physicalInteraction.getPhysicalInteractionType().equals("Physical Interaction");
-		
-		// left side
-		components = physicalInteraction.getLeftSideComponents();
-		cnt = components.size();
-		for (lc = 0; lc < cnt; lc++){
-			PhysicalInteractionComponent component = (PhysicalInteractionComponent)components.elementAt(lc);
-			String link = new String("<a href=\"record.do?id=" +
-									 String.valueOf(component.getRecordID()) +
-									 "\">" + component.getName() +
-									 "</a>");
-			summaryString += link;
-			// add location:feature string
-			summaryString += createSummaryFeatureString(component);
-			// add summary detail string - see function definition for more info
-			summaryString += summaryDetailString(component.getRecordID());
-			// we may have more than one left participant, if so, separate with "+" or " "
-			if (lc < cnt-1){
-				if (!physicalInteractionType){
-					summaryString += " + ";
-				}
-				else{
-					summaryString += " ";
-				}
-			}
-		}
-
-		if (!physicalInteractionType){
-			// operator
-			summaryString += (" " + physicalInteraction.getOperator() + " ");
-
-			// right side
-			components = physicalInteraction.getRightSideComponents();
-			cnt = components.size();
-			for (lc = 0; lc < cnt; lc++){
-				PhysicalInteractionComponent component = (PhysicalInteractionComponent)components.elementAt(lc);
-				String link = new String("<a href=\"record.do?id=" +
-										 String.valueOf(component.getRecordID()) +
-										 "\">" + component.getName() +
-										 "</a>");
-				summaryString += link;
-				// add location:feature string
-				summaryString += createSummaryFeatureString(component);
-				// add summary detail string - see function definition for more info
-				summaryString += summaryDetailString(component.getRecordID());
-				// we may have more than one right participant, if so, separate with "+"
-				if (lc < cnt-1){
-					summaryString += " + ";
-				}
-			}
-		}
-		
-		// outta here
-		return summaryString;
-	}
-
-    /**
-     * Gets Interaction Summary string.
-	 *
-	 * This has been added to augment originally spec'd
-	 * summary information like 'Phosphorylation' with
-	 * the actual interaction, like 'Alpha6 --> Alpha6',
-	 * so we will have 'Phosphorylation: Alpha6 --> Alpha6'
-	 *
-	 * @param recordID long.
-	 * @return String.
-     */
-	private String summaryDetailString(long recordID) {
-
-		try{
-			String summaryDetails = getInteractionSummary(recordID);
-			if (summaryDetails.length() > 0){
-				return " (" + summaryDetails + ")";
-			}
-		}
-		catch (Exception e){
-			jspError(e);
-		}
-
-		// outta here
-		return "";
-	}
-
-    /**
-     * Creates location:feature string.
-	 *
-	 * @param physicalInteractionComponent PhysicalInteractionComponent.
-	 * @return String.
-     */
-	private String createSummaryFeatureString(PhysicalInteractionComponent physicalInteractionComponent) {
-		
-		// string to return
-		String summaryFeatureString = "";
-
-		// get data from component
-		String cellularLocation = physicalInteractionComponent.getCellularLocation();
-		Vector featureList = physicalInteractionComponent.getFeatureList();
-		int cnt = featureList.size();
-
-		if (cellularLocation.length() > 0){
-			summaryFeatureString = "(" + physicalInteractionComponent.getCellularLocation() + ":";
-		}
-
-		// process feature list
-		if (cnt > 0){
-			if (summaryFeatureString.length() == 0){
-				summaryFeatureString = "(:";
-			}
-			for (int lc = 0; lc < cnt; lc++){
-				String feature = (String)featureList.get(lc);
-				if (lc == 0){
-					summaryFeatureString += feature;
-				}
-				else{
-					summaryFeatureString += ", " + feature;
-				}
-			}
-
-		}
-		
-		// cap off the string
-		if (summaryFeatureString.length() > 0){
-			summaryFeatureString += ")";
-		}
-
-		// outta here
-		return summaryFeatureString;
-	}
 
     /**
      * Handles error processing.
