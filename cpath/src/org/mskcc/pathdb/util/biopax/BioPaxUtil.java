87943fe877a1a63264a5431ba93c3618b7f79f9f
Benjamin.Gross <Benjamin.Gross@localhost>
2006-01-27 14:44:20 +0000

    *** empty log message ***

--- cpath/src/org/mskcc/pathdb/util/biopax/BioPaxUtil.java
+++ cpath/src/org/mskcc/pathdb/util/biopax/BioPaxUtil.java
@@ -27,7 +27,7 @@
  ** along with this library; if not, write to the Free Software Foundation,
  ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  **/
-package org.mskcc.pathdb.schemas.biopax;
+package org.mskcc.pathdb.util.biopax;
 
 import org.jdom.Attribute;
 import org.jdom.Document;
