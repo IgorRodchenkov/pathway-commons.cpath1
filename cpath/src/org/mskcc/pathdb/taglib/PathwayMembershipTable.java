d66385e8fae067824551f11902a94f7a81e66887
Benjamin.Gross <Benjamin.Gross@localhost>
2006-02-16 21:03:40 +0000

    commit to test email notification

--- cpath/src/org/mskcc/pathdb/taglib/PathwayMembershipTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/PathwayMembershipTable.java
@@ -1,4 +1,4 @@
-// $Id: PathwayMembershipTable.java,v 1.1 2006-02-16 15:41:41 grossb Exp $
+// $Id: PathwayMembershipTable.java,v 1.2 2006-02-16 21:03:40 grossb Exp $
 //------------------------------------------------------------------------------
 /** Copyright (c) 2005 Memorial Sloan-Kettering Cancer Center.
  **
@@ -37,7 +37,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Collections;
 import java.util.Comparator;
-
 
 /**
  * Custom jsp tag to generate a table of pathways that a molecule is a member of.
