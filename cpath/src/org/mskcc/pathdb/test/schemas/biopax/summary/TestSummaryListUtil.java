9db28bf8c2a5dbcdc99b29dc65f23c20b6e3a614
cerami <cerami@code.google.com>
2006-02-17 22:17:16 +0000

    Updated to adhere to checkstyle requirements

--- cpath/src/org/mskcc/pathdb/test/schemas/biopax/summary/TestSummaryListUtil.java
+++ cpath/src/org/mskcc/pathdb/test/schemas/biopax/summary/TestSummaryListUtil.java
@@ -1,11 +1,39 @@
+/** Copyright (c) 2004 Memorial Sloan-Kettering Cancer Center.
+ **
+ ** Code written by: Ethan Cerami
+ ** Authors: Ethan Cerami, Gary Bader, Chris Sander
+ **
+ ** This library is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation; either version 2.1 of the License, or
+ ** any later version.
+ **
+ ** This library is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ ** documentation provided hereunder is on an "as is" basis, and
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has no obligations to provide maintenance, support,
+ ** updates, enhancements or modifications.  In no event shall
+ ** Memorial Sloan-Kettering Cancer Center
+ ** be liable to any party for direct, indirect, special,
+ ** incidental or consequential damages, including lost profits, arising
+ ** out of the use of this software and its documentation, even if
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has been advised of the possibility of such damage.  See
+ ** the GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this library; if not, write to the Free Software Foundation,
+ ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ **/
 package org.mskcc.pathdb.test.schemas.biopax.summary;
 
-import org.mskcc.pathdb.schemas.biopax.summary.SummaryListUtil;
+import junit.framework.TestCase;
 import org.mskcc.pathdb.schemas.biopax.summary.EntitySummary;
+import org.mskcc.pathdb.schemas.biopax.summary.SummaryListUtil;
 
 import java.util.ArrayList;
-
-import junit.framework.TestCase;
 
 /**
  * Tests the SummaryListUtil Class.
@@ -16,6 +44,7 @@ public class TestSummaryListUtil extends TestCase {
 
     /**
      * Tests the SummaryListUtil Class.
+     *
      * @throws Exception All Exceptions
      */
     public void testSummaryList() throws Exception {
@@ -23,12 +52,12 @@ public class TestSummaryListUtil extends TestCase {
         ArrayList list = util.getSummaryList();
         EntitySummary summary = (EntitySummary) list.get(0);
         String currentType = summary.getSpecificType();
-        for (int i=0; i<list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             summary = (EntitySummary) list.get(i);
 
             //  Verify that list is sorted by specific type, in ascending order.
             int compare = summary.getSpecificType().compareTo(currentType);
-            assertTrue (compare >= 0);
+            assertTrue(compare >= 0);
             currentType = summary.getSpecificType();
         }
     }
