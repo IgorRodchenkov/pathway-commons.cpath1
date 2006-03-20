75a17a3d64d9dc38863b5d6e6a72230f09d4fcf2
cerami <cerami@code.google.com>
2006-03-20 22:08:35 +0000

    First draft of script to automatically download all pathways from Reactome

---
+++ cpath/scripts/downloadReactome.py
@@ -0,0 +1,42 @@
+# Download All Reactome Data from Live Reactome.org Web Site
+import urllib                                       
+import urllib
+import os
+from sgmllib import SGMLParser
+
+# Bare Bones HTML Parser
+class HtmlParser(SGMLParser):
+	def reset(self):                              
+		SGMLParser.reset(self)
+		self.pathways = []
+
+	def handle_data(self, text):
+		if text.startswith("[Pathway:"):
+			self.pathways.append(text)
+
+# Conditionally create reactome directory
+dir = "reactome"
+if os.path.isdir(dir):
+	pass
+else:
+	os.mkdir (dir)
+
+#  Get Pathway List
+usock = urllib.urlopen("http://www.reactome.org/cgi-bin/instancebrowser?DB=gk_current&ID=170792&")
+parser = HtmlParser()
+parser.feed(usock.read())         
+usock.close()                     
+parser.close()                    
+
+# Download Each Pathway, one at a time
+for pathway in parser.pathways:
+	print "Downloading Pathway:  " + pathway
+	tokens = pathway.split(" ")
+	values = tokens[0].split(":")
+	id = values[1].strip("]")
+	fileName = dir + "/" + id + ".owl"
+	url = "http://reactome.org/cgi-bin/biopaxexporter?DB=gk_current&ID=" + id
+	print "Connecting to:  " + url
+	urllib.urlretrieve(url, fileName)
+	print "Stored to:  " + fileName
+
