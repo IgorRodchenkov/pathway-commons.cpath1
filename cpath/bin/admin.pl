da1b53ce2aeae085473cfe9cfb6d39c294a85b18
cerami <cerami@code.google.com>
2006-06-09 19:13:17 +0000

    Reduced memory allocation for admin command

--- cpath/bin/admin.pl
+++ cpath/bin/admin.pl
@@ -2,4 +2,4 @@
 
 require "env.pl";
 
-system ("java -Xmx2024M -cp $cp -DCPATH_HOME=$cpathHome org.mskcc.pathdb.tool.Admin @ARGV");
+system ("java -Xmx1024M -cp $cp -DCPATH_HOME=$cpathHome org.mskcc.pathdb.tool.Admin @ARGV");
