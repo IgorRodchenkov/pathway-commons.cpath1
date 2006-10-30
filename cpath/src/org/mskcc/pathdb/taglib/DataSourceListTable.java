82b1bbdfd5d9391623c52ac1082d723c3a725034
cerami <cerami@code.google.com>
2006-10-30 16:15:12 +0000

    Initial Commit of DataSourceListTable taglib

---
+++ cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
@@ -0,0 +1,50 @@
+package org.mskcc.pathdb.taglib;
+
+import org.mskcc.pathdb.model.CPathRecord;
+import org.mskcc.pathdb.model.ExternalDatabaseSnapshotRecord;
+import org.mskcc.pathdb.sql.dao.DaoExternalDbSnapshot;
+import org.mskcc.pathdb.sql.dao.DaoException;
+
+import java.util.ArrayList;
+
+/**
+ * Custom JSP Tag for displaying a list of data sources
+ *
+ * @author Benjamin Gross
+ */
+public class DataSourceListTable extends HtmlTable {
+
+    /**
+     * Executes JSP Custom Tag
+     *
+     * @throws Exception Exception in writing to JspWriter.
+     */
+    protected void subDoStartTag() throws Exception {
+        startTable();
+        outputRecords();
+        endTable();
+    }
+
+    /**
+     * Output the Pathway List.
+     */
+    private void outputRecords() throws DaoException {
+        DaoExternalDbSnapshot dao = new DaoExternalDbSnapshot();
+        ArrayList list = dao.getAllDatabaseSnapshots();
+
+        // process records
+        if (list.size() == 0) {
+            startRow();
+            append("<TD COLSPAN=2>No Data Sources Available</TD>");
+            endRow();
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                ExternalDatabaseSnapshotRecord snapshotRecord =
+                        (ExternalDatabaseSnapshotRecord) list.get(i);
+                startRow(1);
+                outputDataField(DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId()));
+                endRow();
+            }
+        }
+    }
+}
