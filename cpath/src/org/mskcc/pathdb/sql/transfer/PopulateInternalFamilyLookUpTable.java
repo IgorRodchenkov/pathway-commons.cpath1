f6fcd1a4c0e134685d8534151f14799c16a7b8dd
cerami <cerami@code.google.com>
2006-11-20 22:12:59 +0000

    Optimized code to be about 2.75X faster than previous version

--- cpath/src/org/mskcc/pathdb/sql/transfer/PopulateInternalFamilyLookUpTable.java
+++ cpath/src/org/mskcc/pathdb/sql/transfer/PopulateInternalFamilyLookUpTable.java
@@ -3,13 +3,13 @@ package org.mskcc.pathdb.sql.transfer;
 import org.mskcc.pathdb.model.CPathRecord;
 import org.mskcc.pathdb.model.CPathRecordType;
 import org.mskcc.pathdb.model.InternalLinkRecord;
-import org.mskcc.pathdb.sql.dao.DaoException;
-import org.mskcc.pathdb.sql.dao.DaoCPath;
-import org.mskcc.pathdb.sql.dao.DaoInternalLink;
-import org.mskcc.pathdb.sql.dao.DaoInternalFamily;
 import org.mskcc.pathdb.sql.JdbcUtil;
-import org.mskcc.pathdb.util.tool.ConsoleUtil;
+import org.mskcc.pathdb.sql.dao.DaoCPath;
+import org.mskcc.pathdb.sql.dao.DaoException;
+import org.mskcc.pathdb.sql.dao.DaoInternalFamily;
+import org.mskcc.pathdb.sql.dao.DaoInternalLink;
 import org.mskcc.pathdb.task.ProgressMonitor;
+import org.mskcc.pathdb.util.tool.ConsoleUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,14 +25,17 @@ import java.util.*;
  */
 public class PopulateInternalFamilyLookUpTable {
     private static final String GET_RECORD_ITERATOR =
-        "select `CPATH_ID`, `TYPE` from cpath WHERE TYPE = ? LIMIT ?,1";
+            "select `CPATH_ID`, `TYPE` from cpath WHERE TYPE = ? LIMIT ?,1";
     private static final String GET_RECORD_BY_ID =
-        "select `CPATH_ID`, `TYPE` from cpath WHERE CPATH_ID = ?";
+            "select `CPATH_ID`, `TYPE` from cpath WHERE CPATH_ID = ?";
     private ProgressMonitor pMonitor;
+    private HashMap cache = new HashMap();
+    private HashSet visitedSet;
 
     /**
      * Constructor.
-     * @param pMonitor  ProgressMonitor Object.
+     *
+     * @param pMonitor ProgressMonitor Object.
      */
     public PopulateInternalFamilyLookUpTable (ProgressMonitor pMonitor) {
         this.pMonitor = pMonitor;
@@ -40,9 +43,10 @@ public class PopulateInternalFamilyLookUpTable {
 
     /**
      * Executes task.
+     *
      * @throws DaoException Data access error.
      */
-    public void execute() throws DaoException {
+    public void execute () throws DaoException {
         DaoCPath daoCPath = DaoCPath.getInstance();
         DaoInternalFamily daoFamily = new DaoInternalFamily();
 
@@ -53,23 +57,31 @@ public class PopulateInternalFamilyLookUpTable {
         int numPathways = daoCPath.getNumEntities(CPathRecordType.PATHWAY);
         pMonitor.setMaxValue(numPathways);
 
+        int option = 1;
+
         //  Index each pathway
         for (int i = 0; i < numPathways; i++) {
-            pMonitor.incrementCurValue();
-            ConsoleUtil.showProgress(pMonitor);
             CPathRecord record = getRecordIdAtOffset(CPathRecordType.PATHWAY, i);
-            ArrayList idList = getAllDescendents (record.getId());
-            Iterator iterator = idList.iterator();
+            if (option == 0) {
+                pMonitor.incrementCurValue();
+                ConsoleUtil.showProgress(pMonitor);
+                ArrayList idList = getAllDescendents(record.getId());
+                Iterator iterator = idList.iterator();
 
-            while (iterator.hasNext()) {
-                Long descendentId = (Long) iterator.next();
-                CPathRecord descendentRecord = getRecordById(descendentId);
+                while (iterator.hasNext()) {
+                    Long descendentId = (Long) iterator.next();
+                    CPathRecord descendentRecord = getRecordById(descendentId);
 
-                //  Only index physical entities
-                if (descendentRecord.getType().equals(CPathRecordType.PHYSICAL_ENTITY)) {
-                    daoFamily.addRecord(record.getId(), record.getType(),
-                        descendentRecord.getId(), descendentRecord.getType());
+                    //  Only index descendents, which are of type:  physical entity
+                    if (descendentRecord.getType().equals(CPathRecordType.PHYSICAL_ENTITY)) {
+                        daoFamily.addRecord(record.getId(), record.getType(),
+                                descendentRecord.getId(), descendentRecord.getType());
+                    }
                 }
+
+            } else {
+                visitedSet = new HashSet();
+                populateInternalFamilyTable(record.getId());
             }
         }
     }
@@ -77,14 +89,14 @@ public class PopulateInternalFamilyLookUpTable {
     /**
      * Gets records at offset.  Optimized SQL (does not retrieve XML content).
      */
-    private CPathRecord getRecordIdAtOffset(CPathRecordType recordType, int offset)
+    private CPathRecord getRecordIdAtOffset (CPathRecordType recordType, int offset)
             throws DaoException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         try {
             con = JdbcUtil.getCPathConnection();
-            pstmt = getPreparedStatement1 (con);
+            pstmt = getPreparedStatement1(con);
             pstmt.setString(1, recordType.toString());
             pstmt.setInt(2, offset);
             rs = pstmt.executeQuery();
@@ -108,7 +120,7 @@ public class PopulateInternalFamilyLookUpTable {
         ResultSet rs = null;
         try {
             con = JdbcUtil.getCPathConnection();
-            pstmt = getPreparedStatement2 (con);
+            pstmt = getPreparedStatement2(con);
             pstmt.setLong(1, cPathId);
             rs = pstmt.executeQuery();
             if (rs.next()) {
@@ -122,19 +134,19 @@ public class PopulateInternalFamilyLookUpTable {
         return null;
     }
 
-    private CPathRecord extractRecord(ResultSet rs) throws SQLException {
+    private CPathRecord extractRecord (ResultSet rs) throws SQLException {
         CPathRecord record = new CPathRecord();
-        record.setId(rs.getLong("CPATH_ID"));
-        record.setType(CPathRecordType.getType(rs.getString("TYPE")));
+        record.setId(rs.getLong(1));
+        record.setType(CPathRecordType.getType(rs.getString(2)));
         return record;
     }
 
-    private PreparedStatement getPreparedStatement1(Connection con) throws SQLException {
-        return con.prepareStatement (GET_RECORD_ITERATOR);
+    private PreparedStatement getPreparedStatement1 (Connection con) throws SQLException {
+        return con.prepareStatement(GET_RECORD_ITERATOR);
     }
 
-    private PreparedStatement getPreparedStatement2(Connection con) throws SQLException {
-        return con.prepareStatement (GET_RECORD_BY_ID);
+    private PreparedStatement getPreparedStatement2 (Connection con) throws SQLException {
+        return con.prepareStatement(GET_RECORD_BY_ID);
     }
 
     /**
@@ -145,7 +157,7 @@ public class PopulateInternalFamilyLookUpTable {
      * @return arraylist of descendent Ids.
      * @throws DaoException Database Access Error.
      */
-    private ArrayList getAllDescendents(long cpathId) throws DaoException {
+    private ArrayList getAllDescendents (long cpathId) throws DaoException {
         ArrayList masterList = new ArrayList();
         Stack stack = new Stack();
         stack.push(cpathId);
@@ -165,5 +177,80 @@ public class PopulateInternalFamilyLookUpTable {
             }
         }
         return masterList;
+    }
+
+    private long[] populateInternalFamilyTable (long cpathId)
+            throws DaoException {
+
+        //  Prevent infinite loop on, e.g. circular pathways
+        if (visitedSet.contains(cpathId)) {
+            return new long[0];
+        } else {
+            visitedSet.add(cpathId);
+        }
+
+        //  First, check to see if we have already visited this record
+        long descendentIds[] = (long[]) cache.get(cpathId);
+        if (descendentIds != null) {
+            //  If we have been here before, use data from family table and immediately return
+            return descendentIds;
+        } else {
+
+            //  Otherwise, recursively walk through children list
+            HashSet descendentList = new HashSet();
+            DaoInternalLink internalLinker = new DaoInternalLink();
+            ArrayList childrenList = internalLinker.getTargets(cpathId);
+
+            //  Recurse through all children
+            if (childrenList != null) {
+                for (int i = 0; i < childrenList.size(); i++) {
+                    InternalLinkRecord link = (InternalLinkRecord) childrenList.get(i);
+                    descendentList.add(link.getTargetId());
+                    long childDescendents[] = populateInternalFamilyTable(link.getTargetId());
+                    for (int j = 0; j < childDescendents.length; j++) {
+                        descendentList.add(childDescendents[j]);
+                    }
+                }
+            }
+
+            //  Store pathway descendents to family table
+            CPathRecord parentRecord = getRecordById(cpathId);
+            if (parentRecord.getType().equals(CPathRecordType.PATHWAY)) {
+                storeFamilyMembership(descendentList, parentRecord);
+            }
+
+            int index = 0;
+            descendentIds = new long[descendentList.size()];
+            Iterator iterator = descendentList.iterator();
+            while (iterator.hasNext()) {
+                Long descendentId = (Long) iterator.next();
+                descendentIds[index] = descendentId;
+                index++;
+            }
+
+            //  Store descendents to cache
+            cache.put(cpathId, descendentIds);
+            return descendentIds;
+        }
+    }
+
+    private void storeFamilyMembership (HashSet descendentList, CPathRecord parentRecord)
+            throws DaoException {
+        Iterator iterator = descendentList.iterator();
+        pMonitor.incrementCurValue();
+        ConsoleUtil.showProgress(pMonitor);
+        DaoInternalFamily daoFamily = new DaoInternalFamily();
+        ArrayList recordTypes = new ArrayList();
+        ArrayList recordIds = new ArrayList();
+        while (iterator.hasNext()) {
+            Long descendentId = (Long) iterator.next();
+            CPathRecord descendentRecord = getRecordById(descendentId);
+            recordIds.add(descendentRecord.getId());
+            recordTypes.add(descendentRecord.getType().toString());
+        }
+
+        daoFamily.addRecords(parentRecord.getId(), parentRecord.getType(),
+                recordIds, recordTypes);
+
     }
 }
