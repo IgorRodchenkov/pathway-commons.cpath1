ea50754fd362b7502283938ef1dbf3e0de8b4fe2
Benjamin.Gross <Benjamin.Gross@localhost>
2006-12-15 19:39:16 +0000

    in addition to cpath.sql and seed.sql, now loads reference.sql

--- cpath/bin/initDb.pl
+++ cpath/bin/initDb.pl
@@ -51,6 +51,7 @@ if ($forceFlag == 0) {
 if ($answer eq 'YES' || $forceFlag == 1) {
     &importSql ("cpath.sql");
     &importSql ("seed.sql");
+	&importSql ("reference.sql");
 
     if ($unitTestFlag ==1) {
         &importSql ("unit_test.sql");
