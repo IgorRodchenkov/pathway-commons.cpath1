37d5b94829d40ed95b8aaf04ccdbbe78c7685fb5
cerami <cerami@code.google.com>
2006-12-18 21:45:31 +0000

    initial commit

---
+++ cpath/src/org/mskcc/pathdb/model/BioPaxTabs.java
@@ -0,0 +1,50 @@
+package org.mskcc.pathdb.model;
+
+import org.mskcc.pathdb.action.BioPaxParentChild;
+
+import java.util.HashMap;
+
+/**
+ * Hashmap which maps BioPax Entity Type to Plain English.
+ *
+ * @author Benjamin Gross
+ */
+public class BioPaxTabs {
+    HashMap childMap = new HashMap();
+    HashMap parentMap = new HashMap();
+
+    /**
+     * Constructor.
+     */
+    public BioPaxTabs () {
+        childMap.put ("complex" , "Complexes");
+        childMap.put ("pathway", "Sub-Pathways");
+        childMap.put ("protein", "Proteins");
+        childMap.put ("smallMolecule", "Small Molecules");
+        childMap.put ("physicalEntity", "Physical Entities");
+        childMap.put ("transportWithBiochemicalReaction", "Transport with Biochemical Reaction");
+        childMap.put ("transport", "Transport Reactions");
+        childMap.put ("complexAssembly", "Complex Assemblies");
+        childMap.put ("biochemicalReaction", "Biochemical Reactions");
+        childMap.put ("conversion", "Conversion Reactions");
+        childMap.put ("modulation", "Modulation Reactions");
+        childMap.put ("catalysis", "Catalysis Reactions");
+        childMap.put ("control", "Control Reactions");
+        childMap.put ("physicalInteraction", "Physical Interactions");
+        childMap.put ("interaction", "Interactions");
+        childMap.put (BioPaxParentChild.GET_SUB_UNITS, "Sub-Units");
+        childMap.put (BioPaxParentChild.GET_PATHWAY_ROOTS, "Pathways");
+        childMap.put (BioPaxParentChild.GET_PE_LEAVES, "Molecules");
+
+        parentMap.putAll(childMap);
+    }
+
+    public String getTabLabel (String command, String type) {
+        if (command.equals(BioPaxParentChild.GET_CHILDREN)) {
+            return (String) childMap.get(type);
+        } else {
+            return (String) parentMap.get(type);
+        }
+    }
+
+}
