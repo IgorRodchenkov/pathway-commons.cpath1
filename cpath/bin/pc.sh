cce33d8290a50a34aa40a9739df4964053057da3
cerami <cerami@code.google.com>
2007-01-08 22:00:48 +0000

    Added unification refs

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -1,5 +1,6 @@
 ./initDb.pl
 ./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
+./admin.pl -f ~/dev/sander/pathway-commons/ids/cpath_unification_sp2refseq.txt import
 ./admin.pl -f ~/dev/sander/pathway-commons/reactome/version19/Homo_sapiens.owl import
 ./admin.pl -f ~/dev/sander/pathway-commons/humancyc import
 ./admin.pl -f ~/dev/sander/pathway-commons/nci import
