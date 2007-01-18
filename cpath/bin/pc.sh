8dc2483818531b56c9889fe9b7669879f8e2819b
Benjamin.Gross <Benjamin.Gross@localhost>
2007-01-18 16:37:36 +0000

    updated path to pathway commons project to be relative to $CPATH_HOME

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -1,9 +1,9 @@
 ./initDb.pl
 ./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
-./admin.pl -f ~/dev/sander/pathway-commons/ids/cpath_unification_sp2refseq.txt import
-./admin.pl -f ~/dev/sander/pathway-commons/reactome/version19/Homo_sapiens.owl import
-./admin.pl -f ~/dev/sander/pathway-commons/humancyc import
-./admin.pl -f ~/dev/sander/pathway-commons/nci import
-./admin.pl -f ~/dev/sander/pathway-commons/cellmap import
+./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
+./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version19/Homo_sapiens.owl import
+./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
+./admin.pl -f $CPATH_HOME/../pathway-commons/nci import
+./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
 ./admin.pl pop_ref
 ./admin.pl index
