12ce4aaa6dbaacd83c796b67fc9706e9253574cc
Benjamin.Gross <Benjamin.Gross@localhost>
2007-02-15 17:08:55 +0000

    added code to narrow by data source

--- cpath/src/org/mskcc/pathdb/action/ExecuteSearch.java
+++ cpath/src/org/mskcc/pathdb/action/ExecuteSearch.java
@@ -1,4 +1,4 @@
-// $Id: ExecuteSearch.java,v 1.10 2007-02-07 18:43:35 grossb Exp $
+// $Id: ExecuteSearch.java,v 1.11 2007-02-15 17:08:55 grossb Exp $
 //------------------------------------------------------------------------------
 /** Copyright (c) 2006 Memorial Sloan-Kettering Cancer Center.
  **
@@ -53,6 +53,8 @@ import org.mskcc.pathdb.util.security.XssFilter;
 import org.mskcc.pathdb.xdebug.XDebug;
 import org.mskcc.pathdb.model.GlobalFilterSettings;
 import org.mskcc.pathdb.model.BioPaxEntityTypeMap;
+import org.mskcc.pathdb.model.ExternalDatabaseSnapshotRecord;
+import org.mskcc.pathdb.sql.dao.DaoExternalDbSnapshot;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -218,50 +220,129 @@ public class ExecuteSearch extends BaseAction {
                 }
                 xdebug.logMsg(this, "User has Global Filter Settings");
             }
-			// grab user selected entity type
-			String userSelectedEntityType =
-				request.getParameter(GlobalFilterSettings.ENTITY_TYPES_FILTER_NAME);
-			// interate through all types, and store query hits by type
-			// also set request attributes as appropriate
-			HashMap<String, Integer> hitByTypeMap = new HashMap<String, Integer>();
-			BioPaxEntityTypeMap typesMap = new BioPaxEntityTypeMap();
-			typesMap.put(GlobalFilterSettings.ALL_ENTITY_TYPES_FILTER_VALUE,
-						 GlobalFilterSettings.ALL_ENTITY_TYPES_FILTER_VALUE);
-			for (String type : (Set<String>)typesMap.keySet()) {
-				// set the type
-				List<String> typeList = new ArrayList();
-				typeList.add(type);
-				GlobalFilterSettings globalFilterSettings = (filterSettings == null) ?
-					new GlobalFilterSettings() : filterSettings;
-				globalFilterSettings.setEntityTypeSelected(typeList);
-				// perform the query
-				LuceneQuery search = new LuceneQuery(protocolRequest, globalFilterSettings, xdebug);
-				long cpathIds[] = search.executeSearch();
-				if (userSelectedEntityType.equals(type)) {
-					request.setAttribute(BaseAction.ATTRIBUTE_CPATH_IDS, cpathIds);
-					request.setAttribute(BaseAction.ATTRIBUTE_TOTAL_NUM_HITS,
-										 new Integer(search.getTotalNumHits()));
-					request.setAttribute(BaseAction.ATTRIBUTE_TEXT_FRAGMENTS,
-										 search.getTextFragments());
-					request.setAttribute(BaseAction.ATTRIBUTE_DATA_SOURCE_SET,
-										 search.getDataSourceSet());
-					request.setAttribute(BaseAction.ATTRIBUTE_DATA_SOURCES,
-										 search.getDataSources());
-					request.setAttribute(BaseAction.ATTRIBUTE_SCORES,
-										 search.getScores());
-				}
-				int totalNumberHits = search.getTotalNumHits();
-				if (totalNumberHits > 0) hitByTypeMap.put(type, totalNumberHits);
-			}
-			// add hits by record type map to request object
-			if (hitByTypeMap.size() > 0) {
-				request.setAttribute(BaseAction.ATTRIBUTE_HITS_BY_RECORD_TYPE_MAP, hitByTypeMap);
-			}
+			// query by type(s)
+			queryByType(xdebug, request, protocolRequest, filterSettings);
+			// query by data source
+			queryByDataSource(xdebug, request, protocolRequest, filterSettings);
+			// outta here
             return mapping.findForward(CPathUIConfig.BIOPAX);
         } catch (DaoException e) {
             throw new ProtocolException (ProtocolStatusCode.INTERNAL_ERROR, e);
+		} catch (CloneNotSupportedException e) {
+            throw new ProtocolException (ProtocolStatusCode.INTERNAL_ERROR, e);
         }
     }
+
+	private void queryByDataSource(XDebug xdebug,
+								   HttpServletRequest request,
+								   ProtocolRequest protocolRequest,
+								   GlobalFilterSettings globalFilterSettings)
+		throws QueryException, DaoException, IOException,
+			   AssemblyException, ParseException, CloneNotSupportedException {
+
+		// needed vars
+		int totalNumberHits = 0;
+		LuceneQuery search = null;
+		HashMap<String, Integer> hitByDataSourceMap = new HashMap<String, Integer>();
+
+		// we are going to be modifying global filter settings, lets make a clone
+		GlobalFilterSettings filterSettings = globalFilterSettings.clone();
+
+		// grab user selected entity type, and set it in global settings
+		String userSelectedEntityType =
+			request.getParameter(GlobalFilterSettings.NARROW_BY_ENTITY_TYPES_FILTER_NAME);
+		List<String> typeList = new ArrayList();
+		typeList.add(userSelectedEntityType);
+		filterSettings.setEntityTypeSelected(typeList);
+
+		// process all (global) data sources
+		search = new LuceneQuery(protocolRequest, filterSettings, xdebug);
+		search.executeSearch();
+		totalNumberHits = search.getTotalNumHits();
+		if (totalNumberHits > 0) {
+			hitByDataSourceMap.put(GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL,
+								   totalNumberHits);
+		}
+
+		Set<Long> snapShotIds = filterSettings.getSnapshotIdSet();
+		// we override global filter settings when we narrow by specific type
+		filterSettings = new GlobalFilterSettings();
+		for (Long id : snapShotIds) {
+			// set the datasource
+			List<Long> dataSourceList = new ArrayList();
+			dataSourceList.add(id);
+			filterSettings.setSnapshotsSelected(dataSourceList);
+			// set the type list
+			filterSettings.setEntityTypeSelected(typeList);
+			// perform the query
+			search = new LuceneQuery(protocolRequest, filterSettings, xdebug);
+			search.executeSearch();
+			totalNumberHits = search.getTotalNumHits();
+			if (totalNumberHits > 0) {
+				hitByDataSourceMap.put(String.valueOf(id.intValue()), totalNumberHits);
+			}
+		}
+		// add hits by data source map to request object
+		request.setAttribute(BaseAction.ATTRIBUTE_HITS_BY_DATA_SOURCE_MAP, hitByDataSourceMap);
+	}
+
+	private void queryByType(XDebug xdebug,
+							 HttpServletRequest request,
+							 ProtocolRequest protocolRequest,
+							 GlobalFilterSettings globalFilterSettings)
+		throws QueryException, DaoException, IOException,
+			   AssemblyException, ParseException, CloneNotSupportedException {
+
+		// grab data source
+		String userSelectedDataSource =
+			request.getParameter(GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_NAME);
+		// grab user selected entity type
+		String userSelectedEntityType =
+			request.getParameter(GlobalFilterSettings.NARROW_BY_ENTITY_TYPES_FILTER_NAME);
+
+		// we are going to be modifying global filter settings, lets make a clone
+		GlobalFilterSettings filterSettings = globalFilterSettings.clone();
+
+		// setup global filters setting - data source filter
+		if (!userSelectedDataSource.equals(GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL)) {
+			// user selected is not equal to global, therefore it is equal to a snapshot id
+			List<Long> dataSourceList = new ArrayList();
+			dataSourceList.add(Long.valueOf(userSelectedDataSource));
+			filterSettings.setSnapshotsSelected(dataSourceList);
+		}
+
+		// setup types map
+		HashMap<String, Integer> hitByTypeMap = new HashMap<String, Integer>();
+		BioPaxEntityTypeMap typesMap = new BioPaxEntityTypeMap();
+		typesMap.put(GlobalFilterSettings.NARROW_BY_ENTITY_TYPES_FILTER_VALUE_ALL,
+					 GlobalFilterSettings.NARROW_BY_ENTITY_TYPES_FILTER_VALUE_ALL);
+
+		// interate through all types, and store query hits by type
+		for (String type : (Set<String>)typesMap.keySet()) {
+			// setup global filters setting - types filter
+			List<String> typeList = new ArrayList();
+			typeList.add(type);
+			filterSettings.setEntityTypeSelected(typeList);
+			// perform the query
+			LuceneQuery search = new LuceneQuery(protocolRequest, filterSettings, xdebug);
+			long cpathIds[] = search.executeSearch();
+			if (userSelectedEntityType.equals(type)) {
+				request.setAttribute(BaseAction.ATTRIBUTE_CPATH_IDS, cpathIds);
+				request.setAttribute(BaseAction.ATTRIBUTE_TOTAL_NUM_HITS,
+									 new Integer(search.getTotalNumHits()));
+				request.setAttribute(BaseAction.ATTRIBUTE_TEXT_FRAGMENTS,
+									 search.getTextFragments());
+				request.setAttribute(BaseAction.ATTRIBUTE_DATA_SOURCES,
+									 search.getDataSources());
+				request.setAttribute(BaseAction.ATTRIBUTE_SCORES,
+									 search.getScores());
+			}
+			int totalNumberHits = search.getTotalNumHits();
+			if (totalNumberHits > 0) hitByTypeMap.put(type, totalNumberHits);
+		}
+		// add hits by record type map to request object
+		request.setAttribute(BaseAction.ATTRIBUTE_HITS_BY_RECORD_TYPE_MAP, hitByTypeMap);
+	}
 
     /**
      * If cPath contains only PSI-MI data and the web_mode is set to:
