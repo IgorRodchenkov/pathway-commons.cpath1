e6e35b5a9729a6d69dcb0f0a4b4d8a81e8952673
Benjamin.Gross <Benjamin.Gross@localhost>
2007-03-12 18:58:43 +0000

    script to initialize bb tables

---
+++ cpath/dbData/bb.sql
@@ -0,0 +1,27 @@
+#
+# Table structure for table `bb_pathway`
+#
+CREATE TABLE `bb_pathway` (
+	`EXTERNAL_PATHWAY_ID` varchar(255) NOT NULL default '',
+    `PATHWAY_NAME` varchar(255) NOT NULL default '',
+    `SOURCE` varchar(255) NOT NULL default '',
+    PRIMARY KEY  (`EXTERNAL_PATHWAY_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+#
+# Table structure for table `bb_gene`
+#
+CREATE TABLE `bb_gene` (
+    `ENTREZ_GENE_ID` varchar(255) NOT NULL default '',
+    `GENE_NAME` varchar(255) NOT NULL default '',
+    PRIMARY KEY  (`ENTREZ_GENE_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+#
+# Table structure for table `bb_internal_link`
+#
+CREATE TABLE `bb_internal_link` (
+	`EXTERNAL_PATHWAY_ID` varchar(255) NOT NULL default '',
+    `ENTREZ_GENE_ID` varchar(255) NOT NULL default '',
+    PRIMARY KEY  (`EXTERNAL_PATHWAY_ID`, `ENTREZ_GENE_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
