584b8e21761836c17af49caa5a0f030f591dbcda
Benjamin.Gross <Benjamin.Gross@localhost>
2007-03-13 16:00:46 +0000

    added dropped statements

--- cpath/dbData/bb.sql
+++ cpath/dbData/bb.sql
@@ -1,8 +1,10 @@
 USE db_name__value;
 
+
 #
 # Table structure for table `bb_pathway`
 #
+DROP TABLE IF EXISTS `bb_pathway`;
 CREATE TABLE `bb_pathway` (
 	`EXTERNAL_PATHWAY_ID` varchar(255) NOT NULL default '',
     `PATHWAY_NAME` varchar(255) NOT NULL default '',
@@ -14,6 +16,7 @@ CREATE TABLE `bb_pathway` (
 #
 # Table structure for table `bb_gene`
 #
+DROP TABLE IF EXISTS `bb_gene`;
 CREATE TABLE `bb_gene` (
     `ENTREZ_GENE_ID` varchar(255) NOT NULL default '',
     `GENE_NAME` varchar(255) NOT NULL default '',
@@ -23,6 +26,7 @@ CREATE TABLE `bb_gene` (
 #
 # Table structure for table `bb_internal_link`
 #
+DROP TABLE IF EXISTS `bb_internal_link`;
 CREATE TABLE `bb_internal_link` (
 	`EXTERNAL_PATHWAY_ID` varchar(255) NOT NULL default '',
     `ENTREZ_GENE_ID` varchar(255) NOT NULL default '',
