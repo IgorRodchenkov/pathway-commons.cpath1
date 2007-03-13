aae5e243cfddf806738f033d6e394940a0abd3eb
Benjamin.Gross <Benjamin.Gross@localhost>
2007-03-13 15:30:28 +0000

    update to imports

--- cpath/src/org/mskcc/pathdb/bb/sql/dao/DaoBBGene.java
+++ cpath/src/org/mskcc/pathdb/bb/sql/dao/DaoBBGene.java
@@ -33,6 +33,7 @@ package org.mskcc.pathdb.bb.sql.dao;
 
 import org.mskcc.pathdb.model.BBGeneRecord;
 import org.mskcc.pathdb.sql.JdbcUtil;
+import org.mskcc.pathdb.sql.dao.DaoException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
