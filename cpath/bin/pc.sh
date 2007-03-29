6cfa961ed232fed8efbda8bf5827229ec4b4e6b1
Benjamin.Gross <Benjamin.Gross@localhost>
2007-03-29 17:28:19 +0000

    removed call to InitBB.pl

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -1,10 +1,9 @@
-./initDb.pl
-./initBBDb.pl
+./initDb.pl -f
 ./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
-#./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version19/Homo_sapiens.owl import
-#./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
+./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version19/Homo_sapiens.owl import
+./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/nci import
-#./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
+./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
 ./admin.pl pop_ref
 ./admin.pl index
