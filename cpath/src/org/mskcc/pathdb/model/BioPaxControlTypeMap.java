e2091a77de267ce0e966beec38568cbb4dcf95e3
cerami <cerami@code.google.com>
2007-05-01 20:37:04 +0000

    Added support for both past-tense and present-tense translations

--- cpath/src/org/mskcc/pathdb/model/BioPaxControlTypeMap.java
+++ cpath/src/org/mskcc/pathdb/model/BioPaxControlTypeMap.java
@@ -1,4 +1,4 @@
-// $Id: BioPaxControlTypeMap.java,v 1.7 2006-06-09 19:22:03 cerami Exp $
+// $Id: BioPaxControlTypeMap.java,v 1.8 2007-05-01 20:37:04 cerami Exp $
 //------------------------------------------------------------------------------
 /** Copyright (c) 2006 Memorial Sloan-Kettering Cancer Center.
  **
@@ -38,23 +38,58 @@ import java.util.HashMap;
  *
  * @author Benjamin Gross
  */
-public class BioPaxControlTypeMap extends HashMap {
+public class BioPaxControlTypeMap {
+    private static HashMap presentTenseMap;
+    private static HashMap pastTenseMap;
 
     /**
-     * Constructor.
+     * Gets a HashMap that maps BioPAX control types to plain english (present tense).
+     * For exmple, "INHIBITION" maps to "inhibits" (present tense).
+     *
+     * @return HashMap, keyed by BioPAX control type.
      */
-    public BioPaxControlTypeMap() {
-        put("INHIBITION", "inhibits");
-        put("ACTIVATION", "activates");
-        put("INHIBITION-ALLOSTERIC", "allosterically inhibits");
-        put("INHIBITION-COMPETITIVE", "competitively inhibits");
-        put("INHIBITION-IRREVERSIBLE", "irreversibly inhibits");
-        put("INHIBITION-NONCOMPETITIVE", "noncompetitively inhibits");
-        put("INHIBITION-OTHER", "inhibits (other)");
-        put("INHIBITION-UNCOMPETITIVE", "uncompetitively inhibits");
-        put("INHIBITION-UNKMECH", "inhibits");
-        put("ACTIVATION-NONALLOSTERIC", "nonallosterically activates");
-        put("ACTIVATION-ALLOSTERIC", "allosterically activates");
-        put("ACTIVATION-UNKMECH", "activates");
+    public static HashMap getPresentTenseMap() {
+        if (presentTenseMap == null) {
+            presentTenseMap = new HashMap();
+            presentTenseMap.put("INHIBITION", "inhibits");
+            presentTenseMap.put("ACTIVATION", "activates");
+            presentTenseMap.put("INHIBITION-ALLOSTERIC", "allosterically inhibits");
+            presentTenseMap.put("INHIBITION-COMPETITIVE", "competitively inhibits");
+            presentTenseMap.put("INHIBITION-IRREVERSIBLE", "irreversibly inhibits");
+            presentTenseMap.put("INHIBITION-NONCOMPETITIVE", "noncompetitively inhibits");
+            presentTenseMap.put("INHIBITION-OTHER", "inhibits (other)");
+            presentTenseMap.put("INHIBITION-UNCOMPETITIVE", "uncompetitively inhibits");
+            presentTenseMap.put("INHIBITION-UNKMECH", "inhibits");
+            presentTenseMap.put("ACTIVATION-NONALLOSTERIC", "nonallosterically activates");
+            presentTenseMap.put("ACTIVATION-ALLOSTERIC", "allosterically activates");
+            presentTenseMap.put("ACTIVATION-UNKMECH", "activates");
+        }
+        return presentTenseMap;
     }
+
+    /**
+     * Gets a HashMap that maps BioPAX control types to plain english (past tense).
+     * For exmple, "INHIBITION" maps to "inhibited" (past tense).
+     *
+     * @return HashMap, keyed by BioPAX control type.
+     */
+    public static HashMap getPastTenseMap() {
+        if (pastTenseMap == null) {
+            pastTenseMap = new HashMap();
+            pastTenseMap.put("INHIBITION", "inhibited");
+            pastTenseMap.put("ACTIVATION", "activated");
+            pastTenseMap.put("INHIBITION-ALLOSTERIC", "allosterically inhibited");
+            pastTenseMap.put("INHIBITION-COMPETITIVE", "competitively inhibited");
+            pastTenseMap.put("INHIBITION-IRREVERSIBLE", "irreversibly inhibited");
+            pastTenseMap.put("INHIBITION-NONCOMPETITIVE", "noncompetitively inhibited");
+            pastTenseMap.put("INHIBITION-OTHER", "inhibited (other)");
+            pastTenseMap.put("INHIBITION-UNCOMPETITIVE", "uncompetitively inhibited");
+            pastTenseMap.put("INHIBITION-UNKMECH", "inhibited");
+            pastTenseMap.put("ACTIVATION-NONALLOSTERIC", "nonallosterically activated");
+            pastTenseMap.put("ACTIVATION-ALLOSTERIC", "allosterically activated");
+            pastTenseMap.put("ACTIVATION-UNKMECH", "activated");
+        }
+        return pastTenseMap;
+    }
+
 }
