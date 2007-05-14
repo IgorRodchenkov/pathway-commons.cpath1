a6fd0dbe85fdd13d4c79c8a5c696d0b7ecb42a68
cerami <cerami@code.google.com>
2007-05-14 19:24:01 +0000

    Removed call to Python pre-processor, as this is longer required for NCI data

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -3,8 +3,7 @@
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version19/Homo_sapiens.owl import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
-python $CPATH_HOME/../pathway-commons/nci/preprocess.py $CPATH_HOME/../pathway-commons/nci/NCI-Nature_Curated.owl $CPATH_HOME/../pathway-commons/nci/NCI-Nature_Curated-Processed.owl 
-./admin.pl -f $CPATH_HOME/../pathway-commons/nci/NCI-Nature_Curated-Processed.owl import
+./admin.pl -f $CPATH_HOME/../pathway-commons/nci/ import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
 ./admin.pl pop_ref
 ./admin.pl index
