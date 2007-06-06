6e20bed69bd1db561d66a97eb99f624449bcad57
cerami <cerami@code.google.com>
2007-06-06 15:52:59 +0000

    updated links to use current web API version, as defind by skin

--- cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
@@ -6,6 +6,8 @@ import org.mskcc.pathdb.model.ExternalDatabaseSnapshotRecord;
 import org.mskcc.pathdb.model.ExternalDatabaseRecord;
 import org.mskcc.pathdb.sql.dao.DaoExternalDbSnapshot;
 import org.mskcc.pathdb.sql.dao.DaoException;
+import org.mskcc.pathdb.form.WebUIBean;
+import org.mskcc.pathdb.servlet.CPathUIConfig;
 
 import java.util.ArrayList;
 
@@ -33,6 +35,7 @@ public class DataSourceListTable extends HtmlTable {
     private void outputRecords() throws DaoException {
         DaoExternalDbSnapshot dao = new DaoExternalDbSnapshot();
         ArrayList list = dao.getAllDatabaseSnapshots();
+        WebUIBean webUIBean = CPathUIConfig.getWebUIBean();
 
         // process records
         if (list.size() == 0) {
@@ -57,7 +60,9 @@ public class DataSourceListTable extends HtmlTable {
                         + DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId()));
                 if (snapshotRecord.getExternalDatabase() != null) {
                     ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
-                    append ("<br><a href=\"webservice.do?version=1.0&format=html&cmd=get_by_keyword");
+                    append ("<br><a href=\"webservice.do?version=");
+                    append (webUIBean.getWebApiVersion());
+                    append ("&format=html&cmd=get_by_keyword");
 					append ("&" + GlobalFilterSettings.NARROW_BY_ENTITY_TYPES_FILTER_NAME + "=pathway");
 					append ("&" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_NAME + "=" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL);
                     append ("&q=data_source%3A%22" + dbRecord.getMasterTerm() + "%22+AND+entity_type%3Apathway\">");
