3b34e217c1912a819e9d8314c91f8f585c799d42
cerami <cerami@code.google.com>
2007-06-06 13:42:01 +0000

    Added validation for version #

--- cpath/src/org/mskcc/pathdb/protocol/ProtocolValidatorVersion2.java
+++ cpath/src/org/mskcc/pathdb/protocol/ProtocolValidatorVersion2.java
@@ -34,6 +34,7 @@ class ProtocolValidatorVersion2 {
      * @throws NeedsHelpException Indicates user requests/needs help.
      */
     public void validate() throws ProtocolException, NeedsHelpException {
+        validateVersion();        
         validateCommand();
         validateQuery();
     }
@@ -91,6 +92,25 @@ class ProtocolValidatorVersion2 {
                     "Argument:  '" + ProtocolRequest.ARG_QUERY
                             + "' is not specified." + ProtocolValidator.HELP_MESSAGE,
                     "You did not specify a query term.  Please try again.");
+        }
+    }
+
+    /**
+     * Validates the Version Parameter.
+     *
+     * @throws ProtocolException Indicates Violation of Protocol.
+     */
+    private void validateVersion() throws ProtocolException {
+        if (request.getVersion() == null) {
+            throw new ProtocolException(ProtocolStatusCode.MISSING_ARGUMENTS,
+                    "Argument: '" + ProtocolRequest.ARG_VERSION
+                            + "' is not specified." + ProtocolValidator.HELP_MESSAGE);
+        }
+        if (!request.getVersion().equals(ProtocolConstantsVersion2.VERSION_2)) {
+            throw new ProtocolException
+                    (ProtocolStatusCode.VERSION_NOT_SUPPORTED,
+                            "This data service currently only supports "
+                                    + "version 2.0." + ProtocolValidator.HELP_MESSAGE);
         }
     }
 }
