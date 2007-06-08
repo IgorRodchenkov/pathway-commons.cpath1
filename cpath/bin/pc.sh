17bf1a6dd3a6e8aaea6ccbcf24b061d6e5e1a28f
Benjamin.Gross <Benjamin.Gross@localhost>
2007-06-08 20:28:51 +0000

    now imports reactome v21 data

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -1,7 +1,7 @@
 ./initDb.pl
 ./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
-./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version19/Homo_sapiens.owl import
+./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo\ sapiens.owl" import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/nci/ import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
