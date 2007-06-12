3f58b8bde66e100419ad74e1df398ad610fcf717
cerami <cerami@code.google.com>
2007-06-12 16:47:23 +0000

    Added validation for data source parameter

--- cpath/src/org/mskcc/pathdb/protocol/ProtocolValidatorVersion2.java
+++ cpath/src/org/mskcc/pathdb/protocol/ProtocolValidatorVersion2.java
@@ -1,10 +1,13 @@
 package org.mskcc.pathdb.protocol;
 
 import org.mskcc.pathdb.form.WebUIBean;
+import org.mskcc.pathdb.model.ExternalDatabaseSnapshotRecord;
 import org.mskcc.pathdb.servlet.CPathUIConfig;
+import org.mskcc.pathdb.sql.dao.DaoException;
+import org.mskcc.pathdb.sql.dao.DaoExternalDbSnapshot;
 
-import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Validates Client/Browser Request, Version 1.0.
@@ -12,7 +15,7 @@ import java.util.ArrayList;
  * @author cPath Dev Team.
  */
 class ProtocolValidatorVersion2 {
-   /**
+    /**
      * Protocol Request.
      */
     private ProtocolRequest request;
@@ -37,10 +40,11 @@ class ProtocolValidatorVersion2 {
      * @throws ProtocolException  Indicates Violation of Protocol.
      * @throws NeedsHelpException Indicates user requests/needs help.
      */
-    public void validate() throws ProtocolException, NeedsHelpException {
+    public void validate () throws ProtocolException, NeedsHelpException {
         validateCommand();
         validateVersion();
         validateInputIdType();
+        validateDataSources();
         validateQuery();
     }
 
@@ -50,7 +54,7 @@ class ProtocolValidatorVersion2 {
      * @throws ProtocolException  Indicates Violation of Protocol.
      * @throws NeedsHelpException Indicates user requests/needs help.
      */
-    private void validateCommand() throws ProtocolException,
+    private void validateCommand () throws ProtocolException,
             NeedsHelpException {
         if (request.getCommand() == null) {
             throw new ProtocolException(ProtocolStatusCode.MISSING_ARGUMENTS,
@@ -76,7 +80,7 @@ class ProtocolValidatorVersion2 {
      *
      * @throws ProtocolException Indicates Violation of Protocol.
      */
-    private void validateQuery() throws ProtocolException {
+    private void validateQuery () throws ProtocolException {
         String command = request.getCommand();
         String q = request.getQuery();
         String org = request.getOrganism();
@@ -96,10 +100,11 @@ class ProtocolValidatorVersion2 {
             if (q != null) {
                 String ids[] = q.split("[\\s]");
                 if (ids.length > ProtocolConstantsVersion2.MAX_NUM_IDS) {
-                throw new ProtocolException(ProtocolStatusCode.INVALID_ARGUMENT,
-                        "To prevent overloading of the system, clients are "
-                                + "restricted to a maximum of "
-                                + ProtocolConstantsVersion2.MAX_NUM_IDS + " IDs at a time.");                }
+                    throw new ProtocolException(ProtocolStatusCode.INVALID_ARGUMENT,
+                            "To prevent overloading of the system, clients are "
+                                    + "restricted to a maximum of "
+                                    + ProtocolConstantsVersion2.MAX_NUM_IDS + " IDs at a time.");
+                }
             }
         }
         if (errorFlag) {
@@ -115,7 +120,7 @@ class ProtocolValidatorVersion2 {
      *
      * @throws ProtocolException Indicates Violation of Protocol.
      */
-    private void validateVersion() throws ProtocolException {
+    private void validateVersion () throws ProtocolException {
         if (request.getVersion() == null) {
             throw new ProtocolException(ProtocolStatusCode.MISSING_ARGUMENTS,
                     "Argument: '" + ProtocolRequest.ARG_VERSION
@@ -129,31 +134,66 @@ class ProtocolValidatorVersion2 {
         }
     }
 
-    private void validateInputIdType() throws ProtocolException {
+    private void validateInputIdType () throws ProtocolException {
         String command = request.getCommand();
         if (command != null && command.equals(ProtocolConstantsVersion2.COMMAND_GET_PATHWAY_LIST)) {
             String inputIdType = request.getInputIDType();
             if (inputIdType == null || inputIdType.trim().length() == 0) {
                 throw new ProtocolException(ProtocolStatusCode.MISSING_ARGUMENTS,
-                    "Argument:  '" + ProtocolRequest.ARG_INPUT_ID_TYPE
-                            + "' is not specified." + ProtocolValidator.HELP_MESSAGE);
+                        "Argument:  '" + ProtocolRequest.ARG_INPUT_ID_TYPE
+                                + "' is not specified." + ProtocolValidator.HELP_MESSAGE);
             } else {
                 WebUIBean webBean = CPathUIConfig.getWebUIBean();
                 ArrayList supportedIdList = webBean.getSupportedInputIdTypes();
                 if (!supportedIdList.contains(inputIdType)) {
                     StringBuffer buf = new StringBuffer();
-                    for (int i=0; i<supportedIdList.size(); i++) {
+                    for (int i = 0; i < supportedIdList.size(); i++) {
                         buf.append(supportedIdList.get(i));
-                        if (i < supportedIdList.size() -1) {
+                        if (i < supportedIdList.size() - 1) {
                             buf.append(", ");
                         }
                     }
                     throw new ProtocolException(ProtocolStatusCode.INVALID_ARGUMENT,
                             ProtocolRequest.ARG_INPUT_ID_TYPE
-                            + " must be set to one of the following: "
-                            + buf.toString() + ".");
+                                    + " must be set to one of the following: "
+                                    + buf.toString() + ".");
                 }
             }
         }
+    }
+
+    private void validateDataSources () throws ProtocolException {
+        String command = request.getCommand();
+        try {
+            if (command != null && command.equals
+                    (ProtocolConstantsVersion2.COMMAND_GET_PATHWAY_LIST)) {
+                String dataSources[] = request.getDataSources();
+                if (dataSources != null) {
+                    ArrayList masterTermList = getMasterTermList();
+                    for (int i = 0; i < dataSources.length; i++) {
+                        if (!masterTermList.contains(dataSources[i])) {
+                            throw new ProtocolException(ProtocolStatusCode.INVALID_ARGUMENT,
+                                    ProtocolRequest.ARG_DATA_SOURCE + ": "
+                                            + dataSources[i] + " is not a recognized data source.");
+                        }
+                    }
+                }
+            }
+        } catch (DaoException e) {
+            throw new ProtocolException(ProtocolStatusCode.INTERNAL_ERROR);
+        }
+    }
+
+    private ArrayList getMasterTermList () throws DaoException {
+        DaoExternalDbSnapshot dao = new DaoExternalDbSnapshot();
+        ArrayList list = dao.getAllDatabaseSnapshots();
+        ArrayList masterTermList = new ArrayList();
+        for (int i = 0; i < list.size(); i++) {
+            ExternalDatabaseSnapshotRecord snapshotRecord = (ExternalDatabaseSnapshotRecord)
+                    list.get(i);
+            String masterTerm = snapshotRecord.getExternalDatabase().getMasterTerm();
+            masterTermList.add(masterTerm);
+        }
+        return masterTermList;
     }
 }
