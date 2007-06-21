04db309e4ce2872e7ced65610868d2e5ad54c838
Benjamin.Gross <Benjamin.Gross@localhost>
2007-06-21 17:24:59 +0000

    code to run reactome file cooker

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -1,9 +1,10 @@
 ./initDb.pl
 ./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
-/usr/bin/python $CPATH_HOME/../pathway-commons/bin/reactome-cooker.py $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.owl" $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.cooked.owl"
-./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo\ sapiens.cooked.owl" import
-rm -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.cooked.owl"
+mv -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.owl" $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.bak.owl"
+/usr/bin/python $CPATH_HOME/../pathway-commons/bin/reactome-cooker.py $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.bak.owl" $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.owl"
+./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo\ sapiens.owl" import
+mv -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.bak.owl" $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.owl"
 ./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/nci/ import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
