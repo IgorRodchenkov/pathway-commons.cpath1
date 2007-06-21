5dc0e408b76020872817b139de6ba0f9e37f403e
Benjamin.Gross <Benjamin.Gross@localhost>
2007-06-21 14:45:29 +0000

    now cooks reactome data before importing

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -1,7 +1,9 @@
 ./initDb.pl
 ./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
-./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo\ sapiens.owl" import
+/usr/bin/python $CPATH_HOME/../pathway-commons/bin/reactome-cooker.py $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.owl" $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.cooked.owl"
+./admin.pl -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo\ sapiens.cooked.owl" import
+rm -f $CPATH_HOME/../pathway-commons/reactome/version21/"Homo sapiens.cooked.owl"
 ./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/nci/ import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
