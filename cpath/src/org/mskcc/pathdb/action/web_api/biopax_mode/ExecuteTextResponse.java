7800aca97af7024b083d1040edec5624919f28f8
cerami <cerami@code.google.com>
2007-09-12 14:56:31 +0000

    Fleshed out option for return Text;  now working

--- cpath/src/org/mskcc/pathdb/action/web_api/biopax_mode/ExecuteTextResponse.java
+++ cpath/src/org/mskcc/pathdb/action/web_api/biopax_mode/ExecuteTextResponse.java
@@ -1,4 +1,4 @@
-// $Id: ExecuteTextResponse.java,v 1.1 2007-09-04 18:21:08 cerami Exp $
+// $Id: ExecuteTextResponse.java,v 1.2 2007-09-12 14:56:31 cerami Exp $
 //------------------------------------------------------------------------------
 /** Copyright (c) 2006 Memorial Sloan-Kettering Cancer Center.
  **
@@ -31,12 +31,92 @@
  **/
 package org.mskcc.pathdb.action.web_api.biopax_mode;
 
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.mskcc.pathdb.protocol.ProtocolRequest;
+import org.mskcc.pathdb.protocol.ProtocolException;
+import org.mskcc.pathdb.protocol.ProtocolConstantsVersion2;
+import org.mskcc.pathdb.xdebug.XDebug;
+import org.mskcc.pathdb.sql.query.GetNeighborsCommand;
+import org.mskcc.pathdb.sql.dao.DaoException;
+import org.mskcc.pathdb.query.batch.PathwayBatchQuery;
+import org.mskcc.pathdb.query.batch.PhysicalEntityWithPathwayList;
+import org.mskcc.pathdb.util.ExternalDatabaseConstants;
+import org.mskcc.pathdb.schemas.biopax.summary.BioPaxRecordSummaryException;
+import org.mskcc.pathdb.action.web_api.WebApiUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+import java.util.ArrayList;
+import java.io.IOException;
+
 /**
- * Created by IntelliJ IDEA.
- * User: cerami
- * Date: Sep 4, 2007
- * Time: 10:16:33 AM
- * To change this template use File | Settings | File Templates.
+ * BioPAX Web Mode:  Response is of type Text.
+ *
+ * @author Ethan Cerami, Benjamin Gross.
  */
 public class ExecuteTextResponse {
+
+    /**
+     * Processes Client Request.
+     * @param xdebug            XDebug Object.
+     * @param protocolRequest   Protocol Request Object.
+     * @param request           HttpServlet Request Object.
+     * @param response          HttpServlet Response Object.
+     * @param mapping           Struts Action Mapping Object.
+     * @return                  Struts Action Forward Object.
+     * @throws ProtocolException    Protocol Error.
+     * @throws DaoException         Database Error.
+     * @throws IOException          I/O Error.
+     * @throws BioPaxRecordSummaryException Error creating BioPAX Record Summary.
+     */
+    public ActionForward processRequeset (XDebug xdebug, ProtocolRequest protocolRequest, HttpServletRequest request,
+        HttpServletResponse response, ActionMapping mapping) throws ProtocolException, DaoException,
+            IOException, BioPaxRecordSummaryException {
+
+		if (protocolRequest.getCommand().equals(ProtocolConstantsVersion2.COMMAND_GET_NEIGHBORS)) {
+			return getNeighborsHandler(protocolRequest, response, xdebug);
+		} else if (protocolRequest.getCommand().equals
+                (ProtocolConstantsVersion2.COMMAND_GET_PATHWAY_LIST)) {
+            return getPathwayListHandler(protocolRequest, response);
+        }
+		return null;
+	}
+
+	/**
+	 * Special-Case handler for getNeighbors Command.
+	 *
+	 */
+	private ActionForward getNeighborsHandler(ProtocolRequest protocolRequest,
+        HttpServletResponse response, XDebug xdebug) throws ProtocolException, IOException, DaoException {
+        GetNeighborsCommand cmd = new GetNeighborsCommand(protocolRequest, xdebug);
+        Set<GetNeighborsCommand.Neighbor> neighbors = cmd.getNeighbors();
+        String table = cmd.outputTabDelimitedText(neighbors);
+        WebApiUtil.returnText(response, table);
+        return null;
+	}
+
+	/**
+	 * Special-Case handler for getPathwayList Command.
+	 */
+	private ActionForward getPathwayListHandler(ProtocolRequest protocolRequest, HttpServletResponse response)
+            throws ProtocolException, DaoException, BioPaxRecordSummaryException {
+        PathwayBatchQuery batchQuery = new PathwayBatchQuery();
+        String ids[] = protocolRequest.getQuery().split("[\\s]");
+        String dbTerm = protocolRequest.getInputIDType();
+        if (dbTerm == null) {
+            dbTerm = ExternalDatabaseConstants.INTERNAL_DATABASE;
+        }
+        String dataSources[] = protocolRequest.getDataSources();
+        ArrayList<PhysicalEntityWithPathwayList> list;
+        if (dataSources != null) {
+            list = batchQuery.executeBatchQuery (ids, dbTerm, dataSources);
+        } else {
+            list = batchQuery.executeBatchQuery (ids, dbTerm);
+        }
+        String table = batchQuery.outputTabDelimitedText(list);
+        WebApiUtil.returnText(response, table);
+        return null;
+	}
 }
