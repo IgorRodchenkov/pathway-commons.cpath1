a03d990a4df00682f2d377e1ccaa7e403c2255f7
cerami <cerami@code.google.com>
2008-04-17 13:52:42 +0000

    Initial commit

---
+++ cpath/src/org/mskcc/pathdb/util/file/UniProtFileUtil.java
@@ -0,0 +1,43 @@
+package org.mskcc.pathdb.util.file;
+
+import java.io.File;
+
+/**
+ * Generates organism specific file names, based on UniProt File Names.
+ *
+ * For example, given the file name:  uniprot_sprot_human.dat.
+ * getOrganismSpecificFileName (file, "entrez_gene") will generate
+ * the file name:  entrez_gene_human.txt.
+ *
+ * @author Ethan Cerami 
+ */
+public class UniProtFileUtil {
+
+    /**
+     * Generates an organism specific file name, based on a UniProt File.
+     *
+     * For example, given the UniProt file:  uniprot_sprot_human.dat.
+     * getOrganismSpecificFileName (file, "entrez_gene") will generate
+     * the file name:  entrez_gene_human.txt.
+     *
+     * @param file      UniProt File of the pattern "xxx_xxx_[organism].dat"
+     * @param prefix    Prefix of file that you want to generate.
+     * @return  file name of the form:  [prefix]_[organism].txt
+     */
+    public static String getOrganismSpecificFileName (File file, String prefix) {
+        String fileName = file.getName();
+        String parts[] = fileName.split("\\.");
+        if (parts.length == 2) {
+            String firstPart = parts[0];
+            parts = firstPart.split("_");
+            if (parts.length == 1) {
+                throw new IllegalArgumentException ("Illegal file name:  " + fileName);
+            } else {
+                String organism = parts[parts.length-1];
+                return prefix + "_" + organism + ".txt";
+            }
+        } else {
+            throw new IllegalArgumentException ("Illegal file name:  " + fileName);
+        }
+    }
+}
