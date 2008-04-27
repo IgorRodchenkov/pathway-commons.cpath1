fdf5e761b79b5ba3ce4990b7d820b4e25855b136
cerami <cerami@code.google.com>
2008-04-27 00:19:53 +0000

    commented out lines that auto-convert intact psi-mi to biopax, since the biopax files are now checked into cvs

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -76,17 +76,17 @@ cp $CPATH_HOME/../pathway-commons/hprd/09-01-2007/db.info $CPATH_HOME/../pathway
 rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/{*.owl,db.info}
 
 # intact
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py intact-cooker.py $CPATH_HOME/../pathway-commons/intact/12-14-2007 $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.xml
-for i in $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.owl
-do
-	iconv -f ISO-8859-1 -t UTF-8 $i > $i.iconv;
-	mv $i.iconv $i
-done
-cp $CPATH_HOME/../pathway-commons/intact/12-14-2007/db.info $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
+#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py intact-cooker.py $CPATH_HOME/../pathway-commons/intact/12-14-2007 $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
+#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
+#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.xml
+#for i in $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.owl
+#do
+#	iconv -f ISO-8859-1 -t UTF-8 $i > $i.iconv;
+#	mv $i.iconv $i
+#done
+#cp $CPATH_HOME/../pathway-commons/intact/12-14-2007/db.info $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
 ./admin.pl -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax import
-rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/{*.owl,db.info}
+#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/{*.owl,db.info}
 
 # mint
 $CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py mint-cooker.py $CPATH_HOME/../pathway-commons/mint/12-21-2007 $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
