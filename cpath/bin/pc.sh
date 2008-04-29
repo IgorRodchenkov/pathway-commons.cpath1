fe6fd6c772fb7de40e663cc99b4b966bd03c2a54
cerami <cerami@code.google.com>
2008-04-29 15:09:38 +0000

    Reorder loading of databases;  protein-protein interaction databases, with good protein annotations go first

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -49,6 +49,36 @@ date
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_uniprot2uniprot.txt import
 ./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
 
+# Load intact first, as it has broad coverage, and good protein annotations
+#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py intact-cooker.py $CPATH_HOME/../pathway-commons/intact/12-14-2007 $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
+#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
+#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.xml
+#for i in $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.owl
+#do
+#	iconv -f ISO-8859-1 -t UTF-8 $i > $i.iconv;
+#	mv $i.iconv $i
+#done
+#cp $CPATH_HOME/../pathway-commons/intact/12-14-2007/db.info $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
+./admin.pl -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax import
+#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/{*.owl,db.info}
+
+# Then, mint, as it also has broad coverage, and good protein annotations
+$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py mint-cooker.py $CPATH_HOME/../pathway-commons/mint/12-21-2007 $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
+$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
+rm -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax/*.xml
+cp $CPATH_HOME/../pathway-commons/mint/12-21-2007/db.info $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
+./admin.pl -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax import
+rm -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax/{*.owl,db.info}
+
+# Then hprd, as it also has broad coverage, and good protein annotations
+# note to self:  as of 4/29/08:  HPRD incorrectly annotates some proteins as "Mammalia", but annotates them with Human UniPROT Accession IDs.
+$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py hprd-cooker.py $CPATH_HOME/../pathway-commons/hprd/09-01-2007 $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
+$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
+rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/*.xml
+cp $CPATH_HOME/../pathway-commons/hprd/09-01-2007/db.info $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
+./admin.pl -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax import
+rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/{*.owl,db.info}
+
 # reactome
 echo Cooking and Char Converting Reactome Human Files...
 mv -f $FRESH_HOME/reactome/"Homo sapiens.owl" $FRESH_HOME/reactome/Homo_sapiens.owl.bak
@@ -77,35 +107,6 @@ mv -f $CPATH_HOME/../pathway-commons/humancyc/biopax.owl.bak $CPATH_HOME/../path
 #cp $CPATH_HOME/../pathway-commons/ihop/03-14-2008/db.info $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax
 #./admin.pl -f $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax/iHOP.owl import
 #rm -f $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax/{*.owl,db.info}
-
-# hprd
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py hprd-cooker.py $CPATH_HOME/../pathway-commons/hprd/09-01-2007 $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
-rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/*.xml
-cp $CPATH_HOME/../pathway-commons/hprd/09-01-2007/db.info $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
-./admin.pl -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax import
-rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/{*.owl,db.info}
-
-# intact
-#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py intact-cooker.py $CPATH_HOME/../pathway-commons/intact/12-14-2007 $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.xml
-#for i in $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.owl
-#do
-#	iconv -f ISO-8859-1 -t UTF-8 $i > $i.iconv;
-#	mv $i.iconv $i
-#done
-#cp $CPATH_HOME/../pathway-commons/intact/12-14-2007/db.info $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-./admin.pl -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax import
-#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/{*.owl,db.info}
-
-# mint
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py mint-cooker.py $CPATH_HOME/../pathway-commons/mint/12-21-2007 $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
-rm -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax/*.xml
-cp $CPATH_HOME/../pathway-commons/mint/12-21-2007/db.info $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
-./admin.pl -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax import
-rm -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax/{*.owl,db.info}
 
 # fetch publication references
 ./admin.pl pop_ref
