6f43e5f2fa03e7bd1ea139500b1ab49f34fd5b10
cerami <cerami@code.google.com>
2008-04-30 14:20:30 +0000

    Added constants for GO and ENTREZ_GENE

--- cpath/src/org/mskcc/pathdb/util/ExternalDatabaseConstants.java
+++ cpath/src/org/mskcc/pathdb/util/ExternalDatabaseConstants.java
@@ -23,6 +23,16 @@ public class ExternalDatabaseConstants {
     public static String REACTOME = "REACTOME";
 
     /**
+     * Gene Ontology.
+     */
+    public static String GO = "GO";
+
+    /**
+     * Entrez Gene.
+     */
+    public static String ENTREZ_GENE = "ENTREZ_GENE";
+
+    /**
      * Internal Database ID. 
      */
     public static String INTERNAL_DATABASE = "CPATH_ID";
