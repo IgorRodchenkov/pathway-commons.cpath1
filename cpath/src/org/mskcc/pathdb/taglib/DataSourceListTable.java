fcc3a5e0720b2a3eeaeb3992c3a3614eb0accd9b
cerami <cerami@code.google.com>
2008-05-21 15:31:41 +0000

    PROTEIN_UNIFICATION snapshots are now excluded

--- cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
@@ -1,9 +1,6 @@
 package org.mskcc.pathdb.taglib;
 
-import org.mskcc.pathdb.model.CPathRecord;
-import org.mskcc.pathdb.model.GlobalFilterSettings;
-import org.mskcc.pathdb.model.ExternalDatabaseSnapshotRecord;
-import org.mskcc.pathdb.model.ExternalDatabaseRecord;
+import org.mskcc.pathdb.model.*;
 import org.mskcc.pathdb.sql.dao.DaoExternalDbSnapshot;
 import org.mskcc.pathdb.sql.dao.DaoException;
 import org.mskcc.pathdb.sql.dao.DaoExternalDb;
@@ -18,6 +15,7 @@ import org.mskcc.pathdb.sql.assembly.AssemblyException;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.log4j.Logger;
 import org.mskcc.pathdb.sql.query.QueryException;
+import org.mskcc.pathdb.util.ExternalDbSnapshotUtil;
 import org.mskcc.dataservices.util.PropertyManager;
 
 import javax.swing.*;
@@ -55,13 +53,15 @@ public class DataSourceListTable extends HtmlTable {
         DaoExternalDbSnapshot dao = new DaoExternalDbSnapshot();
         ArrayList list = dao.getAllDatabaseSnapshots();
         WebUIBean webUIBean = CPathUIConfig.getWebUIBean();
-        DaoExternalDb daoExternalDb = new DaoExternalDb();
 
+        //  Remove Protein Unification Snapshots before proceeding.
+        list = ExternalDbSnapshotUtil.removeProteinUnificationSnapshots(list);
+        
         // process records
         if (list.size() == 0) {
             startTable();
             startRow();
-            append("<TD COLSPAN=2>No Data Sources Available</TD>");
+            append("<TD COLSPAN=2>No Data Sources Available.</TD>");
             endRow();
             endTable();
         } else {
@@ -75,43 +75,41 @@ public class DataSourceListTable extends HtmlTable {
             for (int i = 0; i < list.size(); i++) {
                 ExternalDatabaseSnapshotRecord snapshotRecord =
                         (ExternalDatabaseSnapshotRecord) list.get(i);
-                append("<table border='0' cellspacing='2' cellpadding='3' "
-                    + "width='100%' class='datasource_table'>");
-                append ("<tr class='c' valign=top>");
-                append("<td valign=top width=50>");
-                if (snapshotRecord.getExternalDatabase() != null) {
-                    ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
+                ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
+                //  Show all snapshots, except PROTEIN_UNIFICATION databases.
+                if (dbRecord != null) {
+                    append("<table border='0' cellspacing='2' cellpadding='3' "
+                        + "width='100%' class='datasource_table'>");
+                    append ("<tr class='c' valign=top>");
+                    append("<td valign=top width=50>");
                     if (dbRecord.getIconFileExtension() != null) {
                         append("<img class='data_source_logo' src='jsp/images/database/"
                             + "db_" + dbRecord.getId() + "." + dbRecord.getIconFileExtension()
                             + "'/>");
                     }
-                }
-                append ("</td>");
-                append ("<td valign=center>"
-                        + DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId()));
-                if (snapshotRecord.getExternalDatabase() != null) {
-                    ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
-					// set proper data source for lucene query
-					ArrayList<Long> dataSourceList = new ArrayList();
-					dataSourceList.add(Long.valueOf(snapshotRecord.getId()));
-					filterSettings.setSnapshotsSelected(dataSourceList);
-					protocolRequest.setQuery("q=data_source:" + dbRecord.getMasterTerm());
-					// do lucene query
-					LuceneQuery search = new LuceneQuery(protocolRequest, filterSettings, new XDebug());
-					search.executeSearch();
-					// create the html
+                    append ("</td>");
+                    append ("<td valign=center>"
+                            + DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId()));
+                    // set proper data source for lucene query
+                    ArrayList<Long> dataSourceList = new ArrayList();
+                    dataSourceList.add(Long.valueOf(snapshotRecord.getId()));
+                    filterSettings.setSnapshotsSelected(dataSourceList);
+                    protocolRequest.setQuery("q=data_source:" + dbRecord.getMasterTerm());
+                    // do lucene query
+                    LuceneQuery search = new LuceneQuery(protocolRequest, filterSettings, new XDebug());
+                    search.executeSearch();
+                    // create the html
                     append ("<br><a href=\"webservice.do?version=");
                     append (webUIBean.getWebApiVersion());
                     append ("&format=html&cmd=get_by_keyword");
-					append ("&" + ProtocolRequest.ARG_ENTITY_TYPE + "=" + (search.getTotalNumHits() > 0 ? "pathway" : "protein"));
-					append ("&" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_NAME + "=" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL);
-					append ("&q=data_source%3A" + dbRecord.getMasterTerm() + "\">");
+                    append ("&" + ProtocolRequest.ARG_ENTITY_TYPE + "=" + (search.getTotalNumHits() > 0 ? "pathway" : "protein"));
+                    append ("&" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_NAME + "=" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL);
+                    append ("&q=data_source%3A" + dbRecord.getMasterTerm() + "\">");
                     append ("Browse</a>");
+                    append ("</td>");
+                    endRow();
+                    endTable();
                 }
-                append ("</td>");
-                endRow();
-                endTable();
             }
         }
     }
