80d9e84f81e9ada3d0b011b484e6ddd680ac925d
cerami <cerami@code.google.com>
2008-06-26 20:57:49 +0000

    First attempt to modularize / refactor pc.sh

--- cpath/bin/pc.sh
+++ cpath/bin/pc.sh
@@ -4,114 +4,182 @@
 # Pathway Commons Import Script
 ############################################################################
 
+# Log Progress
+function logProgress {
+	echo "$1 [`date`]"
+        echo "$1 [`date`]" >> pc_progress.txt
+}
+
 # Dependency Checks
 # If any of these dependencies fail, abort.
-FRESH_HOME=$CPATH_HOME/../pathway-commons/fresh
-if [ ! -f $FRESH_HOME/ncbi/gene2accession ]
-then
-	echo "Fresh NCBI data files not found!  Run pc_get_fresh.sh first. Aborting..."
-	exit
-fi
+function checkDependencies {
+	logProgress "Checking Dependencies..."
+	FRESH_HOME=$CPATH_HOME/../pathway-commons/fresh
+	if [ ! -f $FRESH_HOME/ncbi/gene2accession ]; then
+		echo "Fresh NCBI data files not found!  Run pc_get_fresh.sh first. Aborting..."
+		exit
+	fi
 
-if [ ! -f $FRESH_HOME/uniprot/uniprot_sprot_human.dat ]
-then
-	echo "Fresh UniProt data files not found!  Run pc_get_fresh.sh first. Aborting..."
-	exit
-fi
+	if [ ! -f $FRESH_HOME/uniprot/uniprot_sprot_human.dat ]; then
+		echo "Fresh UniProt data files not found!  Run pc_get_fresh.sh first. Aborting..."
+		exit
+	fi
 
-if [ ! -f $FRESH_HOME/reactome/db.info ]
-then
-	echo "Fresh Reactome data files not found!  Run pc_get_fresh.sh first. Aborting..."
-	exit
-fi
+	if [ ! -f $FRESH_HOME/reactome/db.info ]; then
+		echo "Fresh Reactome data files not found!  Run pc_get_fresh.sh first. Aborting..."
+		exit
+	fi
+}
 
-# setup the database
-echo Import started at:
-date
-./initDb.pl -f
+function init {
+	rm -f pc_progress.txt
+	logProgress "Import started."
+}
 
-# Import the Pathway Commons Data Sources --> Meta Data
-./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
+# Initializes the database
+function initDB {
+	./initDb.pl -f
+
+	# Import the Pathway Commons Data Sources --> Meta Data
+	logProgress "Importing Pathway Commons:  Meta Data."
+	./admin.pl -f ../dbData/externalDb/pathway_commons.xml import
+}
 
 # Import the Human UniProt Protein Info
-./pc_import_uniprot.sh
+function importUniProtBackground {
+	logProgress "Importing UniProt Background Information."
+	./pc_import_uniprot.sh
+}
 
-# Create the Entrez Gene ID Mapping Files from NCBI Source
-# Currently restricted to Human and Mouse
-./entrez_gene.pl $CPATH_HOME/../pathway-commons/fresh/ncbi/gene2accession
+function importEntrezGeneIds {
+	# Create the Entrez Gene ID Mapping Files from NCBI Source
+	# Currently restricted to Human and Mouse
+	logProgress "Creating Entrez Gene ID Mapping Files."
+	./entrez_gene.pl $CPATH_HOME/../pathway-commons/fresh/ncbi/gene2accession
 
-# Import the Entrez Gene Link Out Mapping Files
-./admin.pl -f $CPATH_HOME/../pathway-commons/fresh/ncbi/refseq_2_entrez_gene_id.txt import
-./admin.pl -f $CPATH_HOME/../pathway-commons/fresh/ncbi/uniprot_2_entrez_gene_id.txt import
+	# Import the Entrez Gene Link Out Mapping Files
+	logProgress "Importing Entrez Gene ID Mapping Files."
+	./admin.pl -f $CPATH_HOME/../pathway-commons/fresh/ncbi/refseq_2_entrez_gene_id.txt import
+	./admin.pl -f $CPATH_HOME/../pathway-commons/fresh/ncbi/uniprot_2_entrez_gene_id.txt import
+}
 
 # Import the Unification ID Mapping Files
-./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_uniprot2uniprot.txt import
-./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
+function importUnificationRefs {
+	logProgress "Importing UniProt and RefSeq Mapping Files."
+	./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_uniprot2uniprot.txt import
+	./admin.pl -f $CPATH_HOME/../pathway-commons/ids/cpath_unification_sp2refseq.txt import
+}
 
 # Load intact first, as it has broad coverage, and good protein annotations
-#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py intact-cooker.py $CPATH_HOME/../pathway-commons/intact/12-14-2007 $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.xml
-#for i in $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/*.owl
-#do
-#	iconv -f ISO-8859-1 -t UTF-8 $i > $i.iconv;
-#	mv $i.iconv $i
-#done
-#cp $CPATH_HOME/../pathway-commons/intact/12-14-2007/db.info $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax
-./admin.pl -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax import
-#rm -f $CPATH_HOME/../pathway-commons/intact/12-14-2007/biopax/{*.owl,db.info}
+function importIntAct {
+	logProgress "Loading IntAct."
+	local INTACT_HOME="$CPATH_HOME/../pathway-commons/intact/12-14-2007"
+	#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py intact-cooker.py $INTACT_HOME $INTACT_HOME/biopax
+	#$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $INTACT_HOME/biopax $INTACT_HOME/biopax
+	#rm -f $INTACT_HOME/biopax/*.xml
+	#for i in $INTACT_HOME/biopax/*.owl
+	#do
+	#	iconv -f ISO-8859-1 -t UTF-8 $i > $i.iconv;
+	#	mv $i.iconv $i
+	#done
+	#cp $INTACT_HOME/db.info $INTACT_HOME/biopax
+	./admin.pl -f $INTACT_HOME/biopax import
+	#rm -f $INTACT_HOME/biopax/{*.owl,db.info}
+}
 
 # Then, mint, as it also has broad coverage, and good protein annotations
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py mint-cooker.py $CPATH_HOME/../pathway-commons/mint/12-21-2007 $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
-rm -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax/*.xml
-cp $CPATH_HOME/../pathway-commons/mint/12-21-2007/db.info $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax
-./admin.pl -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax import
-rm -f $CPATH_HOME/../pathway-commons/mint/12-21-2007/biopax/{*.owl,db.info}
+function importMint {
+	logProgress "Loading MINT."
+	local MINT_HOME="$CPATH_HOME/../pathway-commons/mint/12-21-2007"
+	$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py mint-cooker.py $MINT_HOME $MINT_HOME/biopax
+	$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $MINT_HOME/biopax $MINT_HOME/biopax
+	rm -fv $MINT_HOME/biopax/*.xml
+	cp $MINT_HOME/db.info $MINT_HOME/biopax
+	./admin.pl -f $MINT_HOME/biopax import
+	rm -vf $MINT_HOME/biopax/{*.owl,db.info}
+}
 
 # Then hprd, as it also has broad coverage, and good protein annotations
-# note to self:  as of 4/29/08:  HPRD incorrectly annotates some proteins as "Mammalia", but annotates them with Human UniPROT Accession IDs.
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py hprd-cooker.py $CPATH_HOME/../pathway-commons/hprd/09-01-2007 $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
-$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
-rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/*.xml
-cp $CPATH_HOME/../pathway-commons/hprd/09-01-2007/db.info $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax
-./admin.pl -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax import
-rm -f $CPATH_HOME/../pathway-commons/hprd/09-01-2007/biopax/{*.owl,db.info}
+# note to self:  as of 4/29/08:  HPRD incorrectly annotates some proteins as "Mammalia", but annotates
+# them with Human UniPROT Accession IDs.
+function importHPRD {
+	logProgress "Loading HPRD."
+	local HPRD_HOME="$CPATH_HOME/../pathway-commons/hprd/09-01-2007"
+	$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-cooker.py hprd-cooker.py $HPRD_HOME $HPRD_HOME/biopax
+	$CPATH_HOME/../pathway-commons/bin/psi-mi-batch-converter.py $HPRD_HOME/biopax $HPRD_HOME/biopax
+	rm -fv $HPRD_HOME/biopax/*.xml
+	cp $HPRD_HOME/db.info $HPRD_HOME/biopax
+	./admin.pl -f $HPRD_HOME/biopax import
+	rm -fv $HPRD_HOME/biopax/{*.owl,db.info}
+}
 
-# reactome
-echo Cooking and Char Converting Reactome Human Files...
-mv -f $FRESH_HOME/reactome/"Homo sapiens.owl" $FRESH_HOME/reactome/Homo_sapiens.owl.bak
-iconv -f ISO8859-1 -t UTF-8 $FRESH_HOME/reactome/Homo_sapiens.owl.bak > $FRESH_HOME/reactome/Homo_sapiens.owl.iconv
-$CPATH_HOME/../pathway-commons/bin/reactome-cooker.py < $FRESH_HOME/reactome/Homo_sapiens.owl.iconv > $FRESH_HOME/reactome/Homo_sapiens.owl
-rm -f $FRESH_HOME/reactome/Homo_sapiens.owl.iconv
-./admin.pl -f $FRESH_HOME/reactome/Homo_sapiens.owl import
-mv -f $FRESH_HOME/reactome/Homo_sapiens.owl.bak $FRESH_HOME/reactome/"Homo sapiens.owl"
+function importReactome {
+	logProgress "Cooking and Char Converting Reactome Human Files."
+	local REACTOME_HOME="$FRESH_HOME/reactome/"
+	mv -f $REACTOME_HOME/"Homo sapiens.owl" $REACTOME_HOME/Homo_sapiens.owl.bak
+	iconv -f ISO8859-1 -t UTF-8 $REACTOME_HOME/Homo_sapiens.owl.bak > $REACTOME_HOME/Homo_sapiens.owl.iconv
+	$CPATH_HOME/../pathway-commons/bin/reactome-cooker.py < $REACTOME_HOME/Homo_sapiens.owl.iconv > $REACTOME_HOME/Homo_sapiens.owl
+	rm -f $REACTOME_HOME/Homo_sapiens.owl.iconv
+	logProgress "Loading Reactome."
+	./admin.pl -f $REACTOME_HOME/Homo_sapiens.owl import
+	mv -f $REACTOME_HOME/Homo_sapiens.owl.bak $REACTOME_HOME/"Homo sapiens.owl"
+}
 
-# humancyc
-echo Cooking and Char Converting Human Cyc Files...
-mv -f $CPATH_HOME/../pathway-commons/humancyc/biopax.owl $CPATH_HOME/../pathway-commons/humancyc/biopax.owl.bak
-$CPATH_HOME/../pathway-commons/bin/humancyc-cooker.py < $CPATH_HOME/../pathway-commons/humancyc/biopax.owl.bak > $CPATH_HOME/../pathway-commons/humancyc/biopax.owl
-./admin.pl -f $CPATH_HOME/../pathway-commons/humancyc/biopax.owl import
-mv -f $CPATH_HOME/../pathway-commons/humancyc/biopax.owl.bak $CPATH_HOME/../pathway-commons/humancyc/biopax.owl
+function importHumanCyc {
+	logProgress "Cooking and Char Converting HumanCyc Files."
+	local HUMANCYC_HOME="$CPATH_HOME/../pathway-commons/humancyc"
+	mv -f $HUMANCYC_HOME/biopax.owl $HUMANCYC_HOME/biopax.owl.bak
+	$CPATH_HOME/../pathway-commons/bin/humancyc-cooker.py < $HUMANCYC_HOME/biopax.owl.bak > HUMANCYC_HOME/biopax.owl
+	./admin.pl -f $HUMANCYC_HOME/biopax.owl import
+	mv -f $HUMANCYC_PATH/biopax.owl.bak $HUMANCYC_PATH/biopax.owl
+}
 
-# nci
-./admin.pl -f $CPATH_HOME/../pathway-commons/nci/01-28-2008 import
+function importNci {
+	logProgress "Loading NCI / Nature PID."
+	./admin.pl -f $CPATH_HOME/../pathway-commons/nci/01-28-2008 import
+}
 
-# cellmap
-./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
+function importCellMap {
+	logProgress "Loading MSKCC Cell Map."
+	./admin.pl -f $CPATH_HOME/../pathway-commons/cellmap import
+}
 
-# ihop
-#echo "Importing iHOP file..."
-#$CPATH_HOME/../pathway-commons/bin/ihop-converter.sh < $CPATH_HOME/../pathway-commons/ihop/03-14-2008/iHOP_network_gene_chem.txt > $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax/iHOP.owl
-#cp $CPATH_HOME/../pathway-commons/ihop/03-14-2008/db.info $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax
-#./admin.pl -f $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax/iHOP.owl import
-#rm -f $CPATH_HOME/../pathway-commons/ihop/03-14-2008/biopax/{*.owl,db.info}
+function importiHop {
+	#echo "Importing iHOP file..."
+	local IHOP_HOME="$CPATH_HOME/../pathway-commons/ihop/03-14-2008/"
+	#$CPATH_HOME/../pathway-commons/bin/ihop-converter.sh < $IHOPE_HOME/iHOP_network_gene_chem.txt > $IHOP_HOME/biopax/iHOP.owl
+	#cp $IHOP_HOME/db.info $IHOP_HOME/biopax
+	#./admin.pl -f $IHOP_HOME/biopax/iHOP.owl import
+	#rm -fv $IHOP_HOME/biopax/{*.owl,db.info}
+}
 
 # fetch publication references
-./admin.pl pop_ref
+function fetchPublications {
+	logProgress "Fetching Pulication References."
+	./admin.pl pop_ref
+}
 
-# create lucene index
-./admin.pl index
-echo
-echo Import finished at:
-date
+function index {
+	logProgress "Running Lucene Indexer"
+	./admin.pl index
+}
+
+function wrapUp {
+	logProgress "Import finished."
+}
+
+init
+checkDependencies
+initDB
+importUniProtBackground
+importEntrezGeneIds
+importUnificationRefs
+importIntAct
+importHPRD
+importReactome
+importHumanCyc
+importNci
+importCellMap
+#fetchPublications
+#index
+wrapUp
