bda258df059de7b9b9f47e78e288b133cf6e33fa
cerami <cerami@code.google.com>
2008-07-18 13:40:57 +0000

    Modified to now import all rodent Uniprot data

--- cpath/bin/pc_import_uniprot.sh
+++ cpath/bin/pc_import_uniprot.sh
@@ -1,3 +1,9 @@
+# Log Progress
+function logProgress {
+	echo "$1 [`date`]"
+        echo "$1 [`date`]" >> pc_progress.txt
+}
+
 FRESH_HOME=$CPATH_HOME/../pathway-commons/fresh
 
 # If $FRESH_HOME/uniprot/biopax already exists, delete it and all its content
@@ -12,10 +18,14 @@ fi
 echo "Making directory:  $FRESH_HOME/uniprot/biopax"
 mkdir $FRESH_HOME/uniprot/biopax
 
-echo "Converting Human UniProt Files to BioPAX"
+logProgress "Converting Human UniProt Files to BioPAX"
 ./uniprot2biopax.pl $FRESH_HOME/uniprot/uniprot_sprot_human.dat
+
+logProgress "Converting Rodent UniProt Files to BioPAX"
+./uniprot2biopax.pl $FRESH_HOME/uniprot/uniprot_sprot_rodents.dat
+
 mv $FRESH_HOME/uniprot/*.owl $FRESH_HOME/uniprot/biopax
 cp $FRESH_HOME/uniprot/db.info $FRESH_HOME/uniprot/biopax
 
-echo "Importing UniProt BioPax Files"
+logProgress "Importing UniProt BioPax Files"
 ./admin.pl -f $FRESH_HOME/uniprot/biopax import
