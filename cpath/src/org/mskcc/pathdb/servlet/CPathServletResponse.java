c8115f1efb64b3ffdf0859c75174e22ec67a5a04
cerami <cerami@code.google.com>
2008-12-11 18:25:42 +0000

    Fixed compilation error

--- cpath/src/org/mskcc/pathdb/servlet/CPathServletResponse.java
+++ cpath/src/org/mskcc/pathdb/servlet/CPathServletResponse.java
@@ -47,6 +47,4 @@ public class CPathServletResponse implements HttpServletResponse {
 	public String encodeRedirectURL(String url) { return response.encodeRedirectURL(url); }
 	public boolean containsHeader(String header) { return response.containsHeader(header); }
 	public void addCookie(Cookie cookie) { response.addCookie(cookie); }
-    public void setCharacterEncoding(String s) { response.setCharacterEncoding(s); }
-    public String getContentType() { return response.getContentType(); }
 }
