1cb03dcdd8d482257151f18d9f7552789ff6b006
cerami <cerami@code.google.com>
2008-12-17 20:49:13 +0000

    Renamed dumpGsea.pl to more generic dumpGeneSets.pl

---
+++ cpath/bin/dumpGeneSets.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+require "env.pl";
+
+system ("java -ea -Xmx2048M -cp $cp -DCPATH_HOME=$cpathHome org.mskcc.pathdb.tool.DumpGeneSets @ARGV");
--- cpath/bin/dumpGsea.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-
-require "env.pl";
-
-system ("java -ea -Xmx2048M -cp $cp -DCPATH_HOME=$cpathHome org.mskcc.pathdb.tool.DumpGsea @ARGV");
