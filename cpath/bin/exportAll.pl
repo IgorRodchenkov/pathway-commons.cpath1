f852da8ed94dd085f5a9e3bd8181d9c02829d228
cerami <cerami@code.google.com>
2009-01-05 17:37:03 +0000

    Changed target to ExportAll

--- cpath/bin/exportAll.pl
+++ cpath/bin/exportAll.pl
@@ -2,4 +2,4 @@
 
 require "env.pl";
 
-system ("java -ea -Xmx2048M -cp $cp -DCPATH_HOME=$cpathHome org.mskcc.pathdb.tool.ExportGeneSets @ARGV");
+system ("java -ea -Xmx2048M -cp $cp -DCPATH_HOME=$cpathHome org.mskcc.pathdb.tool.ExportAll @ARGV");
