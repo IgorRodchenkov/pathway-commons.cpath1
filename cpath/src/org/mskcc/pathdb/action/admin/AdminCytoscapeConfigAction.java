282c915a60848e37192552b3c06312918a181eb4
Benjamin.Gross <Benjamin.Gross@localhost>
2009-03-26 19:32:09 +0000

    actions to turn on/off cytoscape webstart

---
+++ cpath/src/org/mskcc/pathdb/action/admin/AdminCytoscapeConfigAction.java
@@ -0,0 +1,71 @@
+// $Id: AdminCytoscapeConfigAction.java,v 1.1 2009-03-26 19:32:09 grossben Exp $
+//------------------------------------------------------------------------------
+/** Copyright (c) 2006 Memorial Sloan-Kettering Cancer Center.
+ **
+ ** Code written by: Ethan Cerami
+ ** Authors: Ethan Cerami, Gary Bader, Chris Sander
+ **
+ ** This library is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation; either version 2.1 of the License, or
+ ** any later version.
+ **
+ ** This library is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ ** documentation provided hereunder is on an "as is" basis, and
+ ** Memorial Sloan-Kettering Cancer Center 
+ ** has no obligations to provide maintenance, support,
+ ** updates, enhancements or modifications.  In no event shall
+ ** Memorial Sloan-Kettering Cancer Center
+ ** be liable to any party for direct, indirect, special,
+ ** incidental or consequential damages, including lost profits, arising
+ ** out of the use of this software and its documentation, even if
+ ** Memorial Sloan-Kettering Cancer Center 
+ ** has been advised of the possibility of such damage.  See
+ ** the GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this library; if not, write to the Free Software Foundation,
+ ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ **/
+package org.mskcc.pathdb.action.admin;
+
+import org.mskcc.pathdb.action.*;
+import org.mskcc.pathdb.form.WebUIBean;
+import org.mskcc.pathdb.servlet.CPathUIConfig;
+import org.mskcc.pathdb.xdebug.XDebug;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.*;
+
+
+public class AdminCytoscapeConfigAction extends AdminBaseAction {
+
+    public ActionForward adminExecute(ActionMapping mapping,
+									  ActionForm form,
+									  HttpServletRequest request,
+									  HttpServletResponse response,
+									  XDebug xdebug) throws Exception {
+
+        if (CPathUIConfig.getAdminModeActive() == 0) {
+            return mapping.findForward(BaseAction.FORWARD_UNAUTHORIZED);
+		}
+
+        if (form != null) {
+            populateWebUIBean(form);
+		}
+        return mapping.findForward(BaseAction.FORWARD_SUCCESS);
+    }
+
+    private void populateWebUIBean(ActionForm form) throws Exception {
+
+        WebUIBean formBean = (WebUIBean)form;
+		if (formBean.getApplicationName() == null) {
+			WebUIBean currentBean = CPathUIConfig.getWebUIBean();
+			formBean.setWantCytoscape(currentBean.getWantCytoscape());
+		}
+    }
+}
