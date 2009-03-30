cd9a3a6f16d01df3e7df355c5c47c707d4cc8f39
Benjamin.Gross <Benjamin.Gross@localhost>
2009-03-30 16:32:00 +0000

    update to getNCBITaxonomyIDs - prevent duplicate tax ids from appearing in list

--- cpath/src/org/mskcc/pathdb/tool/ExportUtil.java
+++ cpath/src/org/mskcc/pathdb/tool/ExportUtil.java
@@ -1,8 +1,12 @@
 package org.mskcc.pathdb.tool;
 
+import org.mskcc.pathdb.model.CPathRecord;
+import org.mskcc.pathdb.model.CPathRecordType;
 import org.mskcc.pathdb.model.ExternalLinkRecord;
+import org.mskcc.pathdb.sql.dao.DaoCPath;
 import org.mskcc.pathdb.sql.dao.DaoException;
 import org.mskcc.pathdb.sql.dao.DaoExternalLink;
+import org.mskcc.pathdb.sql.dao.DaoInternalLink;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,4 +30,52 @@ public class ExportUtil {
         }
         return xrefMap;
     }
+
+	/**
+	 * NCBI Taxonomy ID lookup.
+	 *
+	 * @param record CPathRecord
+	 * @param taxIDs ArrayList<Integer>
+	 * @param recIDs ArrayList<Integer>
+     * @throws DaoException         Database Error.
+	 */
+	public static void getNCBITaxonomyIDs(CPathRecord record, ArrayList<Integer> taxIDs, ArrayList<Long> recIDs)
+		throws DaoException {
+
+		// prevent infinite looping
+		if (recIDs.contains(record.getId())) {
+			return;
+		}
+		else {
+			recIDs.add(record.getId());
+		}
+
+		// get records tax id
+		int ncbiTaxonomyID = record.getNcbiTaxonomyId();
+
+		// we need to iterate over all participants and get list of organism ids
+		if (ncbiTaxonomyID == -9999) {
+			DaoCPath daoCPath = DaoCPath.getInstance();
+			DaoInternalLink internalLinker = new DaoInternalLink();
+			ArrayList internalLinks = internalLinker.getTargetsWithLookUp(record.getId());
+			for (int i = 0; i < internalLinks.size(); i++) {
+				CPathRecord descendentRecord = (CPathRecord) internalLinks.get(i);
+				if (descendentRecord.getType() == CPathRecordType.PATHWAY ||
+					descendentRecord.getType() == CPathRecordType.INTERACTION) {
+					ExportUtil.getNCBITaxonomyIDs(descendentRecord, taxIDs, recIDs);
+				}
+				else {
+					if (!taxIDs.contains(descendentRecord.getNcbiTaxonomyId())) {
+						taxIDs.add(descendentRecord.getNcbiTaxonomyId());
+					}
+				}
+			}
+		}
+		// add rec tax id directly and bail
+		else {
+			if (!taxIDs.contains(ncbiTaxonomyID)) {
+				taxIDs.add(ncbiTaxonomyID);
+			}
+		}
+	}
 }
