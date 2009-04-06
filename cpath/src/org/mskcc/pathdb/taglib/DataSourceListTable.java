14d84ec52aa2cd0d129ccba7221431bfd66332f8
Benjamin.Gross <Benjamin.Gross@localhost>
2009-04-06 14:25:28 +0000

    added support for icon - tiling of databsource

--- cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
+++ cpath/src/org/mskcc/pathdb/taglib/DataSourceListTable.java
@@ -23,7 +23,29 @@ import java.io.IOException;
  * @author Benjamin Gross
  */
 public class DataSourceListTable extends HtmlTable {
+	
+	private static int NUM_ICONS_ROW = 4;
+
+	private boolean renderForHomepage;
     private Logger log = Logger.getLogger(DataSourceListTable.class);
+
+	/**
+	 * Set renderForHomepage boolean
+	 *
+	 * @param renderForHomepage Boolean
+	 */
+	public void setRenderForHomepage(Boolean renderForHomepage) {
+		this.renderForHomepage = renderForHomepage;
+	}
+
+	/**
+	 * Get renderForHomepage boolean
+	 *
+	 * @return renderForHomepage Boolean
+	 */
+	public Boolean getRenderForHomepage() {
+		return renderForHomepage;
+	}
 
     /**
      * Executes JSP Custom Tag
@@ -39,10 +61,10 @@ public class DataSourceListTable extends HtmlTable {
      */
     private void outputRecords() throws DaoException, QueryException, IOException,
 										AssemblyException, ParseException {
-        DaoExternalDbSnapshot dao = new DaoExternalDbSnapshot();
+
+		DaoExternalDbSnapshot dao = new DaoExternalDbSnapshot();
         ArrayList list = dao.getAllNetworkDatabaseSnapshots();
-        WebUIBean webUIBean = CPathUIConfig.getWebUIBean();
-        
+
         // process records
         if (list.size() == 0) {
             startTable();
@@ -50,55 +72,140 @@ public class DataSourceListTable extends HtmlTable {
             append("<TD COLSPAN=2>No Data Sources Available.</TD>");
             endRow();
             endTable();
-        } else {
-			// create protocol request and filter objects
+        }
+		else {
+			WebUIBean webUIBean = CPathUIConfig.getWebUIBean();
 			ProtocolRequest protocolRequest = getProtocolRequest(webUIBean);
 			GlobalFilterSettings filterSettings = new GlobalFilterSettings();
-			// set proper type list for lucene query
-			ArrayList<String> typeList = new ArrayList();
-			typeList.add("pathway");
-			filterSettings.setEntityTypeSelected(typeList);
-            for (int i = 0; i < list.size(); i++) {
-                ExternalDatabaseSnapshotRecord snapshotRecord =
-                        (ExternalDatabaseSnapshotRecord) list.get(i);
-                ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
-                //  Show all snapshots, except PROTEIN_UNIFICATION databases.
-                if (dbRecord != null) {
-                    append("<table border='0' cellspacing='2' cellpadding='3' "
-                        + "width='100%' class='datasource_table'>");
-                    append ("<tr class='c' valign=top>");
-                    append("<td valign=top width=50>");
-                    if (dbRecord.getIconFileExtension() != null) {
-                        append("<img class='data_source_logo' src='jsp/images/database/"
-                            + "db_" + dbRecord.getId() + "." + dbRecord.getIconFileExtension()
-                            + "'/>");
-                    }
-                    append ("</td>");
-                    append ("<td valign=center>"
-                            + DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId()));
-                    // set proper data source for lucene query
-                    ArrayList<Long> dataSourceList = new ArrayList();
-                    dataSourceList.add(Long.valueOf(snapshotRecord.getId()));
-                    filterSettings.setSnapshotsSelected(dataSourceList);
-                    protocolRequest.setQuery("q=data_source:" + dbRecord.getMasterTerm());
-                    // do lucene query
-                    LuceneQuery search = new LuceneQuery(protocolRequest, filterSettings, new XDebug());
-                    search.executeSearch();
-                    // create the html
-                    append ("<br><a href=\"webservice.do?version=");
-                    append (webUIBean.getWebApiVersion());
-                    append ("&format=html&cmd=get_by_keyword");
-                    append ("&" + ProtocolRequest.ARG_ENTITY_TYPE + "=" + (search.getLuceneResults().getNumHits() > 0 ? "pathway" : "protein"));
-                    append ("&" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_NAME + "=" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL);
-                    append ("&q=data_source%3A" + dbRecord.getMasterTerm() + "\">");
-                    append ("Browse</a>");
-                    append ("</td>");
-                    endRow();
-                    endTable();
-                }
-            }
+			if (renderForHomepage) {
+				renderRenderForHomepage(filterSettings, protocolRequest, webUIBean, list);
+			}
+			else {
+				renderStatsPage(filterSettings, protocolRequest, webUIBean, list);
+			}
         }
     }
+
+	/**
+	 * Routine to render data source list table on home page.
+	 *
+	 * @param filterSettings GlobalFilterSettings
+	 * @param request ProtocolRequest
+	 * @param webUIBean WebUIBean
+	 * @param list ArrayList
+	 * @throws QueryException
+	 * @throws DaoException
+	 * @throws AssemblyException
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	private void renderRenderForHomepage(GlobalFilterSettings filterSettings, ProtocolRequest protocolRequest, WebUIBean webUIBean, ArrayList list) 
+		throws QueryException, DaoException, AssemblyException, IOException, ParseException {
+
+		ArrayList<String> typeList = new ArrayList();
+		typeList.add("pathway");
+		filterSettings.setEntityTypeSelected(typeList);
+		int rowIconCounter = -1;
+		boolean tableCreated = false;
+		for (int i = 0; i < list.size(); i++) {
+			ExternalDatabaseSnapshotRecord snapshotRecord =
+				(ExternalDatabaseSnapshotRecord) list.get(i);
+			ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
+			//  Show all snapshots, except PROTEIN_UNIFICATION databases.
+			if (dbRecord != null) {
+				if (!tableCreated) {
+					append("<table class='datasource_homepage_table'>");
+					tableCreated = true;
+				}
+				if (rowIconCounter == -1) {
+					append ("<tr valign=top>");
+				}
+				if (++rowIconCounter < NUM_ICONS_ROW) {
+					append("<td class='datasource_homepage_table_cell'");
+					if (dbRecord.getIconFileExtension() != null) {
+						append("<a href=" + "'" + dbRecord.getHomePageUrl() + "'>" +
+							   "<img class='data_source_logo' src='jsp/images/database/" +
+							   "db_" + dbRecord.getId() + "." + dbRecord.getIconFileExtension() + "'" +
+							   "title='" + DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId(), false) + "'" +
+							   "/>" + "</a>");
+					}
+					append ("</td>");
+				}
+				else {
+					endRow();
+					rowIconCounter = -1;
+				}
+			}
+		}
+		if (tableCreated) {
+			if (rowIconCounter != -1) {
+				endRow();
+			}
+			endTable();
+		}
+	}
+
+	/**
+	 * Routine to render data source list table on stats page.
+	 *
+	 * @param filterSettings GlobalFilterSettings
+	 * @param request ProtocolRequest
+	 * @param webUIBean WebUIBean
+	 * @param list ArrayList
+	 * @throws QueryException
+	 * @throws DaoException
+	 * @throws AssemblyException
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	private void renderStatsPage(GlobalFilterSettings filterSettings, ProtocolRequest protocolRequest, WebUIBean webUIBean, ArrayList list)
+		throws QueryException, DaoException, AssemblyException, IOException, ParseException {
+
+		// set proper type list for lucene query
+		ArrayList<String> typeList = new ArrayList();
+		typeList.add("pathway");
+		filterSettings.setEntityTypeSelected(typeList);
+		for (int i = 0; i < list.size(); i++) {
+			ExternalDatabaseSnapshotRecord snapshotRecord =
+				(ExternalDatabaseSnapshotRecord) list.get(i);
+			ExternalDatabaseRecord dbRecord = snapshotRecord.getExternalDatabase();
+			//  Show all snapshots, except PROTEIN_UNIFICATION databases.
+			if (dbRecord != null) {
+				append("<table border='0' cellspacing='2' cellpadding='3' "
+					   + "width='100%' class='datasource_table'>");
+				append ("<tr class='c' valign=top>");
+				append("<td valign=top width=50>");
+				if (dbRecord.getIconFileExtension() != null) {
+					append("<img class='data_source_logo' src='jsp/images/database/"
+						   + "db_" + dbRecord.getId() + "." + dbRecord.getIconFileExtension()
+						   + "'/>");
+				}
+				append ("</td>");
+				append ("<td valign=center>"
+						+ DbSnapshotInfo.getDbSnapshotHtml(snapshotRecord.getId()));
+				// set proper data source for lucene query
+				ArrayList<Long> dataSourceList = new ArrayList();
+				dataSourceList.add(Long.valueOf(snapshotRecord.getId()));
+				filterSettings.setSnapshotsSelected(dataSourceList);
+				protocolRequest.setQuery("q=data_source:" + dbRecord.getMasterTerm());
+				// do lucene query
+				LuceneQuery search = new LuceneQuery(protocolRequest, filterSettings, new XDebug());
+				search.executeSearch();
+				// create the html
+				append ("<br><a href=\"webservice.do?version=");
+				append (webUIBean.getWebApiVersion());
+				append ("&format=html&cmd=get_by_keyword");
+				append ("&" + ProtocolRequest.ARG_ENTITY_TYPE + "=" + (search.getLuceneResults().getNumHits() > 0 ? "pathway" : "protein"));
+				append ("&" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_NAME + "=" + GlobalFilterSettings.NARROW_BY_DATA_SOURCES_FILTER_VALUE_GLOBAL);
+				append ("&q=data_source%3A" + dbRecord.getMasterTerm() + "\">");
+				append ("Browse</a>");
+				append ("</td>");
+				endRow();
+				endTable();
+			}
+		}
+	}
+
 
     /**
 	 * Creates a ProtocolRequest Object.
