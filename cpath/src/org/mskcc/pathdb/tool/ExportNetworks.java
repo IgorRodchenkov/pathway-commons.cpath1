3a396466f13e0105865597d23a59cf3ca56f4dc6
Benjamin.Gross <Benjamin.Gross@localhost>
2009-05-17 23:38:16 +0000

    bug fixes to network export

--- cpath/src/org/mskcc/pathdb/tool/ExportNetworks.java
+++ cpath/src/org/mskcc/pathdb/tool/ExportNetworks.java
@@ -31,8 +31,9 @@ public class ExportNetworks {
     private ExportFileUtil exportFileUtil;
     private ProgressMonitor pMonitor;
 	private HashMap<String, String> processedSIFs;
-	private HashMap<String, String> processedInteractors;
     private final static String TAB = "\t";
+	private final static String GENE_SYMBOL_UNAVAILABLE = "GENE_SYMBOL_UNAVAILABLE";
+	private final static String PMID_UNAVAILABLE = "PMID_UNAVAILABLE";
 
     /**
      * Constructor.
@@ -44,7 +45,6 @@ public class ExportNetworks {
         this.exportFileUtil = exportFileUtil;
         this.pMonitor = pMonitor;
 		processedSIFs = new HashMap<String, String>();
-		processedInteractors = new HashMap<String, String>();
     }
 
     /**
@@ -54,8 +54,8 @@ public class ExportNetworks {
      * @throws AssemblyException    XML Assembly Error.
      * @throws IOException          IO Error.
      */
-    public void exportInteractionRecord(CPathRecord record)
-            throws DaoException, AssemblyException, IOException {
+    public void exportInteractionRecord(CPathRecord record) throws DaoException, AssemblyException, IOException {
+
         DaoExternalDbSnapshot daoSnapshot = new DaoExternalDbSnapshot();
         long snapshotId = record.getSnapshotId();
         ExternalDatabaseSnapshotRecord snapshotRecord =
@@ -80,29 +80,22 @@ public class ExportNetworks {
 				BinaryInteractionAssemblyFactory.createAssembly
 				(binaryInteractionAssemblyType, binaryInteractionRuleTypes, true,
 				 xmlAssembly.getXmlString());
-            String sif = sifAssembly.getBinaryInteractionString();
-            ArrayList <Interaction> interactionList = convertToInteractionList (dbTerm,
-                    record.getId(), sif);
+            List<String> sifs = sifAssembly.getExtendedBinaryInteractionStrings();
+            ArrayList <Interaction> interactionList = convertToInteractionList (dbTerm, record.getId(), sifs.get(0));
+			//String sif = sifAssembly.getBinaryInteractionString();
+			//ArrayList <Interaction> interactionList = convertToInteractionList (dbTerm, record.getId(), sif);
+
+			// get list of all organisms associated with this record (organisms of participants)
+			HashSet<Integer> ncbiTaxonomyIDs = new HashSet<Integer>();
+			ExportUtil.getNCBITaxonomyIDs(record, ncbiTaxonomyIDs, new ArrayList<Long>());
 
             for (Interaction interaction:  interactionList) {
-				ArrayList<String> finalSIFs = getFinalSIFs(interaction, ExportFileUtil.SIF_OUTPUT);
-				// more than one sif string can come out of single cpath interaction record
-				// for each sif string, check and see if we have already created similiar sif
-				ArrayList<String> finalSIFsClone = (ArrayList<String>)finalSIFs.clone();
-				for (String sifClone : finalSIFsClone) {
-					if (processedSIFs.containsKey(sifClone)) {
-						finalSIFs.remove(sifClone);
-					}
+				// per spec, if official gene symbol is not found for any members of interaction, the interaction is not exported in SIF
+				if (!interaction.getGeneA().equals(GENE_SYMBOL_UNAVAILABLE) && !interaction.getGeneB().equals(GENE_SYMBOL_UNAVAILABLE)) {
+					exportRecord(record, dbTerm, interaction, getFinalSIFs(interaction, ExportFileUtil.SIF_OUTPUT), ExportFileUtil.SIF_OUTPUT, ncbiTaxonomyIDs);
 				}
-				if (finalSIFs.isEmpty()) {
-					continue;
-				}
-                exportRecord(record, dbTerm, interaction, finalSIFs, ExportFileUtil.SIF_OUTPUT);
-                exportRecord(record, dbTerm, interaction, getFinalSIFs(interaction, ExportFileUtil.TAB_DELIM_EDGE_OUTPUT), ExportFileUtil.TAB_DELIM_EDGE_OUTPUT);
-				exportRecord(record, dbTerm, interaction, getFinalSIFs(interaction, ExportFileUtil.TAB_DELIM_NODE_OUTPUT), ExportFileUtil.TAB_DELIM_NODE_OUTPUT);
-				for (String processedSIF : finalSIFs) {
-					processedSIFs.put(processedSIF, "");
-				}
+				exportRecord(record, dbTerm, interaction, getFinalSIFs(interaction, ExportFileUtil.TAB_DELIM_EDGE_OUTPUT), ExportFileUtil.TAB_DELIM_EDGE_OUTPUT, ncbiTaxonomyIDs);
+				exportRecord(record, dbTerm, interaction, getFinalSIFs(interaction, ExportFileUtil.TAB_DELIM_NODE_OUTPUT), ExportFileUtil.TAB_DELIM_NODE_OUTPUT, ncbiTaxonomyIDs);
             }
         } catch (JenaException e) {
             pMonitor.logWarning("Got JenaException:  " + e.getMessage() + ".  Occurred "
@@ -139,7 +132,8 @@ public class ExportNetworks {
 			finalSif.append(interaction.getCPathRecordB().getId() + TAB);
 			finalSif.append(interaction.getGeneA() + TAB);
 			finalSif.append(interaction.getGeneB() + TAB);
-			finalSif.append(interaction.getDbSource());
+			finalSif.append(interaction.getDbSource() + TAB);
+			finalSif.append(interaction.getPMID());
 			finalSif.append("\n");
 			toReturn.add(finalSif.toString());
 		}
@@ -178,13 +172,10 @@ public class ExportNetworks {
 	 * @param interaction Interaction
 	 * @param finalSifs ArrayList<String>
 	 * @param outputFormat int
+	 * @param ncbiTaxonomyIDs HashSet<Integer>
 	 */
-    private void exportRecord(CPathRecord record, String dbTerm, Interaction interaction, ArrayList<String> finalSifs, int outputFormat)
+    private void exportRecord(CPathRecord record, String dbTerm, Interaction interaction, ArrayList<String> finalSifs, int outputFormat, HashSet<Integer> ncbiTaxonomyIDs)
             throws IOException, DaoException {
-
-		// export to species specific file(s)
-		HashSet<Integer> ncbiTaxonomyIDs = new HashSet<Integer>();
-		ExportUtil.getNCBITaxonomyIDs(record, ncbiTaxonomyIDs, new ArrayList<Long>());
 
 		// per spec, SIF should not include cross-species interactions
 		if (outputFormat == ExportFileUtil.SIF_OUTPUT && ncbiTaxonomyIDs.size() > 1) {
@@ -196,24 +187,18 @@ public class ExportNetworks {
 			// export to data file
 			String outputFormatStr = exportFileUtil.getOutputFormatString(outputFormat);
 			String key = outputFormatStr + TAB + dbTerm + TAB + finalSif;
-			boolean exportToDataFile = ((outputFormat != ExportFileUtil.TAB_DELIM_NODE_OUTPUT) ||
-										(outputFormat == ExportFileUtil.TAB_DELIM_NODE_OUTPUT && !processedInteractors.containsKey(key)));
-			if (exportToDataFile) {
+
+			// prevent duplicate output in node
+			if (!processedSIFs.containsKey(key)) {
 				exportFileUtil.appendToDataSourceFile(finalSif, dbTerm, outputFormat);
-				if (outputFormat == ExportFileUtil.TAB_DELIM_NODE_OUTPUT) {
-					processedInteractors.put(key, "");
-				}
+				processedSIFs.put(key, "");
 			}
 			// export to species specific file(s)
 			for (Integer taxID : ncbiTaxonomyIDs) {
-				key = outputFormat + TAB + taxID + TAB + finalSif;
-				boolean exportToSpeciesFile = ((outputFormat != ExportFileUtil.TAB_DELIM_NODE_OUTPUT) ||
-											   (outputFormat == ExportFileUtil.TAB_DELIM_NODE_OUTPUT && !processedInteractors.containsKey(key)));
-				if (exportToSpeciesFile) {
+				key = outputFormatStr + TAB + taxID + TAB + finalSif;
+				if (!processedSIFs.containsKey(key)) {
 					exportFileUtil.appendToSpeciesFile(finalSif, taxID, outputFormat);
-					if (outputFormat == ExportFileUtil.TAB_DELIM_NODE_OUTPUT) {
-						processedInteractors.put(key, "");
-					}
+					processedSIFs.put(key, "");
 				}
 			}
 		}
@@ -231,22 +216,25 @@ public class ExportNetworks {
                 String id0Str = parts[0];
                 String intxnType = parts[1];
                 String id1Str = parts[2];
+				// if pmid exists, grab them
+				String pmid = (parts.length == 4) ? parts[3] : PMID_UNAVAILABLE;
 
                 int idA = Integer.parseInt(id0Str);
                 int idB = Integer.parseInt(id1Str);
                 String gene0 = getGeneSymbol(idA);
+				gene0 = (gene0 == null) ? GENE_SYMBOL_UNAVAILABLE : gene0;
                 String gene1 = getGeneSymbol(idB);
-                if (gene0 != null && gene1 != null) {
-                    CPathRecord recordA = daoCPath.getRecordById(idA);
-                    CPathRecord recordB = daoCPath.getRecordById(idB);
-                    Interaction interaction = new Interaction (interactionId, dbSource);
-                    interaction.setCPathRecordA(recordA);
-                    interaction.setCPathRecordB(recordB);
-                    interaction.setGeneA(gene0);
-                    interaction.setGeneB(gene1);
-                    interaction.setInteractionType(intxnType.toUpperCase());
-                    interactionList.add(interaction);
-                }
+				gene1 = (gene1 == null) ? GENE_SYMBOL_UNAVAILABLE : gene1;
+				CPathRecord recordA = daoCPath.getRecordById(idA);
+				CPathRecord recordB = daoCPath.getRecordById(idB);
+				Interaction interaction = new Interaction (interactionId, dbSource);
+				interaction.setCPathRecordA(recordA);
+				interaction.setCPathRecordB(recordB);
+				interaction.setGeneA(gene0);
+				interaction.setGeneB(gene1);
+				interaction.setInteractionType(intxnType.toUpperCase());
+				interaction.setPMID(pmid);
+				interactionList.add(interaction);
             }
         }
         return interactionList;
@@ -272,6 +260,7 @@ class Interaction {
     private String geneB;
     private String interactionType;
     private String dbSource;
+	private String pmid;
     private long cPathId;
     private CPathRecord recordA;
     private CPathRecord recordB;
@@ -311,6 +300,14 @@ class Interaction {
 
     public void setDbSource(String dbSource) {
         this.dbSource = dbSource;
+    }
+
+    public String getPMID() {
+        return pmid;
+    }
+
+    public void setPMID(String pmid) {
+        this.pmid = pmid;
     }
 
     public long getCPathId() {
