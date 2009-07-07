ce5c8ac8ab7212e9118553c3a5540ab46192fd1b
cerami <cerami@code.google.com>
2009-07-07 18:51:06 +0000

    Added SearchTabs

---
+++ cpath/src/org/mskcc/pathdb/taglib/SearchTabs.java
@@ -0,0 +1,19 @@
+package org.mskcc.pathdb.taglib;
+
+/**
+ * Search Tabs Constants
+ */
+public class SearchTabs {
+
+    /**
+     * Title of Physical Entities Tab.
+     */
+    public static final String PHYSICAL_ENTITIES_TAB_TITLE = "Molecules";
+
+
+    /**
+     * Title of Pathways Tab.
+     */
+    public static final String PATHWAYS_TAB_TITLE = "Pathways";
+
+}
