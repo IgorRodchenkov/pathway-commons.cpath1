b97a25c39fe57107123b7189c5c90e858f1961dd
cerami <cerami@code.google.com>
2009-08-25 12:36:49 +0000

    Refactored ExportGeneSets, so that it can now be called from unit tests and web service API

--- cpath/src/org/mskcc/pathdb/tool/ExportGeneSets.java
+++ cpath/src/org/mskcc/pathdb/tool/ExportGeneSets.java
@@ -3,10 +3,8 @@ package org.mskcc.pathdb.tool;
 import org.mskcc.pathdb.model.*;
 import org.mskcc.pathdb.sql.dao.*;
 import org.mskcc.pathdb.util.ExternalDatabaseConstants;
-import org.mskcc.pathdb.schemas.biopax.BioPaxConstants;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -15,11 +13,14 @@ import java.util.ArrayList;
  *
  * This classes supports two data formats:
  * 
- * 1)  GSEA GMT: Gene Matrix Transposed file format (*.gmt) Format.
+ * 1) ExportFileUtil.GSEA_GENE_SYMBOL_OUTPUT: Gene Matrix Transposed file format (*.gmt) Format.
  * Format is described at:
  * http://www.broad.mit.edu/cancer/software/gsea/wiki/index.php/Data_formats
  *
- * 2)  Pathway Commons Gene Set format:  Similar to the GSEA GMT format, except that all
+ * 2) ExportFileUtil.GSEA_ENTREZ_GENE_ID_OUTPUT:  Same as above, except we export Entre Gene IDs,
+ * instead of gene symbols.
+ *
+ * 3) ExportFileUtil.PC_OUTPUT:  Similar to the GSEA GMT format, except that all
  * participants are micro-encoded with multiple identifiers. For example, each participant
  * is specified as: CPATH_ID:RECORD_TYPE:NAME:UNIPROT_ACCESSION:GENE_SYMBOL:ENTREZ_GENE_ID.
  *
@@ -32,23 +33,28 @@ import java.util.ArrayList;
  * ---- gene_sets
  * ------- by_species
  * ------- by source
+ *
+ * @author Benjamin Gross, Ethan Cerami.
  */
 public class ExportGeneSets {
     private final static String TAB = "\t";
     private final static String COLON = ":";
     private final static String NA = "NOT_SPECIFIED";
-    private ExportFileUtil exportFileUtil;
 	private HashMap<String, Integer> processedRecords;
 
-    public ExportGeneSets (ExportFileUtil exportFileUtil) {
-        this.exportFileUtil = exportFileUtil;
+    /**
+     * Constructor.
+     */
+    public ExportGeneSets () {
 		processedRecords = new HashMap<String, Integer>();
     }
 
     /**
-     * Dumps the Pathway Record in the specified file format.
+     * Exports the Pathway Record in all File Formats.
+     * @param record CPath Record.
      */
-    public void exportPathwayRecord(CPathRecord record) throws DaoException, IOException {
+    public void exportPathwayRecordAllFormats(CPathRecord record, ExportFileUtil exportFileUtil)
+            throws DaoException, IOException {
 
 		// this processed record count
 		int count = (processedRecords.containsKey(record.getName())) ?
@@ -59,50 +65,136 @@ public class ExportGeneSets {
 		}
 
         //  Gets the Database Term
-        DaoExternalDbSnapshot daoSnapshot = new DaoExternalDbSnapshot();
-        long snapshotId = record.getSnapshotId();
-        ExternalDatabaseSnapshotRecord snapshotRecord =
-                daoSnapshot.getDatabaseSnapshot(snapshotId);
-        String dbTerm = snapshotRecord.getExternalDatabase().getMasterTerm();
+        String dbMasterTerm = getMasterDbTerm (record);
+        ArrayList <CPathRecord> participantRecordList = getParticipants (record);
 
-        DaoInternalFamily daoInternalFamily = new DaoInternalFamily();
-        long[] descendentIds = daoInternalFamily.getDescendentIds(record.getId(),
-                CPathRecordType.PHYSICAL_ENTITY);
-
-        ArrayList <CPathRecord> cpathRecordList = new ArrayList <CPathRecord>();
-
-        DaoCPath daoCPath = DaoCPath.getInstance();
         //  Get XRefs for all Participants
-        ArrayList <HashMap <String, String>> xrefList =
-                new ArrayList <HashMap <String, String>>();
-        for (long descendentId : descendentIds) {
-            HashMap <String, String> xrefMap = ExportUtil.getXRefMap (descendentId);
-            cpathRecordList.add(daoCPath.getRecordById(descendentId));
-            xrefList.add (xrefMap);
-        }
+        ArrayList<HashMap<String, String>> xrefList = getParticipantXRefs(participantRecordList);
 
-        //  Dump to both file formats.
-        exportGeneSet(record, dbTerm, cpathRecordList, xrefList, ExportFileUtil.GSEA_GENE_SYMBOL_OUTPUT);
-        exportGeneSet(record, dbTerm, cpathRecordList, xrefList, ExportFileUtil.GSEA_ENTREZ_GENE_ID_OUTPUT);
-        exportGeneSet(record, dbTerm, cpathRecordList, xrefList, ExportFileUtil.PC_OUTPUT);
+        //  Dump to all file formats.
+        exportGeneSet(record, dbMasterTerm, participantRecordList, xrefList, exportFileUtil,
+                ExportFileUtil.GSEA_GENE_SYMBOL_OUTPUT);
+        exportGeneSet(record, dbMasterTerm, participantRecordList, xrefList, exportFileUtil,
+                ExportFileUtil.GSEA_ENTREZ_GENE_ID_OUTPUT);
+        exportGeneSet(record, dbMasterTerm, participantRecordList, xrefList, exportFileUtil,
+                ExportFileUtil.PC_OUTPUT);
 
 		// add to processed list
 		processedRecords.put(record.getName(), ++count);
     }
 
     /**
+     * Exports the Pathway Record in the Specified Output Format.
+     * 
+     * @param record CPath Record.
+     */
+    public String exportPathwayRecord (CPathRecord record, int exportOutputFormat)
+            throws DaoException, IOException {
+
+        //  Gets the Database Term
+        String dbMasterTerm = getMasterDbTerm (record);
+        ArrayList <CPathRecord> participantRecordList = getParticipants (record);
+
+        //  Get XRefs for all Participants
+        ArrayList<HashMap<String, String>> xrefList = getParticipantXRefs(participantRecordList);
+
+        String exportText = exportGeneSetToText (record, dbMasterTerm, participantRecordList, xrefList,
+                exportOutputFormat);
+        return exportText;
+    }
+
+    /**
+     * Gets the Master Database Term.
+     * @param record CPath Record.
+     * @return Master Term
+     * @throws DaoException Database Error.
+     */
+    private String getMasterDbTerm (CPathRecord record) throws DaoException {
+        DaoExternalDbSnapshot daoSnapshot = new DaoExternalDbSnapshot();
+        long snapshotId = record.getSnapshotId();
+        ExternalDatabaseSnapshotRecord snapshotRecord =
+                daoSnapshot.getDatabaseSnapshot(snapshotId);
+        if (snapshotRecord != null) {
+            return snapshotRecord.getExternalDatabase().getMasterTerm();
+        } else {
+            return "NA";
+        }
+    }
+
+    /**
+     * Gets Participants in Specified Record.
+     * @param record    CPath Record.
+     * @return          ArrayList of CPath Records.
+     * @throws DaoException Database Error.
+     */
+    private ArrayList <CPathRecord> getParticipants(CPathRecord record) throws DaoException {
+        ArrayList <CPathRecord> participantRecordList = new ArrayList <CPathRecord>();
+        DaoInternalFamily daoInternalFamily = new DaoInternalFamily();
+        long[] descendentIds = daoInternalFamily.getDescendentIds(record.getId(),
+                CPathRecordType.PHYSICAL_ENTITY);
+        DaoCPath daoCPath = DaoCPath.getInstance();
+        for (long descendentId : descendentIds) {
+            participantRecordList.add(daoCPath.getRecordById(descendentId));
+        }
+        return participantRecordList;
+    }
+
+    /**
+     * Gets the Particpant XRefs.
+     * @param participantRecordList List of CPath Records.
+     * @return Participant XRefs.
+     * @throws DaoException Database Error.
+     */
+    private ArrayList<HashMap<String, String>> getParticipantXRefs(ArrayList<CPathRecord> participantRecordList)
+            throws DaoException {
+        ArrayList <HashMap <String, String>> xrefList =
+                new ArrayList <HashMap <String, String>>();
+        for (CPathRecord participantRecord : participantRecordList) {
+            HashMap <String, String> xrefMap = ExportUtil.getXRefMap (participantRecord.getId());
+            xrefList.add (xrefMap);
+        }
+        return xrefList;
+    }
+
+    /**
      * Actual Output of the Gene Set.
      */
     private void exportGeneSet(CPathRecord record, String dbTerm, ArrayList <CPathRecord>
-            cpathRecordList, ArrayList<HashMap<String, String>> xrefList, int outputFormat)
+            cpathRecordList, ArrayList<HashMap<String, String>> xrefList, ExportFileUtil exportFileUtil,
+           int outputFormat)
+            throws IOException, DaoException {
+        String text = exportGeneSetToText (record, dbTerm, cpathRecordList, xrefList,
+                outputFormat);
+
+        //  Append to the correct output files
+        if (text != null) {
+            exportFileUtil.appendToSpeciesFile(text, record.getNcbiTaxonomyId(),
+                    outputFormat);
+            exportFileUtil.appendToDataSourceFile (text, dbTerm, outputFormat);
+        }
+    }
+
+    /**
+     * Output Gene Set.
+     * @param record                CPath Record
+     * @param dbTermMasterTerm      Database Master Term
+     * @param participantsList      CPath Records for All Participants
+     * @param xrefList              XRefs for all Participants
+     * @param outputFormat          Output Format.
+     * @return Text Output.
+     * @throws IOException          IO Error.
+     * @throws DaoException         Database Error.
+     */
+    private String exportGeneSetToText(CPathRecord record, String dbTermMasterTerm, ArrayList <CPathRecord>
+            participantsList, ArrayList<HashMap<String, String>> xrefList, int outputFormat)
             throws IOException, DaoException {
         StringBuffer line = new StringBuffer();
         line.append (record.getName() + TAB);
-        line.append (dbTerm + TAB);
+        line.append (dbTermMasterTerm + TAB);
 
         int numParticipantsOutput = 0;
-        for (int i=0; i < cpathRecordList.size(); i++) {
-            CPathRecord participantRecord = cpathRecordList.get(i);
+        for (int i=0; i < participantsList.size(); i++) {
+            CPathRecord participantRecord = participantsList.get(i);
 			// per spec, GSEA & PC should not contain cross-species genes
 			if (participantRecord.getNcbiTaxonomyId() != record.getNcbiTaxonomyId()) {
                 continue;
@@ -143,12 +235,10 @@ public class ExportGeneSets {
             }
         }
         line.append ("\n");
-
-        //  Append to the correct output files
         if (numParticipantsOutput > 0) {
-            exportFileUtil.appendToSpeciesFile(line.toString(), record.getNcbiTaxonomyId(),
-                    outputFormat);
-            exportFileUtil.appendToDataSourceFile (line.toString(), dbTerm, outputFormat);
+            return line.toString();
+        } else {
+            return line.toString();
         }
     }
 }
